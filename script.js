fd72632550fe55af7f284c1968b18a9e651d3169
samsantosb <samsantosb@outlook.com>
2021-07-09 15:11:02 -0300

    teste

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const q = document.querySelector.bind(document);
 
-const bg = (li) => {
+const color = (li) => {
   li.addEventListener('click', (event) => {
     if (q('.color')) q('.color').classList.remove('color');
     event.target.classList.add('color');
   });
 };
-
-const todo = () => {
-  const newItem = document.createElement('li');
-  bg(newItem);
-  newItem.innerText = q('#texto-tarefa').value;
-  q('#lista-tarefas').appendChild(newItem);
+const clear = () => (q('#lista-tarefas').innerHTML = '');
+const btn = () => q('#apaga-tudo').addEventListener('click', clear);
+const element = () => {
+  newt = document.createElement('li');
+  color(newt);
+  newt.innerText = q('#texto-tarefa').value;
+  q('#lista-tarefas').appendChild(newt);
+  q('#texto-tarefa').value = null;
 };
-
-q('#criar-tarefa').addEventListener('click', todo);
+q('#criar-tarefa').addEventListener('click', element);
+btn();
