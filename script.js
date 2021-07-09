75764af570905914aa15848ff76917b7c7b53e0d
lucas <mr.pine97@gmail.com>
2021-07-09 14:42:37 -0300

    Requisito #3

--- script.js
+++ script.js
@@ -10,10 +10,23 @@ window.onload = function() {
     let pCreation = document.createElement('p');
     pCreation.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
     pCreation.id = 'funcionamento';
-    bodySelection.appendChild(pCreation)
+    bodySelection.appendChild(pCreation);
   }
-
+  function inputCreation() {
+    let bodySelection = document.querySelector('body');
+    let inputCreation = document.createElement('input')
+    inputCreation.id = 'texto-tarefa';
+    bodySelection.appendChild(inputCreation);
+  }
+  function unorderedListCreation() {
+    let bodySelection = document.querySelector('body');
+    let ulCreation = document.createElement('ul')
+    ulCreation.id = 'lista-tarefas';
+    bodySelection.appendChild(ulCreation);
+  }
   headerCreation();
   paragraphCreation();
+  inputCreation();
+  unorderedListCreation();
 
 }
