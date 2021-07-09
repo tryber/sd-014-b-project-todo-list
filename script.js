f29be2c1a93801a191f5ef11b8a08c54b1953c1d
samsantosb <samsantosb@outlook.com>
2021-07-09 19:42:19 -0300

    last changes

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-//ajuda do meu amigo glauco para desenvolver o Script e Css.
-const query = document.querySelector.bind(document);
-const queryAll = document.querySelectorAll.bind(document);
+//ajuda dos meus amigos glauco e Grasi para desenvolver o Script e Css.
+const q = document.querySelector.bind(document);
+//função da cor cinza
 const color = (x) => x.addEventListener('click', (e) => {
-  if (query('.color')) query('.color').classList.remove('color');
+  if (query('.color')) q('.color').classList.remove('color');
   e.target.classList.add('color');
 });
+//marca e desmarca as tarefas
 const task = (y) => {
   y.addEventListener('dblclick', (e) => {
     if (e.target.classList.contains('completed')) e.target.classList.remove('completed');
     else e.target.classList.add('completed');
   });
 }
-const todo = () => {
+//cria as tarefas e atrela elas as duas primeiras funções
+q('#criar-tarefa').addEventListener('click', () => {
   const newItem = document.createElement('li');
   color(newItem), task(newItem);
-  newItem.innerText = query('#texto-tarefa').value;
-  query('#lista-tarefas').appendChild(newItem);
-  query('#texto-tarefa').value = null;
-};
-query('#criar-tarefa').addEventListener('click', todo);
-const clear = () => (query('#lista-tarefas').innerHTML = '');
-query('#apaga-tudo').addEventListener('click', clear);
-const cleark = () => (query('.completed').remove());
-query('#remover-finalizados').addEventListener('click', cleark);
+  newItem.innerText = q('#texto-tarefa').value;
+  q('#lista-tarefas').appendChild(newItem), q('#texto-tarefa').value = null;
+});
+//apaga tudo e remove os finalizados
+q('#apaga-tudo').addEventListener('click', () => (q('#lista-tarefas').innerHTML = ''));
+q('#remover-finalizados').addEventListener('click', () => (q('.completed').remove()));
+
+
+
+
+
 
 
 
