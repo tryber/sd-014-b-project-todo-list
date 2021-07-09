f755502cf02106325b9e1fb41714eecea3fddb30
belaguarino <izabelaguarino@gmail.com>
2021-07-09 14:25:31 -0300

    definição dos elementos - DOM

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const listaOrdenada = document.getElementById('lista-tarefas');
 const string = '';
+
+const btnEnviar = document.getElementById('criar-tarefa');
+const novaTarefa = document.getElementById('texto-tarefa');
+const btnApagar = document.getElementById('apaga-tudo');
+const btnMoverCima = document.getElementById('mover-cima');
+const btnMoverBaixo = document.getElementById('mover-baixo');
+const btnSalvar = document.getElementById('salvar-tarefas');
+const btnRemoveSelecao = document.getElementById('remover-selecionado');
+const btnRemover = document.getElementById('remover-finalizados');
+const selecao = document.querySelectorAll('.selecionado');
