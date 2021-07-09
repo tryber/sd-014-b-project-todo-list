5dabb46e59031d59c361f32502e5101689ff5df8
Davisralves <davialves324@gmail.com>
2021-07-09 15:37:47 -0300

    adicionando requisito 5

---
+++ script.js
@@ -0,0 +1,11 @@
+function createItem(){
+    let input = document.getElementById('texto-tarefa');
+    let ol = document.querySelector('ol');
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    ol.appendChild(li);
+
+    };
+
+let button = document.getElementById('criar-tarefa');
+button.addEventListener('click', createItem);
