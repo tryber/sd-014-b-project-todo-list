bf2ed410cb965e7eabf36ff0b0d69512318af47d
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-09 14:53:54 -0300

    Requisito 3

---
+++ script.js
@@ -0,0 +1,6 @@
+// Cria o campo de input
+const inputContainer = document.querySelector('#input-container');
+const inputTask = document.createElement('input');
+inputTask.id = 'texto-tarefa';
+inputTask.type = 'text';
+inputContainer.appendChild(inputTask);
