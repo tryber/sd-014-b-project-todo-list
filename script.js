f015717b3dfc0a5016c210c06b730eb465ea9482
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-09 18:24:45 -0300

    Requisito 5.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
- 
+// 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão,
+// um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+
+function newTask() {
+  let taskList = document.querySelector('.lista-tarefas');
+  let inputTask = document.getElementById("texto-tarefa").value;
+  let task = document.createElement('li'); // Criar elemento de item listado (li) no document
+  task.innerText = inputTask; // Atribui valor inserido no input, ao elemento'task', tipo "li". que será listado na lista ordenada
+  taskList.appendChild(task); //  Adiciona elemento task (tarefa do input) como elemento filho do elemento pai taskList
+}
