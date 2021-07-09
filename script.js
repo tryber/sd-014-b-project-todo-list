614fc53b2fc97c4b17655fc3bbb868ff292aefed
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-07-09 17:20:58 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 let addItemList = document.querySelector('#lista-tarefas');
 function adicionaTarefa () {
     if(document.getElementById('texto-tarefa').value !== "") {
-        let criandoLi = document.createElement('li');
-        criandoLi.innerHTML = document.getElementById('texto-tarefa').value;
-        addItemList.appendChild(criandoLi)
+        let criaItem = document.createElement('li');
+        criaItem.innerHTML = document.getElementById('texto-tarefa').value
+        criaItem.classList.add('lista')
+        addItemList.appendChild(criaItem)
     }
-    document.getElementById('texto-tarefa').value = "";
+    document.getElementById('texto-tarefa').value = ""
 }
+
 /* Obtive ajuda no Stackoverflow no link https://pt.stackoverflow.com/questions/455158/estou-treinando-o-b%c3%a1sico-do-javascript-e-preciso-de-ajuda-numa-to-do-list */
 
+/* Requisito 7 */
+
+function pintarSelecionado (event) {
+    let listaTarefas = document.getElementsByClassName('lista')
+    for (let index = 0; index < listaTarefas.length;index += 1) {
+
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+
+    if(event.target.style.backgroundColor === 'rgb(12, 128, 128)') {
+        event.target.style.backgroundColor = 'white';
+    }
+    }
+}
+addItemList.addEventListener('click', pintarSelecionado)
