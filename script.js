a3fe5493b7edfd0ad76ea1bc0c6feef267078984
Anderson Leite <andersonleite.dev@gmail.com>
2021-07-09 19:30:44 -0300

    Req 7 e 8 -Alterando cor de fundo do item clicado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const buttonCreate = document.querySelector('#criar-tarefa');
+const Itenslist = document.querySelector('#lista-tarefas');
 
 function addItemList(item) {
   const list = document.querySelector('#lista-tarefas');
@@ -12,4 +13,18 @@ buttonCreate.addEventListener('click', () => {
   const input = document.querySelector('#texto-tarefa');
   addItemList(input.value);
   input.value = '';
+});
+
+function removeSelectedItemList() {
+  const itemSelected = document.querySelector('.selected');
+
+  if (itemSelected != null) {
+    itemSelected.classList.remove('selected');
+  }
+}
+
+Itenslist.addEventListener('click', (item) => {
+  const itemList = item.target;
+  removeSelectedItemList();
+  itemList.classList.add('selected');
 });
