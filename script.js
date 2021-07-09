72b00a257259dede17f488d048cc701989249d2b
Guilherme <guimfontes7@gmail.com>
2021-07-09 19:34:13 -0300

    Add função que remove tarefas completas

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function() {
 
-  let addTask = document.getElementById('criar-tarefa');
-  addTask.addEventListener('click', addTarefa);
+  let addTaskButton = document.getElementById('criar-tarefa');
+  addTaskButton.addEventListener('click', addTask);
+  let removeClearedButton = document.getElementById('apaga-tudo');
+  removeClearedButton.addEventListener('click', removeClearedTasks);
 
-  function addTarefa() {
+  function addTask() {
     let input = document.getElementById('texto-tarefa');
     let newTask = document.createElement('li');
     let position = document.getElementById('lista-tarefas');
@@ -11,7 +13,7 @@ window.onload = function() {
     newTask.className = 'tarefa';
     newTask.innerText = input.value;
     newTask.addEventListener('click', highlight);
-    newTask.addEventListener('dblclick', riskCompleted);
+    newTask.addEventListener('dblclick', riskCleared);
     position.appendChild(newTask);
   }
 
@@ -23,12 +25,20 @@ window.onload = function() {
     selection.target.classList.add('selected');
   }
 
-  function riskCompleted(selection) {
+  function riskCleared(selection) {
     let classes = selection.target.classList;
     if (classes.length === 2) {
-      classes.add('completed');
+      classes.add('cleared');
     } else {
-      classes.remove('completed');
+      classes.remove('cleared');
+    }
+  }
+
+  function removeClearedTasks() {
+    let clearedTasks = document.querySelectorAll('.cleared');
+    let taskList = document.getElementById('lista-tarefas');
+    for(let i = 0; i < clearedTasks.length; i ++) {
+      taskList.removeChild(clearedTasks[i]);
     }
   }
 }
