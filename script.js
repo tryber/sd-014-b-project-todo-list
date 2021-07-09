e623982bacf9c43a5421d39df5bd4eba7f5d7c93
Theo Lima <theo.suporte@hotmail.com>
2021-07-09 18:36:40 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const selectedItem = document.querySelector('.tasksToDo');
 const list = document.querySelector('#lista-tarefas');
 const addList = document.querySelector('#criar-tarefa');
 const addInput = document.querySelector('#texto-tarefa');
@@ -6,9 +7,19 @@ const createTask = () => {
   const itemList = document.createElement('li');
   itemList.innerText = addInput.value;
   itemList.style.fontSize = '17px';
+  itemList.classList.add('tasksToDo');
   if (addInput.value !== '') list.appendChild(itemList);
   addInput.value = '';
 };
+
+function bcgrdColor(event) {
+  const tasks = document.getElementsByClassName('tasksToDo');
+  for (let i = 0; i < tasks.length; i += 1) {
+    tasks[i].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+list.addEventListener('click', bcgrdColor);
 
 const addTask = () => {
   addList.addEventListener('click', createTask);
