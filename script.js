37074ff4b21cde2239093dfab97badc2802913cc
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-09 18:16:10 -0300

    Requisito #4

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.querySelector('h1').innerText = "Minha Lista de Tarefas";
 document.querySelector('#funcionamento').innerText = "Clique duas vezes em um item para marcá-lo como completo";
+
+let tarefaLits = document.querySelector('#lista-tarefas');
+for (let i = 0; i < 5; i += 1) {
+  let addList = document.createElement('li');
+  tarefaLits.appendChild(addList);
+}
