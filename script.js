1b6a3e8f0ec6d76ba8748b761db118c9ff465257
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-09 15:50:06 -0300

    Atende os requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let addButton = document.getElementById('criar-tarefa');
+const addButton = document.getElementById('criar-tarefa');
 
 function generateTask(){
-    let li = document.createElement('li');
-    let list = document.getElementById('lista-tarefas');
-    let inputValue = document.getElementById('texto-tarefa').value;
-    let deletedInput = document.getElementById('texto-tarefa');
+    const li = document.createElement('li');
+    const list = document.getElementById('lista-tarefas');
+    const inputValue = document.getElementById('texto-tarefa').value;
+    const deletedInput = document.getElementById('texto-tarefa');
     li.innerText = inputValue;
+    li.addEventListener('click', selectLi);
     list.appendChild(li);
-    let inputSection = document.getElementById('inputSection');
+    const inputSection = document.getElementById('inputSection');
    inputSection.removeChild(deletedInput);
-   let newInput = document.createElement('input');
+   const newInput = document.createElement('input');
    newInput.id ="texto-tarefa";
+   newInput.placeholder="Digite uma tarefa";
     inputSection.appendChild(newInput);
 
 }
 
 addButton.addEventListener('click', generateTask);
+
+
+function selectLi(event){
+    const allLis = document.querySelectorAll('li');
+    for(let index = 0; index < allLis.length; index += 1){
+        allLis[index].style.backgroundColor = 'rgb(255, 255, 255)';
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+    
+
+}
