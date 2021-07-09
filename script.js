ddf877263bba493ca50424edecfe2c4dcef26b38
Jucélio <juceneon@gmail.com>
2021-07-09 16:13:37 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const lista = document.querySelector('#lista-tarefas');
 const input = document.querySelector('#texto-tarefa');
 
 createList.addEventListener('click', function () {
-  let textoTarefa = input.value;
-  let novaTarefa = document.createElement('li');
+  const textoTarefa = input.value;
+  const novaTarefa = document.createElement('li');
   novaTarefa.innerText = textoTarefa;
   novaTarefa.classList.add('tarefa');
+  novaTarefa.addEventListener('click', selectItem);
   lista.appendChild(novaTarefa);
   input.value = '';
 });
 
-
+function selectItem (event) {
+  const itensTarefa = document.querySelectorAll('.tarefa');
+  for (let item of itensTarefa) {
+    item.classList.remove('selected');
+  }
+event.target.classList.add('selected');
+}
