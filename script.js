3dd5a127f0130b36736cb16e33d54653cc66ca5b
dhenycruz <dheniarley.ds@gmail.com>
2021-07-09 19:21:57 -0300

    Requisito #9.

--- script.js
+++ script.js
@@ -2,10 +2,20 @@
 function changeBackground(li) {
   const oldSelectedLi = document.querySelector('.li-selected');
   if (oldSelectedLi !== null) {
-    oldSelectedLi.className = 'li-tarefa';
+    oldSelectedLi.classList.remove('li-selected');
   }
   const liSelect = li;
-  liSelect.className = 'li-selected';
+  liSelect.classList.add('li-selected');
+}
+
+function finishTarefa(li) {
+  const liThrough = li;
+  const through = 'completed';
+  if (liThrough.classList.contains(through)) {
+    liThrough.classList.remove(through);
+  } else {
+    liThrough.classList.add(through);
+  }
 }
 
 function selectedTarefa() {
@@ -18,6 +28,7 @@ function selectedTarefa() {
     li.className = 'li-tarefa';
     document.querySelector('#texto-tarefa').value = '';
     li.addEventListener('click', function () { changeBackground(li); });
+    li.addEventListener('dblclick', function () { finishTarefa(li); });
   } else {
     alert('Não foi digitada nenhuma tarefa!');
   }
