52b271aa77ebef02a4921e55a79173792599b16c
vinicius-mns <vinicius_mns@hotmail.com>
2021-07-09 16:59:03 -0300

    requisito 6 e 7

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let botao = document.querySelector('#criar-tarefa').addEventListener ('click', clique)
-let tarefas = document.querySelector('#texto-tarefa')
-let lista = document.querySelector('#lista-tarefas')
+let tarefas = document.querySelector('#texto-tarefa') // caixa de texto onde se escreve a tarefa
+let lista = document.querySelector('#lista-tarefas') // lista ordenada (inicialmente vazia, sem <li>,)
 
 function clique() {
-   let li = document.createElement('li')
-   li.innerText = tarefas.value
-   lista.appendChild(li)
-   tarefas.value = ''
+   let li = document.createElement('li') // cria uma <li>
+   li.innerText = tarefas.value // recupera o valor (txt) de `tarefas`[linha 2]
+   lista.appendChild(li) // adiciona <li> em `lista` [linha 3]
+   tarefas.value = '' // apaga o valor no campo (txt) `tarefas` [linha 2]
 }
+
+lista.addEventListener('click', function (event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+})
+
