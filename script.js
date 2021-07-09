9f2059a7ea75f8cbf1e28f3cc63edd0435e226c1
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-07-09 17:04:25 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-//Requisito 5
+// Requisito 5 e 6
 
 function removeInputText() {
   let input = document.querySelector('#texto-tarefa');
   input.value = '';
 }
 
+function changeColor (event) {
+  let lis = document.getElementsByClassName('tarefa');
+  for (let index = 0; index <= lis.length; index += 1) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    // lis[index].style.backgroundColor = 'white';
+  }
+}
+
 function addTextList() {
   let input = document.querySelector('#texto-tarefa');
   let li = document.createElement('li');
+  li.className = 'tarefa';
   li.innerHTML = input.value;
   document.querySelector('#lista-tarefas').appendChild(li);
+  li.addEventListener('click', changeColor);
   removeInputText();
 }
 
