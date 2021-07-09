ef298e171eea53a1f0e432979b0e9c77d3b7285c
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-07-09 16:00:08 -0300

    Projeto To do List :rocket: : requisito 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let buttonAddTask = document.querySelector('#criar-tarefa');
+const buttonAddTask = document.querySelector('#criar-tarefa');
 
-let listParent = document.querySelector('#lista-tarefas');
+const listParent = document.querySelector('#lista-tarefas');
 console.log(buttonAddTask);
-
 
 buttonAddTask.addEventListener('click', criarTarefa);
 
@@ -11,8 +10,26 @@ function criarTarefa() {
   let input = document.querySelector('#texto-tarefa');
   let inputValue = input.value;
   novaTarefa.innerText = inputValue;
+  novaTarefa.classList.add('list-item');
   listParent.appendChild(novaTarefa);
-  document.querySelector('#texto-tarefa').value ='';
+  document.querySelector('#texto-tarefa').value = '';
   console.log(input);
   console.log(inputValue);
+}
+
+listParent.addEventListener('click', mudarCorDaTarefa);
+
+function mudarCorDaTarefa(mouse) {
+  let listItem = document.querySelectorAll('.list-item');
+  for (let index = 0; index < listItem.length; index +=1) {
+    if (listItem[index] === mouse.target) {
+      if (mouse.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
+        mouse.target.style.backgroundColor = 'rgb(128, 128, 128)'
+      } else {
+        mouse.target.style.backgroundColor = '#15151a'
+      }
+    } else {
+      listItem[index].style.backgroundColor = '#15151a'
+    }
+  } 
 }
