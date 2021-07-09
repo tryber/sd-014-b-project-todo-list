700669bae6c5deba5d37dbea819dca920137324b
samsantosb <samsantosb@outlook.com>
2021-07-09 15:02:42 -0300

    trying some changes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const q = document.querySelector.bind(document);
 
+const bg = (li) => {
+  li.addEventListener('click', (event) => {
+    if (q('.color')) q('.color').classList.remove('color');
+    event.target.classList.add('color');
+  });
+};
+
 const todo = () => {
-  const nw = document.createElement('li');
-  nw.innerText = q('#texto-tarefa').value;
-  q('#lista-tarefas').appendChild(nw);
-  q('#texto-tarefa').value = null;
+  const newItem = document.createElement('li');
+  bg(newItem);
+  newItem.innerText = q('#texto-tarefa').value;
+  q('#lista-tarefas').appendChild(newItem);
 };
 
 q('#criar-tarefa').addEventListener('click', todo);
