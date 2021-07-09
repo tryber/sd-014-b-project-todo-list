106a295ea174b83f81eb6df74480e04466d69bc3
samsantosb <samsantosb@outlook.com>
2021-07-09 18:44:00 -0300

    fim

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 //ajuda do meu amigo glauco para desenvolver o Script e Css.
-const selector = document.querySelector.bind(document);
-
-const color = (li) => li.addEventListener('click', (event) => {
-  if (selector('.color')) selector('.color').classList.remove('color');
-  event.target.classList.add('color');
+const q = document.querySelector.bind(document);
+const color = (l) => l.addEventListener('click', (e) => {
+  if (q('.color')) q('.color').classList.remove('color');
+  e.target.classList.add('color');
 });
-
-const completed = (a) => a.addEventListener('dblclick', (e) => e.target.classList.add('completed'));
-
-const clear = () => (selector('#lista-tarefas').innerHTML = '');
-const clearAll = () => selector('#apaga-tudo').addEventListener('click', clear);
-
-clearAll();
+const task = (m) => {
+  m.addEventListener('dblclick', (e) => {
+    if (e.target.classList.contains('completed')) e.target.classList.remove('completed');
+    else e.target.classList.add('completed');
+  });
+}
 const todo = () => {
   const newI = document.createElement('li');
-  color(newI);
-  completed(newI);
-  newI.innerText = selector('#texto-tarefa').value;
-  selector('#lista-tarefas').appendChild(newI);
-  selector('#texto-tarefa').value = null;
+  color(newI), task(newI);
+  newI.innerText = q('#texto-tarefa').value;
+  q('#lista-tarefas').appendChild(newI);
+  q('#texto-tarefa').value = null;
 };
+q('#criar-tarefa').addEventListener('click', todo);
+const clear = () => (q('#lista-tarefas').innerHTML = '');
+q('#apaga-tudo').addEventListener('click', clear);
+const cleark = () => (q('.completed').remove());
+q('#remover-finalizados').addEventListener('click', cleark);
 
-selector('#criar-tarefa').addEventListener('click', todo);
+
+
+
+
 
