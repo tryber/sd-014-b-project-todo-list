151a3621597cc86c565017e6d2b96ea79a70ee51
dhenycruz <dheniarley.ds@gmail.com>
2021-07-09 17:26:02 -0300

    Requisito #5.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script JS.
+function selectedTarefa() {
+  const conteudo = document.querySelector('#texto-tarefa').value;
+  if (conteudo !== '') {
+    const olPai = document.querySelector('ol');
+    const li = document.createElement('li');
+    li.innerText = conteudo;
+    olPai.appendChild(li);
+    document.querySelector('#texto-tarefa').value = '';
+  } else {
+    alert('Não foi digitada nenhuma tarefa!');
+  }
+}
+const button = document.querySelector('#criar-tarefa');
+
+button.addEventListener('click', selectedTarefa);
