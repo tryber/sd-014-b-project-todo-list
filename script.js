9e1de245939950ba4c2e876ee12cf01a4651743d
Nataniel Santos <nathan.santos159@hotmail.com>
2021-07-09 15:59:08 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskList = document.getElementById('lista-tarefas')
 const inputTask = document.getElementById('texto-tarefa')
 const btnCreateTask = document.getElementById('criar-tarefa')
+let listItens = taskList.childNodes
 
 btnCreateTask.addEventListener('click', addTask)
 function addTask(){
@@ -9,5 +10,21 @@ function addTask(){
     listItem.innerText = inputTask.value
     taskList.appendChild(listItem)
     inputTask.value = null
+    updateListItensReference()
+  }
+}
+
+
+
+function updateListItensReference(){
+  listItens = taskList.childNodes
+  for (item of listItens) {
+    item.addEventListener('click', function(event){
+      if (event.target.style.backgroundColor === 'rgb(128, 128, 128)'){
+        item.style.backgroundColor = 'white'
+      } else {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+      }
+    })
   }
 }
