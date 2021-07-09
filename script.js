dd0d52dc8cdb67817d7ac30b8e7807fe32bf507b
Cecilia Cacais <ceci.cacais@gmail.com>
2021-07-09 17:08:52 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ botaoAdicionar.addEventListener("click", adicionaTarefa);
 function adicionaTarefa(){
     tarefaParaAdicionar = document.querySelector('#texto-tarefa');
     let criandoItem = document.createElement('li');
+    criandoItem.className = 'itensLista';
     criandoItem.innerHTML = tarefaParaAdicionar.value; //copiando o valor da caixa tarefa adc e passando para a li
     listaDeTarefas.appendChild(criandoItem);
     tarefaParaAdicionar.value = ''; //apagando o valor da caixa
+}
+
+let botaoPintarOitem = document.querySelector('#lista-tarefas');
+botaoPintarOitem.addEventListener("click", pintaOItem);
+function pintaOItem(){
+    let itensDaLista = document.querySelector('.itensLista');;
+    itensDaLista.className = 'corDoItem'
 }
