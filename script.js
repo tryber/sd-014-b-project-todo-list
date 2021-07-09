31e2f3f4489283f37f8aa256a5f892665a3f8f01
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-07-09 16:33:24 -0300

    Atende 7° e 8° requisito.

--- script.js
+++ script.js
@@ -2,20 +2,10 @@ window.onload = function () {
   const input = document.querySelector('#texto-tarefa');
   const btnAddList = document.querySelector('#criar-tarefa');
   const todoList = document.querySelector('#lista-tarefas');
-  function addList() {
-    const item = document.createElement('li');
-    const inputValue = input.value;
-    item.innerText = inputValue;
-    todoList.appendChild(item);
-    input.value = '';
-    item.addEventListener('click', selectItem);
-    item.addEventListener('dblclick', functionZ);
-  }
   function selectItem(event) {
-    let itens = todoList.children;
-    console.log(itens);
+    const itens = todoList.children;
     for (let index = 0; index < itens.length; index += 1) {
-      if (itens[index].classList){
+      if (itens[index].classList) {
         itens[index].classList.remove('selected');
       }
     }
@@ -23,18 +13,14 @@ window.onload = function () {
       event.target.classList.add('selected');
     }
   }
-  btnAddList.addEventListener('click', addList);  
+  function addList() {
+    const item = document.createElement('li');
+    const inputValue = input.value;
+    item.innerText = inputValue;
+    todoList.appendChild(item);
+    input.value = '';
+    item.addEventListener('click', selectItem);
+    //item.addEventListener('dblclick', functionZ);
+  }
+  btnAddList.addEventListener('click', addList);
 };
-/*
-function selectColor(event) {
-    // 1° passo um Loop em todas as cores e removo a classe de todos
-    for (let index = 0; index < coresPalet.length; index += 1) {
-      if (coresPalet[index].classList) {
-        coresPalet[index].classList.remove('selected');
-      }
-    }
-    // 2° adiciono a classe novamente apenas na cor que recebeu o clique.
-    if (event.target.classList) {
-      event.target.classList.add('selected');
-    }
-  } */
