97364067492eff320721eebe95ca53c9c4be1ee5
belaguarino <izabelaguarino@gmail.com>
2021-07-09 14:29:45 -0300

    Função de taxar ao clicar

--- script.js
+++ script.js
@@ -92,13 +92,21 @@ window.onload = function () {
 
 // Função de mudar a cor ao clicar
 listaOrdenada.addEventListener('click', (event) => {
-    const selecao = document.querySelectorAll('.selecionado');
-    for (let i = 0; i < selecao.length; i++) {
-      selecao[i].classList.remove('selecionado');
-      selecao[i].style.backgroundColor = '';
-    }
-    event.target.classList.add('selecionado');
-    const cinza = document.querySelector('.selecionado');
-    cinza.style.backgroundColor = 'rgb(128, 128, 128)';
-  });
-  
+  const selecao = document.querySelectorAll('.selecionado');
+  for (let i = 0; i < selecao.length; i++) {
+    selecao[i].classList.remove('selecionado');
+    selecao[i].style.backgroundColor = '';
+  }
+  event.target.classList.add('selecionado');
+  const cinza = document.querySelector('.selecionado');
+  cinza.style.backgroundColor = 'rgb(128, 128, 128)';
+});
+
+// Função de taxar ao clicar
+listaOrdenada.addEventListener('dblclick', (e) => {
+  if (e.target.classList.contains('completed')) {
+    e.target.classList.remove('completed');
+  } else {
+    e.target.classList.add('completed');
+  }
+});
