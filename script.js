8a68063e629d188b2e7fa21838628ff08a7e1c48
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-09 18:49:44 -0300

    Requisito número 9

--- script.js
+++ script.js
@@ -13,15 +13,34 @@ document.querySelector('#criar-tarefa').addEventListener('click', function () {
   inputPath.value = '';
 
   tarefaLi.addEventListener('click', function(event) {
-    let classSelected = document.querySelector('.selected');
-      if (classSelected) {
-        classSelected.classList.remove('selected');
-      }
-        event.target.classList.add('selected');
+    addRemoveSelected(event)
   })
+  tarefaLi.addEventListener('dblclick', function(event) {
+    addRemoveCompleted(event)
+  })
+
 })
 // 1. Bernardo Salgueiro salvador do requisito 7!!! Atribuir o evento logo após criar a Li, resolve a questão de não conseguir percorrer o array vazio de li's.
 // 2. Alternativa do Luiz Gustavo
 
 // * Requisito - 8
-// * O Eli dos Santos me deu uma luz, me lembrando que existia a propriedade 'children', que traz info atualizada das child's/li's
+// * O Eli dos Santos me deu uma luz, me lembrando que existia a propriedade 'children', que traz info atualizada das child's do elemento pai. Não precisei usar '.children', porque percebi conversando com o Thiago Frozzi, que o problema eram as váriveis no início do script que iniciavam vazias, .
+
+function addRemoveSelected (event) {
+  let classSelected = document.querySelector('.selected');
+  if (classSelected) {
+    classSelected.classList.remove('selected');
+  }
+    event.target.classList.add('selected');
+}
+
+// * Requisito - 9
+
+function addRemoveCompleted (event) {
+  let classSelected = document.querySelector('.completed');
+  if (classSelected) {
+    classSelected.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
