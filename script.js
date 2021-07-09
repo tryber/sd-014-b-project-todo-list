cd2315960d147aec8d1cb0a6657a59683535d42b
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-09 16:04:16 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let olPath = document.querySelector('#lista-tarefas');
 let inputPath = document.querySelector('#texto-tarefa');
+let liTarefaPath = document.getElementsByClassName('liTarefa');
 
-
-// * Requisito - 5
+// * Requisitos - 5, 6 e 7
 document.querySelector('#criar-tarefa').addEventListener('click', function () {
   let tarefaLi = document.createElement('li');
-  olPath.appendChild(tarefaLi).innerText = inputPath.value;
+  if (inputPath.value != '') {
+    olPath.appendChild(tarefaLi).innerText = inputPath.value;
+  }
+  tarefaLi.className = 'liTarefa';
   inputPath.value = '';
+
+  tarefaLi.addEventListener('click', function() {
+    tarefaLi.style.backgroundColor = 'rgb(128,128,128)';
+  })
 })
