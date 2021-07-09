d0b4c0e800206e60c49c8f15abb25317714a82e8
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-09 15:59:55 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let listaTarefas = document.querySelector('#lista-tarefas');
 let botao = document.querySelector('#criar-tarefa');
 let tarefa = document.querySelector('#texto-tarefa');
+let selecionada;
 
 function adicionaTarefa () {
   let aux = document.createElement('li');
@@ -9,7 +10,18 @@ function adicionaTarefa () {
   tarefa.value = '';
 }
 
+function removeSelecao () {
+  if(document.querySelectorAll('.selecionada').length >= 1){
+    selecionada = document.querySelector('.selecionada');
+    selecionada.style.backgroundColor = 'white';
+    selecionada.className = '';
+  }
+}
+
 botao.addEventListener('click', adicionaTarefa);
+
 listaTarefas.addEventListener('click', (event) => {
+  removeSelecao();
+  event.target.className = 'selecionada';
   event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 });
