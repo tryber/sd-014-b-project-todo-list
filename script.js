b4c1c338a383b7af427eb7d410f80b1d0463afc8
Esdras Oliveira <esdrasjesus5@gmail.com>
2021-07-09 15:04:31 -0300

    Requisito 7 Completo

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-let botaoCriarTarefa = document.getElementById('criar-tarefa');
-let inputTextoTarefa = document.getElementById('texto-tarefa');
-let listaTarefas = document.getElementById('lista-tarefas');
+const botaoCriarTarefa = document.getElementById('criar-tarefa');
+const inputTextoTarefa = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 function addTarefaLista() {
-  let tarefa = document.createElement('li');
+  const tarefa = document.createElement('li');
   listaTarefas.appendChild(tarefa);
   tarefa.innerText = inputTextoTarefa.value;
+  tarefa.className = 'tarefa';
   inputTextoTarefa.value = '';
 }
 botaoCriarTarefa.addEventListener('click', addTarefaLista);
+
+function corDeFundoItemLista(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaTarefas.addEventListener('click', corDeFundoItemLista);
