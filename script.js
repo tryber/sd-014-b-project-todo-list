b7e68452fb5bd1bedc3a1ab7bd3fb7959d29e183
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-09 14:41:21 -0300

    Requisitos #4 e #5

---
+++ script.js
@@ -0,0 +1,13 @@
+let inputTarefa = document.getElementById('texto-tarefa');
+let botaoAdicionarTarefa = document.getElementById('criar-tarefa');
+
+inputTarefa.addEventListener('keyup', tarefaInserida);
+botaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
+
+function tarefaInserida() {
+
+}
+
+function adicionarTarefa() {
+
+}
