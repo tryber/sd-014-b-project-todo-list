6cf33a9a286203325a9b8f7a3bbbfdd17ef9ebe8
Davisralves <davialves324@gmail.com>
2021-07-09 16:14:43 -0300

    adicionando requisito 6

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-function createItem(){
-    let input = document.getElementById('texto-tarefa');
+function createItem() {
+  let input = document.getElementById('texto-tarefa');
+  let ol = document.querySelector('ol');
+  let li = document.createElement('li');
+  li.innerText = input.value;
+  ol.appendChild(li);
+  input.value = '';
+}
+function apagaLista(){
+    let lista = document.querySelectorAll('li'); // apaga a lista;
     let ol = document.querySelector('ol');
-    let li = document.createElement('li');
-    li.innerText = input.value;
-    ol.appendChild(li);
-    input.value = '';
-    };
+    for (let item of lista){
+      ol.removeChild(item);
+    }
+}
+
+
+function sortItens() {
+  let lista = document.querySelectorAll('li'); // Faz o ultimo Item da lista se tornar o primeiro.
+  let ultimoItem = lista[lista.length - 1];   
+  let ol = document.querySelector('ol');
+  apagaLista();  
+  ol.appendChild(ultimoItem);
+  for (let index = 0; index < lista.length - 1; index += 1) {
+    ol.appendChild(lista[index]);
+  }
+}
 
 let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', createItem);
+button.addEventListener('click', sortItens);
+//let h1 = document.querySelector('h1');
+//h1.addEventListener('click', sortItens);
