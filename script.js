c003377fc18b7f26384989ef4798953d52c4d6e4
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-07-09 17:34:27 -0300

    Projeto To do List :rocket: : requisito 11

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function criarTarefa() {
   let inputValue = input.value;
   novaTarefa.innerText = inputValue;
   novaTarefa.classList.add('list-item');
-  novaTarefa.addEventListener('dblclick', mudarStatusDaTarefa)
+  novaTarefa.addEventListener('dblclick', mudarStatusDaTarefa);
   novaTarefa.addEventListener('click', mudarCorDaTarefa);
   listParent.appendChild(novaTarefa);
   document.querySelector('#texto-tarefa').value = '';
@@ -19,30 +19,44 @@ function criarTarefa() {
 
 function mudarCorDaTarefa(mouse) {
   let listItem = document.querySelectorAll('.list-item');
-  for (let index = 0; index < listItem.length; index +=1) {
+  for (let index = 0; index < listItem.length; index += 1) {
     if (listItem[index] === mouse.target) {
       if (mouse.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
-        mouse.target.style.backgroundColor = 'rgb(128, 128, 128)'
+        mouse.target.style.backgroundColor = 'rgb(128, 128, 128)';
       } else {
-        mouse.target.style.backgroundColor = '#15151a'
-      
+        mouse.target.style.backgroundColor = '#15151a';
+
       }
     } else {
-      listItem[index].style.backgroundColor = '#15151a'
+      listItem[index].style.backgroundColor = '#15151a';
     }
-  } 
+  }
 }
 
-function mudarStatusDaTarefa (event) {
- event.target.classList.toggle('completed')
+function mudarStatusDaTarefa(event) {
+  event.target.classList.toggle('completed');
 }
 
-eraseButton.addEventListener('click', apagaTodasTarefas)
+eraseButton.addEventListener('click', apagaTodasTarefas);
 
-function apagaTodasTarefas () {
-  let tarefas = document.querySelectorAll('.list-item')
-  let tarefasParent = document.querySelector('#lista-tarefas')
-  for (index = 0; index < tarefas.length; index += 1){
+function apagaTodasTarefas() {
+  let tarefas = document.querySelectorAll('.list-item');
+  let tarefasParent = document.querySelector('#lista-tarefas');
+  for (index = 0; index < tarefas.length; index += 1) {
     tarefasParent.removeChild(tarefas[index]);
   }
 }
+
+removeFinishedButton.addEventListener('click', removerTarefasFinalizadas);
+
+function removerTarefasFinalizadas() {
+  let tarefas = document.querySelectorAll('.list-item');
+  let tarefasParent = document.querySelector('#lista-tarefas');
+  for (index = 0; index < tarefas.length; index += 1) {
+    if (tarefas[index].className === 'list-item completed') {
+      tarefasParent.removeChild(tarefas[index]);
+    }
+  }
+}
+
+
