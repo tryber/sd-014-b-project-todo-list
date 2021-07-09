f2f86a300018c9bd901f5e647dee4c2a9d3ab4fa
Vitor Silva <vitor.o.silva95@gmail.com>
2021-07-09 17:39:20 -0300

    Requisito 7# e Requisito8#

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-debugger;
-
+function marcarTarefa() {
+  if (!this.classList.contains('.tarefa-marcada')) {
+    const atual = document.querySelector('.tarefa-marcada');
+    if (atual) {
+      atual.classList.remove('tarefa-marcada');
+    }
+    this.classList.add('tarefa-marcada');
+  }
+}
 function adicionaTarefa() {
   let input = document.getElementById('texto-tarefa');
   let lista = document.querySelector('#lista-tarefas');
   let li = document.createElement('li');
+  li.classList.add('item-li');
   let texto = document.createTextNode(input.value);
   li.appendChild(texto);
+  li.addEventListener('click', marcarTarefa);
   lista.appendChild(li);
   input.value = '';
 }
-let criarTarefa = document.querySelector('#criar-tarefa');
-criarTarefa.addEventListener('click',adicionaTarefa);
+const criarTarefa = document.querySelector('#criar-tarefa');
+criarTarefa.addEventListener('click', adicionaTarefa);
+
