9c7c1bf90d0f1f8f7bf357f00cd393f0ecb329b2
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-09 16:56:52 -0300

    Requisito 7 e 8 ok

--- script.js
+++ script.js
@@ -4,13 +4,30 @@ const input = document.getElementById('texto-tarefa')
 
 
 
+
 button.addEventListener('click',criarTarefas)
+
+
+function trocarCor(event){
+    let selected = document.querySelector('.selected')
+    if (selected){
+        selected.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+}
+    
+
 
 function criarTarefas(){
     let li = document.createElement('li');
-    li.innerText = input.value
+    li.innerText = input.value;
     listaTarefas.appendChild(li);
     clear = document.getElementById('texto-tarefa').value = ""
-    
+    li.addEventListener('click',trocarCor);
+  // Trocar a cor de fundo 
 }
+
+    
+    
+
 
