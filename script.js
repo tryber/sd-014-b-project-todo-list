a7dfdde94836fce715d96ea60fa7e0e4d49ed02a
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-07-09 16:03:20 -0300

    requisito 07

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+function changeColorToGray(color) {
+ color.target.className = 'concluded';
+}
+
 function createListItem() {
  const button = document.getElementById('criar-tarefa');
  let input = document.getElementById('texto-tarefa');
  const ol = document.getElementById('lista-tarefas');
  button.addEventListener('click', function () {
    let li = document.createElement('li');
+   li.addEventListener('click', changeColorToGray)
    li.innerHTML = input.value;
    ol.appendChild(li);
    input.value = '';
  })
 }
 createListItem()
+
+
+
 
