041db7a80547c4273980d867a7c0ada2881d42e6
Matheus Laurindo <matheus104521@gmail.com>
2021-07-09 16:37:55 -0300

    Conclui o desafio 6, 7 e 8

--- script.js
+++ script.js
@@ -3,13 +3,34 @@ btnAddTask.addEventListener('click', createTask);
 
 //Cria os elementos li, dentro da lista
 function createTask() {
+    //Faz a ligação da OL e do Input Text
     let listTask = document.querySelector('#lista-tarefas');
     let txtTask = document.querySelector('#texto-tarefa').value;
 
+    //Cria o elemento, adiciona o texto inserido no input
+    //Adiciona classe, evento de clique e adiciona no html
     let itemTask = document.createElement('li');
     itemTask.innerHTML = txtTask;
+    itemTask.className = 'tasks';
+    itemTask.addEventListener('click', changeColor);
 
     listTask.appendChild(itemTask);
 
+    //Seta o valor do input como vazio
     document.querySelector('#texto-tarefa').value = '';
 }
+
+function changeColor(evento) {
+    let listItems = document.querySelectorAll('.tasks');
+    let itemClicked = evento.target;
+
+    for (let key of listItems) {
+        let item = key;
+
+        //Muda a cor de todos para branco
+        item.style.backgroundColor = 'rgb(0, 0, 0, 0)';
+
+        //Seta a cor do item clickado para cinza
+        itemClicked.style.backgroundColor = 'rgb(128, 128, 128)';
+    };  
+} 
