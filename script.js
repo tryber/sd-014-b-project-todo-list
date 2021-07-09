c7054f5a53abd0222ffca7a4c6cce4b1fd20a2fd
FelipeMartins <mfelipeluiz99@gmail.com>
2021-07-09 20:05:09 -0300

    requisito 7

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function insertLi() {
   let criaLi = document.createElement('li');
   let tagTextoTarefa = document.getElementById('texto-tarefa');
   tagListaTarefa.appendChild(criaLi);
+  let selectLi = document.getElementsByTagName('li');
+  for (let i = 0; i < selectLi.length; i++) {
+    const element = selectLi[i];
+    element.addEventListener('click', turnGrey);
+  }
   criaLi.innerText = tagTextoTarefa.value;
   tagTextoTarefa.value = '';
+}
+
+function turnGrey() {
+  let selectLi = document.getElementsByTagName('li');
+  for (let i = 0; i < selectLi.length; i++) {
+    const element = selectLi[i];
+    element.style.backgroundColor = 'rgb(128,128,128)';
+  }
 }
