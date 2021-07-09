c9feef250aacecd357b0fff847eeaa7e9357ea80
Bruno Trindade <brunotrndd@gmail.com>
2021-07-09 19:32:14 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ function createTask() {
   const taskList = document.querySelector('#lista-tarefas');
   const task = document.createElement('li');
   task.innerHTML = input;
+  task.classList.add('selected');
+  task.style.backgroundColor = 'white';
   if (task !== '') {
     taskList.appendChild(task);
   }
   document.querySelector('#texto-tarefa').value = '';
 }
+
+const list = document.querySelector('#lista-tarefas');
+
+function changeColor(event) {
+  const evento = event;
+  const taskItem = document.querySelectorAll('.selected');
+  for (let index = 0; index < taskItem.length; index += 1) {
+    evento.target.style.backgroundColor = 'grey';
+  }
+}
+list.addEventListener('click', changeColor);
