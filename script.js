c9c31debd9d99397db7e165ff9f6d2e1fd4da0a1
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-07-09 15:58:51 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -15,10 +15,15 @@ function addLi() {
 // Chamada do evento de click para o elemento button
 buttonNewTarefa.addEventListener('click', addLi);
 
-// Função altera backgorund-color da li selecionada;
+// Função altera backgorund-color da li selecionada
+// sem permitir que seja alterada mais de uma;
 function alterBackgrountLi(event) {
+  const liSelect = document.querySelectorAll('li');
+  for (let index = 0; index < liSelect.length; index += 1) {
+    liSelect[index].removeAttribute('style');
+  }
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
-// Chamada de evento de click para selecionar elemento li;
+// Chamada de evento de click para selecionar elemento li
 listTarefas.addEventListener('click', alterBackgrountLi);
