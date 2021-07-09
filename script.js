1e8cbd028363cac5752ff6c1fb52837a3f2437ce
Theo Lima <theo.suporte@hotmail.com>
2021-07-09 20:46:43 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const selectedItem = document.querySelector('.tasksToDo');
 const list = document.querySelector('#lista-tarefas');
 const addList = document.querySelector('#criar-tarefa');
 const addInput = document.querySelector('#texto-tarefa');
@@ -6,24 +5,35 @@ const addInput = document.querySelector('#texto-tarefa');
 const createTask = () => {
   const itemList = document.createElement('li');
   itemList.innerText = addInput.value;
-  itemList.style.fontSize = '17px';
+  itemList.style.fontSize = '30px';
   itemList.classList.add('tasksToDo');
   if (addInput.value !== '') list.appendChild(itemList);
   addInput.value = '';
 };
 
-function bcgrdColor(event) {
+const addTask = () => {
+  addList.addEventListener('click', createTask);
+};
+
+const bcgrdColor = (event) => {
   const tasks = document.getElementsByClassName('tasksToDo');
   for (let i = 0; i < tasks.length; i += 1) {
     tasks[i].style.backgroundColor = '';
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
+};
 list.addEventListener('click', bcgrdColor);
 
-const addTask = () => {
-  addList.addEventListener('click', createTask);
+const lineThrough = (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 };
+list.addEventListener('dblclick', lineThrough);
+
+list.addEventListener('mousedown', (e) => { e.preventDefault(); }, false);
 
 createTask();
 addTask();
