fc6d4275fddb4d24568b2391aa94a1f04014ee9b
Thyara <thyaranonato@outlook.com>
2021-07-09 16:28:56 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -7,14 +7,30 @@ const inputTarefas = document.querySelector('#texto-tarefa');
 function criarItemTarefa() {
   const criarItem = document.createElement('li');
   const textoItem = inputTarefas.value;
-  
+
   criarItem.innerHTML = textoItem;
+  criarItem.className = 'li-tarefas';
   listaOrdenada.appendChild(criarItem);
   inputTarefas.value = '';
 }
 botaoTarefas.addEventListener('click', criarItemTarefa);
 
-// // Requisito #7
-listaOrdenada.addEventListener('click', function(event) {
-  event.target.style.backgroundColor = 'rgb(128,128,128)';
-})
+// Requisito #7
+// listaOrdenada.addEventListener('click', function (event) {
+//   event.target.style.backgroundColor = 'rgb(128,128,128)';
+// });
+function selecionaItem() {
+  listaOrdenada.addEventListener('click', function (event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  });
+}
+selecionaItem();
+
+function removeSelecao(event) {
+  const arrayLi = document.getElementsByClassName('li-tarefas');
+  for (let index = 0; index < arrayLi.length; index += 1) {
+    arrayLi[index].style.backgroundColor = '';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaOrdenada.addEventListener('click', removeSelecao);
