a75a16cfd52375593018d045708377ed702684f2
Vanim <vanimouzad123@gmail.com>
2021-07-09 16:02:17 -0300

    Requisito 7 e 8 - logica arrumada

--- script.js
+++ script.js
@@ -5,22 +5,18 @@ window.onload = function () {
 
     buttonAdd.addEventListener('click', function () {
         let li = document.createElement('li')
+
+        li.addEventListener('click', function() {
+            for (element of listaOrdenada.children) {
+                if (element.style.backgroundColor = 'rgb(128, 128, 128)') {
+                    element.style.backgroundColor = 'white';
+                }
+                li.style.backgroundColor = 'rgb(128, 128, 128)';
+            }
+        })
+
         li.innerText = input.value;
         listaOrdenada.appendChild(li);
         input.value = '';
     })
-    
-    listaOrdenada.addEventListener('click', function(event) {
-
-        for (let element of listaOrdenada.children) {
-            if (element.style.backgroundColor === 'rgb(128, 128, 128)') {
-                element.style.backgroundColor = 'white';
-            }
-        }
-
-        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    })
-
-
 }
-
