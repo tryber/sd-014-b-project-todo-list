84d7f1c244a19f8df727629112ec4f704c103fb8
belaguarino <izabelaguarino@gmail.com>
2021-07-09 14:26:05 -0300

    Função criar

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const listaOrdenada = document.getElementById('lista-tarefas');
 const string = '';
 
-const btnEnviar = document.getElementById('criar-tarefa');
-const novaTarefa = document.getElementById('texto-tarefa');
-const btnApagar = document.getElementById('apaga-tudo');
-const btnMoverCima = document.getElementById('mover-cima');
-const btnMoverBaixo = document.getElementById('mover-baixo');
-const btnSalvar = document.getElementById('salvar-tarefas');
-const btnRemoveSelecao = document.getElementById('remover-selecionado');
-const btnRemover = document.getElementById('remover-finalizados');
-const selecao = document.querySelectorAll('.selecionado');
+
+window.onload = function () {
+    const btnEnviar = document.getElementById('criar-tarefa');
+    const novaTarefa = document.getElementById('texto-tarefa');
+    const btnApagar = document.getElementById('apaga-tudo');
+    const btnMoverCima = document.getElementById('mover-cima');
+    const btnMoverBaixo = document.getElementById('mover-baixo');
+    const btnSalvar = document.getElementById('salvar-tarefas');
+    const btnRemoveSelecao = document.getElementById('remover-selecionado');
+    const btnRemover = document.getElementById('remover-finalizados');
+    const selecao = document.querySelectorAll('.selecionado');
+
+      // Função Criar
+  btnEnviar.onclick = function () {
+    if (novaTarefa.value.length > 0) {
+      const lista = document.createElement('li');
+      lista.innerHTML += novaTarefa.value;
+      listaOrdenada.appendChild(lista);
+      novaTarefa.value = string;
+    } else {
+      alert('Você precisa digitar uma tarefa!');
+    }
+  };
+
+  
+}  
