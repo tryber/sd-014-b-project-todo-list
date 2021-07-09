16c1f50d808ace0fc9e4b9e35bb7b3779d6d2e85
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-07-09 15:34:20 -0300

    Iiciado cogio JS correçoes de Style e tentativa de correçao do 1° requisito.

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const input = document.querySelector('#texto-tarefa');
+  const btnAddList = document.querySelector('#criar-tarefa');
+  let todoList = document.querySelector('#lista-tarefas')
+
+  
+
+  btnAddList.addEventListener('click' functionAdd);
+}
