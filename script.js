d03f712b0181db7b04fc2ff5ed4d9cfa9ef29240
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-09 16:53:38 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -11,10 +11,19 @@ function addTarefa() {
 }
 btnCriarTarefa.addEventListener('click', addTarefa);
 
+// Seleção de item na lista com preenchimento da linha
 lista.addEventListener('click', function(event) {
+  limparLinha();
   let tarefas = document.querySelectorAll('.item-tarefa');
-  for (let i = 0; i < tarefas.length; i += 1) {
-    let newColor = 'rgb(128, 128, 128)';
-    event.target.style.backgroundColor = newColor;
-  }
+  let limpar = event.target.innerText;
+  let newColor = 'rgb(128, 128, 128)';
+  event.target.style.backgroundColor = newColor;
 });
+
+function limparLinha() {
+  const tarefas = document.querySelectorAll('.item-tarefa');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    let taskList = tarefas[i].innerText;
+    tarefas[i].style.backgroundColor = 'green';
+  }
+}
