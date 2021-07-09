0b2262627d704c6fa7750c038434e31570ce6cfc
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-09 16:48:15 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ const ol = document.getElementById('lista-tarefas'); // #5-
 
 // Requisito #7
 function backgroundList (event) {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    const evento = event.target;
+    // 1 se (sem cor)>pintar e apagar restante 2.se(cor) nada a fazer
+    const coloredItem = document.querySelector('.select');
+    if (coloredItem === null) {
+        evento.style.backgroundColor = 'rgb(128, 128, 128)';
+        evento.classList.add('select');
+    } else {
+        coloredItem.style.backgroundColor = '';
+        coloredItem.classList.remove('select');
+        evento.style.backgroundColor = 'rgb(128, 128, 128)';
+        evento.classList.add('select');
+    }
+
 }
 
 function listEvent () {
     const list = document.querySelectorAll('.item-lista');
     console.log(list);
     for (let index = 0; index < list.length; index += 1) {
-        list[index].addEventListener('click', backgroundList);
+        list[index].addEventListener('click', backgroundList); // [#7]adiciona eventos a li
     }
 }
 
