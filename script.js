d66c1f011c9d6e72888097ceb3c0b624ea781499
Renata <renatateixeira.est@gmail.com>
2021-07-09 18:38:02 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -7,13 +7,24 @@ let linha = document.querySelector('li');
 //Requisito 5 e 6:
 addBotao.addEventListener('click',function(){
   let li = document.createElement('li');
-  console.log(li);
+  //console.log(li);
   li.innerText = inputTarefa.value;
   listaOrdenada.appendChild(li); 
   inputTarefa.value = '';
 })
 
-//Requsito 7:
-listaOrdenada.addEventListener('click',function(event){
+//Requsito 7 e 8:
+function teste(){listaOrdenada.addEventListener('click',function(event){
+  let lista = document.querySelectorAll('#lista-tarefas>li')
+  console.log(lista.length);
+  for(let index=0; index<lista.length; index+=1){
+    let fundo = lista[index].style.backgroundColor;
+    console.log("fundo")
+    if(fundo === 'rgb(128, 128, 128)'){
+      lista[index].style.backgroundColor = 'white';
+    }
+  }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-})
+})}
+
+teste()
