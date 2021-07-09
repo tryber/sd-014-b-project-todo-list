97696c5b5e2e0e1dc89c3685c6a455db3e352327
brenoMatias <matiasbreno19@gmail.com>
2021-07-09 14:45:58 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,6 @@
+// Requisito 2
+const instructions = document.createElement('p');
+instructions.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+document.getElementById('header').appendChild(instructions);
+
+
