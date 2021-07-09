f3901396104d0bf794e7746993ebe6e46adda099
Victor Veloso <vitovls37@gmail.com>
2021-07-09 19:05:11 -0300

    Requisito 9 - Adicionando funcionalidade de marcar tarefa feita

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ let liLista = document.getElementsByClassName('li-lista');
 
 function changeColor(event){
   let selected = document.querySelector('.selected');
-
-
   if (selected){
     document.querySelector('.selected').classList.remove('selected')
-  }
-  event.target.classList.add('selected')
+}event.target.classList.add('selected')
+}
 
+
+function changeToComplete(event){
+  let complete = document.querySelector('.completed')
+  event.target.classList.add('completed')
+  if (complete){
+  complete.classList.remove('completed')
+  }
 }
 
 /* 6 - Ordene os itens da lista de tarefas por ordem de criação */
@@ -26,6 +31,7 @@ function addTarefa(){
   addLista.innerText = document.getElementById('texto-tarefa').value;
   document.getElementById('texto-tarefa').value = "";
   addLista.addEventListener('click', changeColor)
+  addLista.addEventListener('dblclick', changeToComplete)
 }
 
 
