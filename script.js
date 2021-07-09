55ef6ec3f6239f6c937797136c0f1886d479e260
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-09 15:29:23 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ const listContainer = document.querySelector('#list-container');
 const taskList = document.createElement('ol');
 taskList.id = 'lista-tarefas';
 listContainer.appendChild(taskList);
+
+// Cria o botão de criar tarefas
+const btnCreateTask = document.createElement('button');
+btnCreateTask.id = 'criar-tarefa';
+btnCreateTask.innerText = 'Adicionar';
+inputContainer.appendChild(btnCreateTask);
+
+// Função para criar e adicionar novas tarefas à lista ordenada
+function createNewTask() {
+  const inputText = inputTask.value;
+  const newTask = document.createElement('li');
+  newTask.innerHTML = inputText;
+  taskList.appendChild(newTask);
+  inputTask.value = '';
+}
+
+// Adiciona o evento de click ao botão de criar tarefas
+btnCreateTask.addEventListener('click', createNewTask);
