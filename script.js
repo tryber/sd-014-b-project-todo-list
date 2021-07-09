766f98c1945d990b2ba2e53f86ecd40bc7c2b1ab
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-09 17:20:25 -0300

    Requisito 09

--- script.js
+++ script.js
@@ -11,19 +11,28 @@ function addTarefa() {
 }
 btnCriarTarefa.addEventListener('click', addTarefa);
 
+function limparLinha() {
+  const tarefas = document.querySelectorAll('.item-tarefa');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    tarefas[i].style.backgroundColor = 'green';
+  }
+}
+
 // Seleção de item na lista com preenchimento da linha
-lista.addEventListener('click', function(event) {
+lista.addEventListener('click', function (event) {
   limparLinha();
   let tarefas = document.querySelectorAll('.item-tarefa');
-  let limpar = event.target.innerText;
   let newColor = 'rgb(128, 128, 128)';
   event.target.style.backgroundColor = newColor;
 });
 
-function limparLinha() {
-  const tarefas = document.querySelectorAll('.item-tarefa');
-  for (let i = 0; i < tarefas.length; i += 1) {
-    let taskList = tarefas[i].innerText;
-    tarefas[i].style.backgroundColor = 'green';
+function tarefaCompleta(event) {
+  let verificador = event.target.className;
+  console.log(verificador);
+  if (verificador === 'item-tarefa completed') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
   }
 }
+lista.addEventListener('dblclick', tarefaCompleta);
