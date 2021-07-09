3bb3d4d00837789a3d7d98d3f53b3f17df3e27a7
Lucas Accurcio <lucas.accurcio@gmail.com>
2021-07-09 15:59:20 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const input = document.querySelector('#texto-tarefa');
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 const lista = document.querySelector('#lista-tarefas');
-console.log(lista);
+
 function addTarefa() {
   let itemTarefa = document.createElement('li');
+  itemTarefa.classList.add('item-tarefa');
   itemTarefa.innerText = input.value;
-  console.log(itemTarefa);
   lista.appendChild(itemTarefa);
   input.value = '';
 }
 btnCriarTarefa.addEventListener('click', addTarefa);
+
+lista.addEventListener('click', function(event) {
+  let tarefas = document.querySelectorAll('.item-tarefa');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    let newColor = 'rgb(128, 128, 128)';
+    event.target.style.backgroundColor = newColor;
+  }
+});
