6eedbdc46cd8361ac895f9555221f5fca389e7b7
Guilherme <guimfontes7@gmail.com>
2021-07-09 16:37:11 -0300

    Adiciona função que permite aplicar realce à tarefa selecionada

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 window.onload = function() {
-    let addTask = document.getElementById('criar-tarefa');
-    addTask.addEventListener('click', addTarefa);
+  let addTask = document.getElementById('criar-tarefa');
+  addTask.addEventListener('click', addTarefa);
 
-    function addTarefa() {
-        let input = document.getElementById('texto-tarefa');
-        let newTask = document.createElement('li');
-        let position = document.getElementById('lista-tarefas');
+  function addTarefa() {
+    let input = document.getElementById('texto-tarefa');
+    let newTask = document.createElement('li');
+    let position = document.getElementById('lista-tarefas');
 
-        newTask.innerText = input.value;
-        position.appendChild(newTask)
+    newTask.className = 'tarefa';
+    newTask.innerText = input.value;
+    newTask.addEventListener('click', highlight);
+    position.appendChild(newTask);
+  }
+
+  function highlight(selection) {
+    let selectionedTask = document.querySelectorAll('.selected');
+    if (selectionedTask.length !== 0) { 
+    selectionedTask[0].classList.remove('selected');
     }
+    selection.target.classList.add('selected');
+  }
 }
