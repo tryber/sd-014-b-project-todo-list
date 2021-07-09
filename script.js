782f37ab2ee8e63251d0eb2e9c2680316da37c96
andremorsch <andremorsch37@gmail.com>
2021-07-09 17:58:08 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let tarefa = document.querySelector("#texto-tarefa")
 let listaTarefa = document.querySelector('#lista-tarefas')
 let botao = document.querySelector('#criar-tarefa')
 
+//adicona os itens nas li da lista ordenada
 function adicionarTarefa() {
     let novaTarefa = document.createElement('li')
     novaTarefa.innerText =  tarefa.value
@@ -10,3 +11,14 @@ function adicionarTarefa() {
     tarefa.value = ""
 }
 botao.addEventListener('click', adicionarTarefa)
+
+//trocar cor da tarefa selecionada - sendo apenas 1
+function alteraFundo(event) {
+    let lista = document.querySelectorAll('li')
+    for (i = 0; i < lista.length; i +=1) {
+        lista[i].removeAttribute('style')
+    }
+    event.target.style.backgroundColor = 'rgb(128,128,128)'
+}
+
+listaTarefa.addEventListener('click', alteraFundo)
