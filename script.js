100b452911445b53067bcf8c33884bf4b97a8c6f
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-07-09 17:53:03 -0300

    Atende 10° requisito.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   const input = document.querySelector('#texto-tarefa');
   const btnAddList = document.querySelector('#criar-tarefa');
   const todoList = document.querySelector('#lista-tarefas');
+  const btnApagaTudo = document.querySelector('#apaga-tudo');
   function selectItem(event) {
     const itens = todoList.children;
     for (let index = 0; index < itens.length; index += 1) {
@@ -31,5 +32,13 @@ window.onload = function () {
     item.addEventListener('click', selectItem);
     item.addEventListener('dblclick', taskCompleted);
   }
+  function clearList() {
+    var fullList = document.querySelector('#lista-tarefas');
+    console.log(fullList);
+    while (fullList.firstChild) {
+      fullList.removeChild(fullList.firstChild);
+    }
+  }
   btnAddList.addEventListener('click', addList);
+  btnApagaTudo.addEventListener('click', clearList)
 };
