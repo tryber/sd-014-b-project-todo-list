27fbd4d5702ba148e434038f4715e01de6e35ad1
dhenycruz <dheniarley.ds@gmail.com>
2021-07-09 18:17:00 -0300

    Requisito #7.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 // Script JS.
+function changeBackground(li) {
+  const oldSelectedLi = document.querySelector('.li-selected');
+  if (oldSelectedLi !== null) {
+    oldSelectedLi.className = 'li-tarefa';
+  }
+  const liSelect = li;
+  liSelect.className = 'li-selected';
+}
+
 function selectedTarefa() {
   const conteudo = document.querySelector('#texto-tarefa').value;
   if (conteudo !== '') {
@@ -6,11 +15,14 @@ function selectedTarefa() {
     const li = document.createElement('li');
     li.innerText = conteudo;
     olPai.appendChild(li);
+    li.className = 'li-tarefa';
     document.querySelector('#texto-tarefa').value = '';
+    li.addEventListener('click', function () { changeBackground(li); });
   } else {
     alert('Não foi digitada nenhuma tarefa!');
   }
 }
+
 const button = document.querySelector('#criar-tarefa');
 
 button.addEventListener('click', selectedTarefa);
