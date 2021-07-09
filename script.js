776bef34d57ff5792a771c2afa9d5905c5b2a3f1
jarana-2020 <julioaranabr@yahoo.com.br>
2021-07-09 15:16:50 -0300

    Requisito #5

---
+++ script.js
@@ -0,0 +1,12 @@
+function addTask() {
+  const getOl = document.querySelector('#lista-tarefas');
+  const getButtonTask = document.querySelector('#criar-tarefa');
+  const getInput = document.querySelector('#texto-tarefa');
+  // eslint-disable-next-line prefer-arrow-callback
+  getButtonTask.addEventListener('click', function () {
+    const createLi = document.createElement('li');
+    createLi.innerHTML = getInput.value;
+    getOl.appendChild(createLi);
+  });
+}
+addTask();
