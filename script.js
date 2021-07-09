8ed6450e2ab23298da5cf1fb2ae7525b7e278c95
brenoMatias <matiasbreno19@gmail.com>
2021-07-09 14:47:46 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ const instructions = document.createElement('p');
 instructions.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 document.getElementById('header').appendChild(instructions);
 
+// Requisito 3
 
+const input = document.querySelector('.input-task'); // seleciona o local de append child
+const inputTask = document.createElement('input');
+inputTask.type = 'text';
+inputTask.id = 'texto-tarefa';
+input.appendChild(inputTask);
+
+//
