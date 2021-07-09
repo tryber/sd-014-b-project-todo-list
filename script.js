4bc067c14e5c167eae622b3208c0d531be6f3121
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-09 18:34:18 -0300

    Correção requisito 5.

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 // um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
 
 function newTask() {
-  let taskList = document.querySelector('.lista-tarefas');
-  let inputTask = document.getElementById("texto-tarefa").value;
-  let task = document.createElement('li'); // Criar elemento de item listado (li) no document
+  const taskList = document.querySelector('.lista-tarefas');
+  const inputTask = document.getElementById('texto-tarefa').value;
+  const task = document.createElement('li'); // Criar elemento de item listado (li) no document
   task.innerText = inputTask; // Atribui valor inserido no input, ao elemento'task', tipo "li". que será listado na lista ordenada
   taskList.appendChild(task); //  Adiciona elemento task (tarefa do input) como elemento filho do elemento pai taskList
+  document.getElementById('texto-tarefa').value = '';
 }
