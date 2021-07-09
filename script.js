6496479fc24d9c75c2ea21f359c4c945740599ac
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-07-09 20:36:22 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let clickButton = document.querySelector('#criar-tarefa')
+clickButton.addEventListener('click', createNewTask);
 
+function createNewTask (){
+  let inputTask = document.querySelector('#texto-tarefa');
+  let taskList = document.querySelector('#lista-tarefas');
+  let createTask = document.createElement('li');
+  createTask.innerText = inputTask.value;
+  taskList.appendChild(createTask);
+  inputTask.value = '';
+
+}
