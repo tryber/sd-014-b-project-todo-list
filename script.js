4ca8ba9556cdd1e17898b8f509ec966a70b1e3d5
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-07-09 17:59:40 -0300

    Requisito #7 :rocket:

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function addTextRemove() {
   let getClick = document.getElementById('criar-tarefa');
   getClick.addEventListener('click', function () {
-    let getText = document.getElementById('texto-tarefa').value;
-    let getList = document.getElementById('lista-tarefas');
-    let li = document.createElement('li');
+    const getText = document.getElementById('texto-tarefa').value;
+    const getList = document.getElementById('lista-tarefas');
+    const li = document.createElement('li');
     li.innerText = getText;
     getList.appendChild(li);
+    getList.addEventListener('click', colorizeText);
     document.getElementById('texto-tarefa').value = '';
   });
 }
 addTextRemove();
-
-
+function colorizeText(event) {
+  event.target.classList.add('selectedItem');
+}
