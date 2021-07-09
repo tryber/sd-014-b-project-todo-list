2282cfef01d43abcf22492957e67643f92b1257f
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-07-09 14:56:15 -0300

    Requisito #5

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 let inputTarefa = document.getElementById('texto-tarefa');
 let botaoAdicionarTarefa = document.getElementById('criar-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas')
 
-inputTarefa.addEventListener('keyup', tarefaInserida);
+inputTarefa.addEventListener('keyup', tarefaDigitada);
 botaoAdicionarTarefa.addEventListener('click', adicionarTarefa);
 
-function tarefaInserida() {
+let tarefaParaInserir = '';
 
+function tarefaDigitada(event) {
+  tarefaParaInserir = event.target.value;
+}
+
+function apagarTextoInput() {
+  inputTarefa.value = '';
 }
 
 function adicionarTarefa() {
-
+  if (tarefaParaInserir === '') { 
+    return;
+  }
+  let itemListaTarefas = document.createElement('li');
+  itemListaTarefas.innerText = tarefaParaInserir;
+  listaTarefas.appendChild(itemListaTarefas);
+  apagarTextoInput();
 }
