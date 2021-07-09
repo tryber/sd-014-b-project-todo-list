391ef91704b7e9d8f00ead50314215a44c8af646
Mateus Gambaro <magambaro@hotmail.com>
2021-07-09 16:26:43 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function (){
     let getButton = document.querySelector('#criar-tarefa');
     let getList = document.querySelector('#lista-tarefas');
     let getInput = document.querySelector('#texto-tarefa');
+    let newTask = document.querySelector('.newTask');
 
     getButton.addEventListener('click', createNewTask);
 
@@ -11,9 +12,17 @@ window.onload = function (){
         listItem.className = 'newTask'
         listItem.innerHTML = getInput.value;
         getList.appendChild(listItem);
-        getInput.value = '';
+        getInput.value = ''; // retorna o input para um valor vazio.
     }
 
-  
+    function newColor(color){
+        color.target.setAttribute('style', 'background-color: rgb(128, 128, 128)'); // retirado de https://developer.mozilla.org/pt-BR/docs/Web/API/Element/setAttribute onde atribuo um novo valor para style background diretamente no meu arquivo html.
+    }
+
+    getList.addEventListener ('click', newColor);
 }
+
+
+
+
 
