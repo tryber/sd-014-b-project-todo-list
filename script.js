549d36a567ad54bf10d0083c6261d056985085d4
Levi Manoel <levimanoel.deob@gmail.br>
2021-07-09 15:39:30 -0300

    Requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,12 @@
+let listaTarefas = document.querySelector('#lista-tarefas');
+let botao = document.querySelector('#criar-tarefa');
+let tarefa = document.querySelector('#texto-tarefa');
+
+function adicionaTarefa () {
+  let aux = document.createElement('li');
+  aux.innerText = tarefa.value;
+  listaTarefas.appendChild(aux);
+  tarefa.value = '';
+}
+
+botao.addEventListener('click', adicionaTarefa);
