40c41757372c2f285d0ef631ec7ce53d1832b025
Guilherme Neri <guinsneri@gmail.com>
2021-07-09 20:12:10 -0300

    Desafio 6, 7

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
 let insert = document.getElementById('criar-tarefa');
 insert.addEventListener('click', function(){
     let li = document.createElement('li');
+    //li.id('tasks');
     li.innerText = document.getElementById('texto-tarefa').value;
     document.getElementById('lista-tarefas').appendChild(li);
+    task = document.getElementById('lista-tarefas').appendChild(li);
+    task.addEventListener('click', updateColor);
     document.getElementById('texto-tarefa').value = '';
 });
+
+function updateColor(event){
+    
+    event.target.classList.add('selected');
+    console.log(event.target);
+}
