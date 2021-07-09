a91e6e4ce0bcf6d6158e784b4a27a8cac3bf9829
Matheus Laurindo <matheus104521@gmail.com>
2021-07-09 17:02:31 -0300

    Conclui o desafio 9

--- script.js
+++ script.js
@@ -7,17 +7,22 @@ function createTask() {
     let listTask = document.querySelector('#lista-tarefas');
     let txtTask = document.querySelector('#texto-tarefa').value;
 
-    //Cria o elemento, adiciona o texto inserido no input
-    //Adiciona classe, evento de clique e adiciona no html
-    let itemTask = document.createElement('li');
-    itemTask.innerHTML = txtTask;
-    itemTask.className = 'tasks';
-    itemTask.addEventListener('click', changeColor);
+    if (txtTask.length === 0) { 
+        alert('[ERRO] Campo vazio!');
+    } else {
+        //Cria o elemento, adiciona o texto inserido no input
+        //Adiciona classe, evento de clique e adiciona no html
+        let itemTask = document.createElement('li');
+        itemTask.innerHTML = txtTask;
+        itemTask.className = 'tasks';
+        itemTask.addEventListener('click', changeColor);
+        itemTask.addEventListener('dblclick', taskCompleted);
 
-    listTask.appendChild(itemTask);
+        listTask.appendChild(itemTask);
 
-    //Seta o valor do input como vazio
-    document.querySelector('#texto-tarefa').value = '';
+        //Seta o valor do input como vazio
+        document.querySelector('#texto-tarefa').value = '';
+    }
 }
 
 function changeColor(evento) {
@@ -34,3 +39,14 @@ function changeColor(evento) {
         itemClicked.style.backgroundColor = 'rgb(128, 128, 128)';
     };  
 } 
+
+function taskCompleted(evento) {
+    let itemClicked = evento.target;
+    let searchClassCompleted = itemClicked.classList.contains('completed');
+
+    if (searchClassCompleted === true) {
+        itemClicked.classList.remove('completed')
+    } else {
+        itemClicked.classList.add('completed');
+    }
+}
