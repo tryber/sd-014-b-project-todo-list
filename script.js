2485de8c91929354fcdbca5d9eecc60db237a340
samsantosb <samsantosb@outlook.com>
2021-07-09 15:33:47 -0300

    riscos

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const q = document.querySelector.bind(document);
-
-const color = (li) => {
-  li.addEventListener('click', (event) => {
-    if (q('.color')) q('.color').classList.remove('color');
-    event.target.classList.add('color');
+//.completed
+const grey = (li) => {
+  li.addEventListener('click', (e) => {
+    if (q('.grey')) q('.grey').classList.remove('grey');
+    e.target.classList.add('grey');
   });
 };
+
+const completed = (li) =>
+  li.addEventListener('dblclick', (e) => {
+    e.target.classList.add('completed');
+  });
+
 const clear = () => (q('#lista-tarefas').innerHTML = '');
-const btn = () => q('#apaga-tudo').addEventListener('click', clear);
-const element = () => {
-  newt = document.createElement('li');
-  color(newt);
-  newt.innerText = q('#texto-tarefa').value;
-  q('#lista-tarefas').appendChild(newt);
+const bt = () => q('#apaga-tudo').addEventListener('click', clear);
+
+const todo = () => {
+  const newI = document.createElement('li');
+  grey(newI);
+  completed(newI);
+  newI.innerText = q('#texto-tarefa').value;
+  q('#lista-tarefas').appendChild(newI);
   q('#texto-tarefa').value = null;
 };
-q('#criar-tarefa').addEventListener('click', element);
-btn();
+
+q('#criar-tarefa').addEventListener('click', todo);
+
+bt();
+completed();
