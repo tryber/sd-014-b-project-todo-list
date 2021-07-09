8727b80d9444d715091edd1d536a1e44cd4cdfc6
lucas <mr.pine97@gmail.com>
2021-07-09 14:36:32 -0300

    Requisito #2

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ window.onload = function() {
     header.innerText = 'Minha Lista de Tarefas';
     bodySelection.appendChild(header);
   }
+  function paragraphCreation() {
+    let bodySelection = document.querySelector('body');
+    let pCreation = document.createElement('p');
+    pCreation.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+    pCreation.id = 'funcionamento';
+    bodySelection.appendChild(pCreation)
+  }
 
   headerCreation();
+  paragraphCreation();
 
 }
