48c98270c57dff1461a355beb72c3f8c5360d346
Denis Espanhol <denisgfespanhol@gmail.com>
2021-07-09 15:41:52 -0300

    Quinto requisito do Project To Do List.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const takeButton = document.querySelector('#criar-tarefa');
+const takeLi = document.querySelector('#lista-tarefas');
+const takeInput = document.querySelector('#texto-tarefa');
 
+function addLi() {
+  const createLi = document.createElement('li');
+  takeLi.appendChild(createLi).innerText = takeInput.value;
+  takeInput.value = '';
+}
+
+takeButton.addEventListener('click', addLi);
