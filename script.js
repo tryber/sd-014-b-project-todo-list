58167949c2899a573299951bf7b3f7fbba6040aa
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-09 15:43:14 -0300

    Cumpre o requisito 5 :rocket:

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const createTodoButton = document.getElementById('criar-tarefa');
+const todoList = document.getElementById('lista-tarefas');
+const inputTodo = document.getElementById('texto-tarefa');
 
+createTodoButton.addEventListener('click', () => {
+  const item = document.createElement('li');
+  item.innerText = inputTodo.value;
+  item.className = 'item';
+  todoList.appendChild(item);
+  inputTodo.value = '';
+});
