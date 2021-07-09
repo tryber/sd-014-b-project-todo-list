96d8c62cf724deb6991609519f3568e1ef5cf3e6
viniciussantana <viniciusantana@yahoo.com.br>
2021-07-09 17:57:17 -0300

    Requisito #9

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const addButton = document.getElementById('criar-tarefa'); // #5-
 const input = document.getElementById('texto-tarefa'); // #5-
 const ol = document.getElementById('lista-tarefas'); // #5-
 
-// Requisito #7
+// Requisito #7 #8
 function backgroundList (event) {
     const evento = event.target;
-    // 1 se (sem cor)>pintar e apagar restante 2.se(cor) nada a fazer
+    // 1 se (nenhum selecionado)> selecionar e pintar [2] senão > apagar selecionado e fazer o 1
     const coloredItem = document.querySelector('.select');
     if (coloredItem === null) {
         evento.style.backgroundColor = 'rgb(128, 128, 128)';
@@ -20,11 +20,25 @@ function backgroundList (event) {
 
 }
 
+function completed(event) {
+    const evento = event.target;
+    // 1 se (conter classe completed )> remover completed [2] senão > adicionar completed
+    const completedItem = evento.classList.contains('completed');
+    console.log(completedItem);
+    if (completedItem) {
+        evento.classList.remove('completed');
+    } else {
+        evento.classList.add('completed');
+    }
+
+}
+
 function listEvent () {
     const list = document.querySelectorAll('.item-lista');
     console.log(list);
     for (let index = 0; index < list.length; index += 1) {
         list[index].addEventListener('click', backgroundList); // [#7]adiciona eventos a li
+        list[index].addEventListener('dblclick', completed);
     }
 }
 
