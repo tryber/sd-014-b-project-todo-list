5bd1a4885d184ce3c3eface0425523b6986df3e3
Victor Martins <victor.martins.duarte@gmail.com>
2021-07-09 18:30:26 -0300

    Requisito número 8

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-let olPath = document.querySelector('#lista-tarefas');
-let inputPath = document.querySelector('#texto-tarefa');
-let liTarefaPath = document.getElementsByClassName('liTarefa');
+const olPath = document.querySelector('#lista-tarefas');
+const inputPath = document.querySelector('#texto-tarefa');
 
-// * Requisitos - 5, 6 e 7
+// * Requisitos - 5, 6
+
 document.querySelector('#criar-tarefa').addEventListener('click', function () {
   let tarefaLi = document.createElement('li');
   if (inputPath.value != '') {
     olPath.appendChild(tarefaLi).innerText = inputPath.value;
+  } else {
+    alert('Adicione uma tarefa!');
   }
-  tarefaLi.className = 'liTarefa';
   inputPath.value = '';
 
-  tarefaLi.addEventListener('click', function() {
-    tarefaLi.style.backgroundColor = 'rgb(128,128,128)';
+  tarefaLi.addEventListener('click', function(event) {
+    let classSelected = document.querySelector('.selected');
+      if (classSelected) {
+        classSelected.classList.remove('selected');
+      }
+        event.target.classList.add('selected');
   })
 })
+// 1. Bernardo Salgueiro salvador do requisito 7!!! Atribuir o evento logo após criar a Li, resolve a questão de não conseguir percorrer o array vazio de li's.
+// 2. Alternativa do Luiz Gustavo
+
+// * Requisito - 8
+// * O Eli dos Santos me deu uma luz, me lembrando que existia a propriedade 'children', que traz info atualizada das child's/li's
