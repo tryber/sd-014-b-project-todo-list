1dfa027bba795589b3ceca67eb209f4269d66270
Victor Veloso <vitovls37@gmail.com>
2021-07-09 17:11:49 -0300

    Requesito #6 - Ordenar lista por ordem de criação

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-
+let listaOrdenada;
 
 /*  5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo */
+
+/* 6 - Ordene os itens da lista de tarefas por ordem de criação */
+
 function addTarefa(){
-  let addLista = document.createElement('li')
-  document.getElementById('lista-tarefas').appendChild(addLista)
-  addLista.innerText = document.getElementById('texto-tarefa').value
-  document.getElementById('texto-tarefa').value = ""
+  let addLista = document.createElement('li');
+  addLista.classList.add('li-lista');
+  document.getElementById('lista-tarefas').appendChild(addLista);
+  addLista.innerText = document.getElementById('texto-tarefa').value;
+  document.getElementById('texto-tarefa').value = "";
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', addTarefa)
+
 
