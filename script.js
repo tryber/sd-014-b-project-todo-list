972db750426ec5f157bfe11f9d9bcd6d716b1637
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-09 18:33:32 -0300

    Requisito 9 ok

--- script.js
+++ script.js
@@ -7,23 +7,40 @@ const input = document.getElementById('texto-tarefa')
 
 button.addEventListener('click',criarTarefas)
 
+function linhaRiscada (event){
+    let completed = document.querySelector('.completed')
+    if (completed){
+        completed.classList.remove('completed');
+    }else {
+        event.target.classList.add('completed');
+    }
+
+}
+
 
 function trocarCor(event){
     let selected = document.querySelector('.selected')
     if (selected){
         selected.classList.remove('selected');
     }
-    event.target.classList.add('selected');
-}
+        event.target.classList.add('selected');
     
+
+    //event.target.classList.add('selected');
+}
 
 
 function criarTarefas(){
+    if(input.value != ''){
     let li = document.createElement('li');
     li.innerText = input.value;
     listaTarefas.appendChild(li);
-    clear = document.getElementById('texto-tarefa').value = ""
+    let clear = document.getElementById('texto-tarefa').value = ""
     li.addEventListener('click',trocarCor);
+    li.addEventListener('dblclick',linhaRiscada);
+    }else {
+        alert('Coloque sua tarefa');
+    }
   // Trocar a cor de fundo 
 }
 
