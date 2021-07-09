610d103eac5b29d7f62c165a70461989160d8b12
Leomar Linhares <leomar_linhares@hotmail.com>
2021-07-09 16:26:36 -0300

    Cumpre os requisitos 7 e 8 :rocket:

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ const createTodoButton = document.getElementById('criar-tarefa');
 const todoList = document.getElementById('lista-tarefas');
 const inputTodo = document.getElementById('texto-tarefa');
 
+function selectItem(event) {
+  if (document.querySelector('.selected') !== null) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
+}
+
 createTodoButton.addEventListener('click', () => {
   const item = document.createElement('li');
   item.innerText = inputTodo.value;
   item.className = 'item';
+  item.addEventListener('click', selectItem);
   todoList.appendChild(item);
   inputTodo.value = '';
 });
