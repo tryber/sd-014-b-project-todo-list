3926069a5c9a81cef32d06c7ef9176854e2cea4a
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-09 20:01:29 -0300

    Requisito 5.

--- script.js
+++ script.js
@@ -2,17 +2,16 @@
 // um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
 
 function newTask() {
-  const taskList = document.querySelector('.lista-tarefas');
-  const inputTask = document.getElementById('texto-tarefa').value;
+  const taskList = document.querySelector('#lista-tarefas'); // Atribuir elemento de id lista-tarefas à variável taskList
   const task = document.createElement('li'); // Criar elemento de item listado (li) no document
-
-  task.innerText = inputTask; // Atribui valor inserido no input, ao elemento'task', tipo "li". que será listado na lista ordenada
   taskList.appendChild(task); //  Adiciona elemento task (tarefa do input) como elemento filho do elemento pai taskList
 
-  document.getElementById('texto-tarefa').value = '';
-  return inputTask;
+  const inputTask = document.getElementById('texto-tarefa').value; // Atribuir valor do input à variável inputTask
+  task.innerText = inputTask; // Atribuir valor inserido no input, ao elemento 'task', tipo "li". que será listado na lista ordenada
+
+  document.getElementById('texto-tarefa').value = ''; // Limpar texto do input
 }
 
-console.log(newTask());
-
 // 10 - Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista
+
+function removeAll() {};
