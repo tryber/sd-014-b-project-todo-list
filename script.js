1a3da0d3feaea31f410e8b51d105b619ca487e62
eli <leonardohcds@gmail.com>
2021-07-09 20:17:22 -0300

    finishes requirement 9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const root = document.querySelector('.root');
 const toDoList = document.querySelector('#lista-tarefas');
 const input = document.querySelector('#texto-tarefa');
+const taskDone = document.querySelector('.completed');
 
 let button = document.createElement('button');
 button.id = 'criar-tarefa';
@@ -23,3 +24,10 @@ toDoList.addEventListener('click', function (event) {
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 });
 
+toDoList.addEventListener('dblclick', function (event) {
+    if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+    } else {
+    event.target.classList.add('completed');
+    }
+});
