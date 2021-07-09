2163cbbf923c24762b6a1ddcd29a135fe3a08904
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-09 15:00:39 -0300

    Requisito 4

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const inputTask = document.createElement('input');
 inputTask.id = 'texto-tarefa';
 inputTask.type = 'text';
 inputContainer.appendChild(inputTask);
+
+// Cria a lista ordenada
+const listContainer = document.querySelector('#list-container');
+const taskList = document.createElement('ol');
+taskList.id = 'lista-tarefas';
+listContainer.appendChild(taskList);
