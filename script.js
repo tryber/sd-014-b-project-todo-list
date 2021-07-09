8557254cd73a2f7c526e104a9d97ef34b7b1e4da
enzo <enzo@pop-os.localdomain>
2021-07-09 16:26:43 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const button = document.querySelector('#criar-tarefa');
+
 button.addEventListener('click', function(){
-    const list = document.querySelector('#lista-tarefas');
-    const input = document.querySelector('#texto-tarefa');
-    const li = document.createElement('li');
-    list.appendChild(li);
-    li.innerText = input.value;
-    input.value = ""
+  const list = document.querySelector('#lista-tarefas');
+  const input = document.querySelector('#texto-tarefa');
+  const li = document.createElement('li');
+  list.appendChild(li);
+  li.innerText = input.value;
+  input.value = ""
+  li.addEventListener('click', changeClass)
 })
+
+function changeClass(event) {
+  const arrayLi = document.getElementsByTagName('li');
+  for (let index = 0; index < arrayLi.length; index += 1){
+    arrayLi[index].classList.remove('selectedTask');
+    event.target.classList.add('selectedTask')
+  }
+}
+
