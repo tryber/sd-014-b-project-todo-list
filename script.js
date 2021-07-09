c0143db25f1c3f6ad75be1a2a6fa4ffd18f41bc6
Lombeira <glomenha@gmail.com>
2021-07-09 15:35:23 -0300

    Styling

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const selector = document.querySelector.bind(document);
 
-const todo = () => {
-  const newItem = document.createElement('li');
-  newItem.innerText = selector('#texto-tarefa').value;
-  selector('#lista-tarefas').appendChild(newItem);
+const color = (li) => {
+  li.addEventListener('click', (event) => {
+    if (selector('.color')) selector('.color').classList.remove('color');
+    event.target.classList.add('color');
+  });
 };
+
+const clear = () => {
+  selector('#lista-tarefas').innerHTML = '';
+};
+
+const ClearAll = () => selector('#apaga-tudo').addEventListener('click', clear);
+
+const todo = () => {
+  const newI = document.createElement('li');
+  color(newI);
+  newI.innerText = selector('#texto-tarefa').value;
+  selector('#lista-tarefas').appendChild(newI);
+  selector('#texto-tarefa').value = null;
+};
+
+selector('#criar-tarefa').addEventListener('click', todo);
+ClearAll();
