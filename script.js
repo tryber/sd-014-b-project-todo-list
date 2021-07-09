d9769f6fbb41c13145aa2c99ad402828de81be27
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-07-09 15:53:45 -0300

    Atende 5° e 6° requisito.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.onload = function () {
   const input = document.querySelector('#texto-tarefa');
   const btnAddList = document.querySelector('#criar-tarefa');
-  let todoList = document.querySelector('#lista-tarefas')
-
-  
-
-  btnAddList.addEventListener('click' functionAdd);
+  let todoList = document.querySelector('#lista-tarefas');
+  function addList() {
+    let item = document.createElement('li');
+    let inputValue = input.value;
+    item.innerText = inputValue;
+    todoList.appendChild(item);
+    input.value = '';
 }
+  btnAddList.addEventListener('click', addList);
+  
+}
+
