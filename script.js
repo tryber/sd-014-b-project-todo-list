1fc42b439d11f1bd99a5a36551d12963dec0d3c3
Diogo Fiuza <difipalm@gmail.com>
2021-07-09 16:43:50 -0300

    Requisito #5 e #6

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const button = document.querySelector('#criar-tarefa');
 
+button.addEventListener('click', () => {
+  const answer = document.querySelector('#texto-tarefa');
+  const task = document.createElement('li');
+  const list = document.querySelector('#lista-tarefas');
+  task.innerText = answer.value;
+  list.appendChild(task);
+  answer.value = '';
+});
