e52c7e06f9a73c39f90229741c028f45da3b07b0
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-07-09 17:42:11 -0300

    correção requisito07

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function changeColorToGray(color) {
- color.target.className = 'concluded';
+function changeColorToGray(event) {
+ event.target.classList.add('selected');
+ let selected = document.querySelectorAll('.selected');
+ for (let index = 0; index < selected.length; index += 1) {
+  if (selected.length > 1) {
+   selected[index].classList.remove('selected')
+   event.target.classList.add('selected');
+  }
+ }
 }
 
 function createListItem() {
@@ -7,14 +14,26 @@ function createListItem() {
  let input = document.getElementById('texto-tarefa');
  const ol = document.getElementById('lista-tarefas');
  button.addEventListener('click', function () {
-   let li = document.createElement('li');
-   li.addEventListener('click', changeColorToGray)
-   li.innerHTML = input.value;
-   ol.appendChild(li);
-   input.value = '';
+  let li = document.createElement('li');
+  li.className = "listItem";
+  li.innerHTML = input.value;
+  li.addEventListener('click', changeColorToGray)
+  ol.appendChild(li);
+  input.value = '';
  })
 }
 createListItem()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
