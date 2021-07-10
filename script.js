8c82dcd530c3f74648514cdb1360e378058c2f9c
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2021-07-10 09:21:07 -0300

    Requisito 8 - 10

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 window.onload = function() {
 
   const botaoCriarTarefa = document.getElementById('criar-tarefa');
+  const botaoApagarLista = document.getElementById('apaga-tudo');
+
   botaoCriarTarefa.addEventListener('click', addTarefa);
+  botaoApagarLista.addEventListener('click', clearList);
 
 }
 
@@ -12,11 +15,29 @@ function addTarefa() {
   let item = document.createElement('li');
   item.innerText = tarefa;
   item.addEventListener('click', markTask);
+  item.addEventListener('dblclick', completeTask);
   lista.appendChild(item);
   document.getElementById('texto-tarefa').value = '';
 
 }
 
 function markTask(event) {
+  
+  let items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].style.backgroundColor = 'transparent';
+  }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+
+}
+
+function completeTask(event) {
+  event.target.classList.toggle('completed');
+}
+
+function clearList() {
+  let items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].remove();
+  }
 }
