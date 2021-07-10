4712ecc0acfd6a07b7d22696e518201eff9d28eb
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-07-10 18:22:51 -0300

    #7 e #8 Requisito

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-
 function createListItem(text) {
-  let listItem = document.createElement('li');
+  const listItem = document.createElement('li');
   listItem.className = 'listItem';
   listItem.innerText = text;
-  return  listItem;
+  return listItem;
 }
 
 function textListItem() {
-  let currentInput = document.getElementById('texto-tarefa').value;
+  const currentInput = document.getElementById('texto-tarefa').value;
   return currentInput;
 }
 
+function addIdStyle() {
+  const listedItem = document.querySelectorAll('.listItem');
+  for (const index of listedItem) {
+    index.addEventListener('click', () => {
+      for (const indexIn of listedItem) { indexIn.id = ''; }
+      index.id = 'selected';
+    });
+  }
+}
+
 function addListItem() {
-  let addItem  = document.getElementById('criar-tarefa');
-  addItem.addEventListener('click', function(){
-    let ordList = document.getElementById('lista-tarefas');
-    let currentInput = document.getElementById('texto-tarefa')
-    let currentItem = createListItem(textListItem());
+  const addItem = document.getElementById('criar-tarefa');
+  addItem.addEventListener('click', () => {
+    const ordList = document.getElementById('lista-tarefas');
+    const currentInput = document.getElementById('texto-tarefa');
+    const currentItem = createListItem(textListItem());
     ordList.appendChild(currentItem);
     currentInput.value = '';
-  })
+    addIdStyle();
+  });
 }
 
-window.onload = function() {
-
+window.onload = () => {
   addListItem();
-
-}
+};
