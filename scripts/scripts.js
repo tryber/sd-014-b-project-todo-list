98ffda9f682eb39da906b2d2966fd43b3d679ddc
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-07-09 22:31:05 -0300

    Adiciona um botão com id='criar-tarefa'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+// [Requisito 5]
+function addTaskToList() {
+  const addTask = document.querySelector('#criar-tarefa');
+  addTask.addEventListener('click', () => {
+    const task = document.querySelector('#texto-tarefa').value;
+    const item = document.createElement('li');
+    item.innerHTML = task;
+    document.querySelector('#lista-tarefas').appendChild(item);
+  });
+}
+
+addTaskToList();
