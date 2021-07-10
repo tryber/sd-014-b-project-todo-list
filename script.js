7b2f23cf049e931dc9a2e5d7c50bfeae2e9a1573
Leandro <leandro2903.lan@gmail.com>
2021-07-10 18:26:14 -0300

    alterando a funçao de colocar fundo cinza (selecinar) para selecionar apenas 1 elemento por vez

--- script.js
+++ script.js
@@ -2,28 +2,35 @@
 const listaOrdenada = document.getElementById('lista-tarefas');
 const inputTarefa = document.getElementById('texto-tarefa');
 const buttonCriar = document.getElementById('criar-tarefa');
-
 // lista de tarefas que serão adicionadas
 // armazenar o texto captrado como tarefa numa variavel
 let list;
-
-// função para alterar a cor de fundo
-
+let lists = [];
+console.log(lists);
+// função para alterar a cor de fundo (selecionar a tarefa)
 function listBackground(event) { // seguindo a recomendação do Linter a função tem que ficar aqui em cima antes dela ser invocada na criação das li
-  event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
+  const tarefaCinza = document.getElementsByClassName('cinza');
+  for(let item of tarefaCinza) {
+    item.classList.remove('cinza');
+  }
+    event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
 }
+
 
 function capturarTarefa() {
   // capturar e guardar, na variavel, o value (texto) digitado
   const tarefa = inputTarefa.value;
   list = tarefa;
+  lists.push(tarefa);
   inputTarefa.value = null; // atribuindo o valor null para limpar o campo de digitação
   const liTarefa = document.createElement('li');
   liTarefa.className = 'atividade-adicionada';
-  liTarefa.innerText = list;
+  liTarefa.innerText = tarefa;
   liTarefa.addEventListener('click', listBackground);
-  console.log(liTarefa);
   listaOrdenada.appendChild(liTarefa);
 }
 // evento no botão que ao receber um click ele execulta a função addTarefas
 buttonCriar.addEventListener('click', capturarTarefa);
+
+// deixar só uma tarefa selecionada.
+
