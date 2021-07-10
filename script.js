b8b6aada4f6ba63ea2bb6b9f001f20c3c34ce837
Kauan Carvalho <kauancarvalho987@gmail.com>
2021-07-09 22:32:30 -0300

    requisito 12

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const ol = document.getElementById('lista-tarefas')
+
 function changeColorToGray(event) {
  event.target.classList.add('selected');
  let selected = document.querySelectorAll('.selected');
@@ -13,8 +15,8 @@ function completedListItem(event) {
  if (event.target.classList.contains('completed')) {
   event.target.classList.remove('completed')
  } else {
- event.target.classList.add('completed');
-}
+  event.target.classList.add('completed');
+ }
 }
 
 function createListItem() {
@@ -36,9 +38,9 @@ createListItem()
 function deleteListItems() {
  const deleteButton = document.getElementById('apaga-tudo');
  const ol = document.getElementById('lista-tarefas');
- deleteButton.addEventListener('click', function() {
+ deleteButton.addEventListener('click', function () {
   let listItems = document.querySelectorAll('.listItem');
-  for (let index = 0; index < listItems.length; index += 1){
+  for (let index = 0; index < listItems.length; index += 1) {
    ol.removeChild(listItems[index])
   }
  })
@@ -48,25 +50,37 @@ deleteListItems()
 function removeCompleted() {
  const removeCompletedButton = document.getElementById('remover-finalizados');
  const ol = document.getElementById('lista-tarefas');
- removeCompletedButton.addEventListener('click', function() {
+ removeCompletedButton.addEventListener('click', function () {
   let completedItems = document.querySelectorAll('.completed');
-  for (let index = 0; index < completedItems.length; index += 1){
+  for (let index = 0; index < completedItems.length; index += 1) {
    ol.removeChild(completedItems[index])
   }
  })
 }
 removeCompleted()
 
+function saveLocalStorage() {
+ const saveButton = document.getElementById('salvar-tarefas');
+ saveButton.addEventListener('click', function () {
+  const ol = document.getElementById('lista-tarefas');
+  let contentsListItem = ol.innerHTML;
+  localStorage.setItem('lista', JSON.stringify(contentsListItem))
+ })
+}
+saveLocalStorage()
 
-
-
-
-
-
-
-
-
-
+function recoverSave() {
+ if (window.localStorage.getItem('lista')) {
+  const save = JSON.parse(window.localStorage.getItem('lista'));
+  ol.innerHTML = save;
+  const listItems = document.querySelectorAll('.listItem');
+  for (let index = 0; index < listItems.length; index += 1){
+   listItems[index].addEventListener('click', changeColorToGray);
+   listItems[index].addEventListener('dblclick', completedListItem);
+  }
+ }
+}
+recoverSave()
 
 
 
