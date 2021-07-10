d8710caa8f898ee6d7bea575c4afc7cb94bfa7ee
Cezar <cezarguimaraes@gmail.com>
2021-07-10 13:22:38 -0300

    item 7 adicionado

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+let getButton = document.getElementById("criar-tarefa");
+let getOl = document.getElementById("lista-tarefas");
+let getInput = document.getElementById("texto-tarefa");
 
 
 
-function addTask(){
-    let getOl = document.getElementById("lista-tarefas");
-    let getInput = document.getElementById("texto-tarefa");
+function addTask() {
     let newLi = document.createElement("li");
     newLi.innerHTML = getInput.value;
+    newLi.classList.add("itens-lista");
     getOl.appendChild(newLi);
+    newLi.addEventListener("click", changeBkg);
     getInput.value = "";
-};
-let getButton = document.getElementById("criar-tarefa");
+    
+}
 getButton.addEventListener("click", addTask);
+
+function changeBkg(event){
+    event.target.style.backgroundColor = "green";
+}
+
+
+ 
+
+
+
+
+
+
 
