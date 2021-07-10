2bd7d01faa7d457e74d539824c88afe1f40d9566
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-07-10 17:00:32 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const clearAllButton = document.querySelector('#apaga-tudo');
 const allTasks = document.getElementsByClassName('task');
+const clearFinished = document.querySelector('#remover-finalizados');
 
 function createTask() {
   const createLi = document.createElement('li');
@@ -13,7 +14,6 @@ function createTask() {
 }
 
 function selectTask(event) {
-  // const allTasks = document.querySelectorAll('.task');
   for (let i = 0; i < allTasks.length; i += 1) {
     allTasks[i].classList.remove('selected');
     event.target.classList.add('selected');
@@ -28,10 +28,20 @@ function clearAll() {
   taskList.innerHTML = '';
 }
 
-taskList.addEventListener('click', selectTask);
+function clearDone() {
+  const completedLis = document.getElementsByClassName('completed');
+  console.log(completedLis);
+  for (let i = 0; i < completedLis.length; i += 0) {
+    completedLis[i].remove();
+  }
+}
 
 button.addEventListener('click', createTask);
+
+taskList.addEventListener('click', selectTask);
 
 clearAllButton.addEventListener('click', clearAll);
 
 taskList.addEventListener('dblclick', markCompleted);
+
+clearFinished.addEventListener('click', clearDone);
