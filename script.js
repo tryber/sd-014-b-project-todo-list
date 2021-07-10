37b539384523185de8aecdb120bff5ef1b2ae3cf
Leandro <leandro2903.lan@gmail.com>
2021-07-10 18:48:04 -0300

    funçao para adicionar e retirar uma clase com a propriedade para riscar o texto

--- script.js
+++ script.js
@@ -2,35 +2,32 @@
 const listaOrdenada = document.getElementById('lista-tarefas');
 const inputTarefa = document.getElementById('texto-tarefa');
 const buttonCriar = document.getElementById('criar-tarefa');
-// lista de tarefas que serão adicionadas
-// armazenar o texto captrado como tarefa numa variavel
-let list;
-let lists = [];
-console.log(lists);
+
 // função para alterar a cor de fundo (selecionar a tarefa)
 function listBackground(event) { // seguindo a recomendação do Linter a função tem que ficar aqui em cima antes dela ser invocada na criação das li
   const tarefaCinza = document.getElementsByClassName('cinza');
-  for(let item of tarefaCinza) {
+  for (const item of tarefaCinza) {
     item.classList.remove('cinza');
   }
-    event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
+  event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
 }
 
-
 function capturarTarefa() {
-  // capturar e guardar, na variavel, o value (texto) digitado
-  const tarefa = inputTarefa.value;
-  list = tarefa;
-  lists.push(tarefa);
+  const tarefa = inputTarefa.value;// capturar e guardar, na variavel, o value (texto) digitado
   inputTarefa.value = null; // atribuindo o valor null para limpar o campo de digitação
   const liTarefa = document.createElement('li');
   liTarefa.className = 'atividade-adicionada';
   liTarefa.innerText = tarefa;
   liTarefa.addEventListener('click', listBackground);
+  liTarefa.addEventListener('dblclick', completed);
   listaOrdenada.appendChild(liTarefa);
 }
 // evento no botão que ao receber um click ele execulta a função addTarefas
 buttonCriar.addEventListener('click', capturarTarefa);
 
-// deixar só uma tarefa selecionada.
-
+function completed(event) {
+  if(event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  }
+  event.target.classList.add('completed');
+}
