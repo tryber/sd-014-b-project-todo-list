9236949faf9b948edd39d6daffe61536d488904a
brunofaria2356 <contatobfimportados@gmail.com>
2021-07-10 20:23:15 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function adicionar() {
     let tar = document.getElementById('texto-tarefa').value;
     let lista = document.getElementById('lista-tarefas').innerHTML;
-    lista += "<li>" + tar + "</li>";
+    lista += "<li class='list' onclick='blk()'>" + tar + "</li>";
 
     document.getElementById('lista-tarefas').innerHTML = lista
     document.getElementById('texto-tarefa').value = ''
+}
+
+function blk() {
+    let allPixels = document.querySelectorAll('.list');
+
+    for (let index = 0; index < allPixels.length; index++) {
+        // allPixels[index].addEventListener('click', function() 
+        if (allPixels[index].style.backgroundColor != 'rgb(128, 128, 128)') {
+            allPixels[index].style.backgroundColor = 'rgb(128, 128, 128)';
+        };
+
+    }
 }
