cfe580cee8598955e73dbda12d041b45ba1c02fc
Israel S. Nazareth <israel.fab@hotmail.com>
2021-07-09 22:21:05 -0300

    Requisitos 07 e 08

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const button = document.querySelector('#criar-tarefa');
+const taskList = document.querySelector('#lista-tarefas')
+const item = document.querySelectorAll('li')
+const inputValue = document.querySelector('input');
 
 button.addEventListener('click', () => {
   const inputValue = document.querySelector('input');
   const listItem = document.createElement('li');
   const list = document.querySelector('#lista-tarefas');
+  listItem.classList.add('item');
   if (inputValue.value == false) {
     alert('Insira um valor, por favor.');
   } else {
@@ -11,4 +15,13 @@ button.addEventListener('click', () => {
     list.appendChild(listItem);
   }
   inputValue.value = '';
+  
+  listItem.addEventListener('click', () => {
+    for (let value of taskList.children) {
+      if (value.className = 'item selected') {
+        value.classList.remove('selected')
+      }
+      listItem.classList.add('selected')
+    };
+  });
 });
