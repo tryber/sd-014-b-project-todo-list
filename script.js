c10e948f9fce8540a74870a8537a72f2db97bfdb
vinicius-mns <vinicius_mns@hotmail.com>
2021-07-10 02:38:45 -0300

    css

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let botao = document.querySelector('#criar-tarefa').addEventListener ('click', c
 let tarefas = document.querySelector('#texto-tarefa') // caixa de texto onde se escreve a tarefa
 let lista = document.querySelector('#lista-tarefas') // lista ordenada (inicialmente vazia, sem <li>,)
 
+for (let index = 0; index < localStorage.length; index += 1) {
+    let li = document.createElement('li')
+    li.innerText = localStorage[index]
+    lista.appendChild(li)
+}
 function clique() {
    let li = document.createElement('li') // cria uma <li>
    li.innerText = tarefas.value // recupera o valor (txt) de `tarefas`[linha 2]
@@ -10,13 +15,12 @@ function clique() {
 }
 lista.addEventListener('click', function (event) { // cria o evento de click na `lista` [linha 3]
     for (let index = 0; index < lista.children.length; index += 1) {
-        lista.children[index].style.backgroundColor = 'white'
+        lista.children[index].style.backgroundColor = '#373737'
     } //ao clickar em qualquer item da lista, o for percorre todos os itens da lista e seta a cor do background como `white`
-    setTimeout(function () { event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    setTimeout(function () { event.target.style.backgroundColor = '#121212'
     },10) // setTimeout de 10ms ao clickar em um item da lista seta a cor do background como `rgb(128, 128, 128`
 })
-lista.addEventListener('dblclick', function (event) { // adiciona o evento double click
-    
+lista.addEventListener('dblclick', function (event) { // adiciona o evento double click   
     if (event.target.style.textDecoration === 'line-through solid rgb(0, 0, 0)'){
         event.target.style.textDecoration = ''
         event.target.classList.remove ('completed')
@@ -38,4 +42,11 @@ document.querySelector('#remover-finalizados').addEventListener ('click', functi
         lista.removeChild(document.querySelector('.completed'))
     }
 })
+
+let save = document.querySelector('#salvar-tarefas').addEventListener('click', function() {
+    for (let index = 0; index < lista.children.length; index += 1) {
+        localStorage.setItem(index,lista.children[index].innerText)
+    }
+})
+
     
