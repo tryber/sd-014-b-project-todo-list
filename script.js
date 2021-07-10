3c4e6e151d50bddc50c0512e915a9273688a61ed
Theo Lima <theo.suporte@hotmail.com>
2021-07-09 22:43:58 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const list = document.querySelector('#lista-tarefas');
 const addList = document.querySelector('#criar-tarefa');
 const addInput = document.querySelector('#texto-tarefa');
+const clearButton = document.querySelector('#apaga-tudo');
 
+// Cria os elementos da lista de tarefas!
 const createTask = () => {
   const itemList = document.createElement('li');
   itemList.innerText = addInput.value;
@@ -10,11 +12,9 @@ const createTask = () => {
   if (addInput.value !== '') list.appendChild(itemList);
   addInput.value = '';
 };
+addList.addEventListener('click', createTask);
 
-const addTask = () => {
-  addList.addEventListener('click', createTask);
-};
-
+// Adiciona o background cinza ao clicar no elemento!
 const bcgrdColor = (event) => {
   const tasks = document.getElementsByClassName('tasksToDo');
   for (let i = 0; i < tasks.length; i += 1) {
@@ -24,6 +24,7 @@ const bcgrdColor = (event) => {
 };
 list.addEventListener('click', bcgrdColor);
 
+// Risca o elemento quando ele é clicado duas vezes!
 const lineThrough = (event) => {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
@@ -33,7 +34,14 @@ const lineThrough = (event) => {
 };
 list.addEventListener('dblclick', lineThrough);
 
-list.addEventListener('mousedown', (e) => { e.preventDefault(); }, false);
+// Apaga todas as tarefas!
+const clear = () => {
+  const itemList = document.querySelectorAll('.tasksToDo');
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+};
+clearButton.addEventListener('click', clear);
 
-createTask();
-addTask();
+// Impede seleção dentro da área da lista - por motivos estéticos
+list.addEventListener('mousedown', (e) => { e.preventDefault(); }, false);
