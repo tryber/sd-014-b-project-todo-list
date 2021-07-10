1763323e1b54468a722e9d3a77e26a78df402a93
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-07-10 14:45:36 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const clearAllButton = document.querySelector('#apaga-tudo');
+const allTasks = document.getElementsByClassName('task');
 
 function createTask() {
   const createLi = document.createElement('li');
@@ -12,20 +13,16 @@ function createTask() {
 }
 
 function selectTask(event) {
-  const allTasks = document.querySelectorAll('.task');
+  // const allTasks = document.querySelectorAll('.task');
   for (let i = 0; i < allTasks.length; i += 1) {
     allTasks[i].classList.remove('selected');
     event.target.classList.add('selected');
   }
 }
 
-// function markCompleted(event) {
-//   if (event.target.classList.contains('completed')) {
-//     event.target.classList.remove('completed');
-//   } else {
-//     event.target.classList.add('completed');
-//   }
-// }
+function markCompleted(event) {
+  event.target.classList.toggle('completed');
+}
 
 function clearAll() {
   taskList.innerHTML = '';
@@ -37,4 +34,4 @@ button.addEventListener('click', createTask);
 
 clearAllButton.addEventListener('click', clearAll);
 
-// taskList.addEventListener('dbclick', markCompleted);
+taskList.addEventListener('dblclick', markCompleted);
