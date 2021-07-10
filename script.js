d0948500a06dd781b8a5afa141eb18c0315b8a33
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-09 21:24:53 -0300

    Requisito numero 5 e 6

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.querySelector('h1').innerText = "Minha Lista de Tarefas";
 document.querySelector('#funcionamento').innerText = "Clique duas vezes em um item para marcá-lo como completo";
 
-let tarefaLits = document.querySelector('#lista-tarefas');
-for (let i = 0; i < 5; i += 1) {
-  let addList = document.createElement('li');
-  tarefaLits.appendChild(addList);
+
+function addNewTarefa() {
+  let addTextTarefa = document.querySelector('#texto-tarefa');
+  let addInputBotton = document.querySelector('#criar-tarefa');
+  let addListTarefa = document.querySelector('#lista-tarefas');
+
+  addInputBotton.addEventListener('click' , function() {
+    let newTarefa = document.createElement('li');
+    newTarefa.innerText = addTextTarefa.value;
+
+    addListTarefa.appendChild(newTarefa);
+    value = '';
+  })
 }
+addNewTarefa();
