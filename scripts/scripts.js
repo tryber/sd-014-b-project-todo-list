bc25aca16323ed3d5f11297870592acc16b582c7
Welton Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-07-10 02:20:17 -0300

    Clicar em um item da lista altera a cor de fundo do item para cinza.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,26 @@
+// [Requisito 7]
+function markText(text) {
+  const item = text;
+  item.addEventListener('click', () => {
+    if (item.style.backgroundColor === 'rgb(255, 255, 255)') {
+      item.style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      item.style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+  });
+}
+
 // [Requisito 5]
 function addTaskToList() {
   const addTask = document.querySelector('#criar-tarefa');
   addTask.addEventListener('click', () => {
     const task = document.querySelector('#texto-tarefa');
     const item = document.createElement('li');
+    markText(item);
     item.innerHTML = task.value;
     task.value = '';
     document.querySelector('#lista-tarefas').appendChild(item);
   });
 }
-
-// [Requisito 6]
 
 addTaskToList();
