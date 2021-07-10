9d959079b636cfe243b0d1d51ec1e33dd758356e
Thales Lima <thales.bplima@gmail.com>
2021-07-10 02:47:36 -0300

    add reqs 7-9

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// <input type = 'text' name = 'atividade' id = 'texto-tarefa'>
-
-
-/* document.querySelector('#criar-tarefa').addEventListener('click', incluiTarefa); */
-/* textoTarefa.addEventListener('submit', incluiTarefa); */
-
 // Req 5 adicionando funcionalidade ao botão Criar Tarefa:
 
 const taskList = document.querySelector('#lista-tarefas'); // OL de tarefas
@@ -16,7 +10,29 @@ function incluiTarefa() {
   const li = document.createElement('li');
   li.innerHTML = taskBox.value;
   taskList.appendChild(li);
+  li.classList.add('task');
   taskBox.value = '';
 }
 
-// Req 7: pintando a Task de cinza
+// Req 7 e 8: pintando a Task de cinza
+taskList.addEventListener('click', highlight);
+
+function highlight(elemento) {
+  let allTasks = document.getElementsByClassName('task');
+  for (let index = 0; index < allTasks.length; index += 1) {
+    allTasks[index].classList.remove('selected');
+  }  
+  elemento.target.classList.add('selected');
+}
+
+// Req 9: riscando uma tarefa concluída
+taskList.addEventListener('dblclick', completion);
+
+function completion(elemento) {
+  if (elemento.target.classList.contains('completed') === true) {
+    elemento.target.classList.remove('completed');  
+  } else {
+    elemento.target.classList.add('completed');  
+  }
+}
+
