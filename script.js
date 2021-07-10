028bdf5d9aa06cef9eb236c1c3f7ad0c93cd7ebe
Mathluiz23 <matheus_luizs@hotmail.com>
2021-07-10 14:56:53 -0300

    Requisito numero 9

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+window.onload
+
 document.querySelector('h1').innerText = "Minha Lista de Tarefas";
 document.querySelector('#funcionamento').innerText = "Clique duas vezes em um item para marcá-lo como completo";
 
