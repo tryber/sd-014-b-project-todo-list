4902c0329b153363b8338c87621417fa7c7fed75
Diogo Fiuza <difipalm@gmail.com>
2021-07-10 17:09:34 -0300

    Requisito 10# e 11#

--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 const button = document.querySelector('#criar-tarefa');
-const tasks = document.querySelectorAll('.item');
 const list = document.querySelector('#lista-tarefas');
-
+const cleanEveryone = document.querySelector('#apaga-tudo');
+const cleanCompleted = document.querySelector('#remover-finalizados');
 
 // Requisito 5 e 6
 button.addEventListener('click', () => {
   const answer = document.querySelector('#texto-tarefa');
   const newtask = document.createElement('li');
-  const listItem = document.querySelectorAll('li');
-  
 
-  // Requisito 7
-  newtask.addEventListener('click', ()=> {
-    
-    for(let index of list.children){
-      if(index.className === 'item changeBg') {
+  // Requisito 7 e 8
+  newtask.addEventListener('click', () => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const index of list.children) {
+      if (index.className === 'item changeBg') {
         index.classList.remove('changeBg');
       }
       newtask.classList.add('changeBg');
     }
   });
-
-  newtask.className ='item';
+  newtask.className = 'item';
   newtask.innerText = answer.value;
   list.appendChild(newtask);
   answer.value = '';
 });
 
+// Requisito 9
+list.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+});
+
+// Requisito 10
+cleanEveryone.addEventListener('click', () => {
+  list.innerHTML = '';
+});
+
+// Requisito 11
+cleanCompleted.addEventListener('click', () => {
+  const removeCompleted = document.getElementsByClassName('completed');
+  for (let index = 0; index < removeCompleted.length; index += 0) {
+    removeCompleted[index].remove();
+  }
+});
