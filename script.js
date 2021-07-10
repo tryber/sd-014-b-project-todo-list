f39da5133d297234057e7a6872ebb07256146f92
Fran Ribeiro <ribeirofrv@gmail.com>
2021-07-10 19:01:50 -0300

    Requisito 5 e 6 - Adiciona tarefas numa lista ordenada

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello!');
+// Ao clicar no botão 'Adicionar' um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo.
+const createTask = document.querySelector('#criar-tarefa'); // button
+
+createTask.addEventListener('click', () => {
+  const listTask = document.querySelector('#lista-tarefas'); // pega ol
+  const input = document.querySelector('#texto-tarefa'); // texto input
+  const listItem = document.createElement('li'); // cria li
+
+  if (input.value === '') alert('Campo vazio, adicione uma tarefa!');
+  listItem.innerText = input.value;
+  listTask.appendChild(listItem);
+
+  input.value = '';
+});
