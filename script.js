a99106a086aa32e251f8e6576615174b57ca14b2
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-07-10 00:00:05 -0300

    Requisito #9

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function adicionaTarefa () {
 
 /* Obtive ajuda no Stackoverflow no link https://pt.stackoverflow.com/questions/455158/estou-treinando-o-b%c3%a1sico-do-javascript-e-preciso-de-ajuda-numa-to-do-list */
 
-/* Requisito 7 */
-
+/* Requisito 7 / 8*/
+/* Ajudinha do colega Esdras da turma 14 tribo B */
 function pintarSelecionado (event) {
     let listaTarefas = document.getElementsByClassName('lista')
     for (let index = 0; index < listaTarefas.length;index += 1) {
@@ -22,3 +22,21 @@ function pintarSelecionado (event) {
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 addItemList.addEventListener('click', pintarSelecionado)
+
+
+
+
+/* Requisito 9 */
+/* toggle -> https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList */
+let teste = document.getElementById('lista-tarefas');
+teste.addEventListener('dblclick', riscarSelecionado);
+
+function riscarSelecionado (event) {
+    document.getElementsByClassName('lista');
+    event.target.classList.toggle('completed')
+    }
+
+
+
+
+
