231340afec43562d7d96721bcf9b0cebfa5f476c
samsantosb <samsantosb@outlook.com>
2021-07-10 01:45:29 -0300

    fim

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-//ajuda do meu amigo glauco para desenvolver o Script e Css.
 const query = document.querySelector.bind(document);
 const queryAll = document.querySelectorAll.bind(document);
-
-const color = (x) => x.addEventListener('click', (e) => {
+const grey = (x) => x.addEventListener('click', (e) => {
   if (query('.color')) query('.color').classList.remove('color');
   e.target.classList.add('color');
 });
-const task = (y) => {
+const mark = (y) => {
   y.addEventListener('dblclick', (e) => {
     if (e.target.classList.contains('completed')) e.target.classList.remove('completed');
     else e.target.classList.add('completed');
   });
 }
-const todoList = () => {
+query('#criar-tarefa').addEventListener('click', () => {
   const newItem = document.createElement('li');
-  color(newItem), task(newItem);
+  grey(newItem), mark(newItem);
   newItem.innerText = query('#texto-tarefa').value;
   query('#lista-tarefas').appendChild(newItem);
   query('#texto-tarefa').value = null;
-};
-query('#criar-tarefa').addEventListener('click', todoList);
-
+});
 query('#apaga-tudo').addEventListener('click', () => (query('#lista-tarefas').innerHTML = ''));
 query('#remover-finalizados').addEventListener('click', () => {
   for (const markedItem of queryAll('.completed')) markedItem.remove();
 });
+
+
+//ajuda do meu amigo Glauco, outras refs estão no projeto de pixel art.
