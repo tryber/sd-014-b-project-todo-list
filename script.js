7703d38ceae84d0b92056de166e9e7b9cf6b7c48
Gustavo <gus.inacio@hotmail.com>
2021-07-10 11:51:30 -0300

    Requisito #7

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 const button = document.querySelector('#criar-tarefa')
-let lista = document.querySelector('#lista-tarefas');
-let input = document.querySelector('#texto-tarefa')
 
 function addTask() {
+
+    let lista = document.querySelector('#lista-tarefas');
+    let input = document.querySelector('#texto-tarefa')
     let newItem = document.createElement('li');
+    let itemClass = 'lista-item';
+    newItem.className = itemClass;
+    newItem.classList.add('color');
+    newItem.classList.remove('color');
     newItem.innerText = input.value;
     lista.appendChild(newItem)
     input.value = '';
-    
+
 }
 button.addEventListener('click', addTask);
-
-   
-
+let taskList = document.querySelector('#lista-tarefas');
+function changeColor (event) {
+    const list = document.querySelectorAll('li');
+    for (let i = 0; i < list.length; i +=1) {
+        list[i].classList.remove('color');
+    }
+    event.target.classList.add('color');
     
+}
+taskList.addEventListener('click', changeColor);
+
+
+
+
+
+
+
+
+
+
+
 
