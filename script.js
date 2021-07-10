8ff3bcfa5d6b4a5db5b143f217e6ef7349c4d92a
Leandro <leandro2903.lan@gmail.com>
2021-07-10 19:29:06 -0300

    arrumando a funçao para tb apagar o riscado da tarefa quando der duplo click em cima

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ const inputTarefa = document.getElementById('texto-tarefa');
 const buttonCriar = document.getElementById('criar-tarefa');
 
 // função para alterar a cor de fundo (selecionar a tarefa)
-function listBackground(event) { // seguindo a recomendação do Linter a função tem que ficar aqui em cima antes dela ser invocada na criação das li
+function listBackground(event) {
+  // seguindo a recomendação do Linter a função tem que ficar aqui em cima antes dela ser invocada na criação das li
   const tarefaCinza = document.getElementsByClassName('cinza');
-  for (const item of tarefaCinza) {
+  for (let item of tarefaCinza) {
     item.classList.remove('cinza');
   }
-  event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
+  event.target.classList.add('cinza'); // criando o evendo para adicionar a classe no novo elemento
 }
-
+// função para riscar a tarefa concluida
+function completed(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
 function capturarTarefa() {
-  const tarefa = inputTarefa.value;// capturar e guardar, na variavel, o value (texto) digitado
+  const tarefa = inputTarefa.value; // capturar e guardar, na variavel, o value (texto) digitado
   inputTarefa.value = null; // atribuindo o valor null para limpar o campo de digitação
   const liTarefa = document.createElement('li');
   liTarefa.className = 'atividade-adicionada';
@@ -24,10 +32,3 @@ function capturarTarefa() {
 }
 // evento no botão que ao receber um click ele execulta a função addTarefas
 buttonCriar.addEventListener('click', capturarTarefa);
-
-function completed(event) {
-  if(event.target.classList.contains('completed')) {
-    event.target.classList.remove('completed');
-  }
-  event.target.classList.add('completed');
-}
