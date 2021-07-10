b3bc0ab3fa6da8e0a4e32a23c2c571e5d83f53b8
Diogo Fiuza <difipalm@gmail.com>
2021-07-09 21:46:58 -0300

    Requisito #7 e 8#

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const button = document.querySelector('#criar-tarefa');
+const tasks = document.querySelectorAll('.item');
+const list = document.querySelector('#lista-tarefas');
 
+
+// Requisito 5 e 6
 button.addEventListener('click', () => {
   const answer = document.querySelector('#texto-tarefa');
-  const task = document.createElement('li');
-  const list = document.querySelector('#lista-tarefas');
-  task.innerText = answer.value;
-  list.appendChild(task);
+  const newtask = document.createElement('li');
+  const listItem = document.querySelectorAll('li');
+  
+
+  // Requisito 7
+  newtask.addEventListener('click', ()=> {
+    
+    for(let index of list.children){
+      if(index.className === 'item changeBg') {
+        index.classList.remove('changeBg');
+      }
+      newtask.classList.add('changeBg');
+    }
+  });
+
+  newtask.className ='item';
+  newtask.innerText = answer.value;
+  list.appendChild(newtask);
   answer.value = '';
 });
+
