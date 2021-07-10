30c05f1434306e4ce6dd417caf606fee8bd6315a
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-07-09 21:23:12 -0300

    quando clica em cima da lista ela fica cinza

--- script.js
+++ script.js
@@ -7,17 +7,23 @@ const buttonCriar = document.getElementById('criar-tarefa');
 // armazenar o texto captrado como tarefa numa variavel
 let list;
 
-function capturarTarefa(event) {
+// função para alterar a cor de fundo
+
+function listBackground(event) { // seguindo a recomendação do Linter a função tem que ficar aqui em cima antes dela ser invocada na criação das li
+  event.target.classList.add('cinza');// criando o evendo para adicionar a classe no novo elemento
+}
+
+function capturarTarefa() {
   // capturar e guardar, na variavel, o value (texto) digitado
   const tarefa = inputTarefa.value;
   list = tarefa;
-  inputTarefa.value = null; // atribuindo o valor null para limpar o campo de digitação  
+  inputTarefa.value = null; // atribuindo o valor null para limpar o campo de digitação
   const liTarefa = document.createElement('li');
   liTarefa.className = 'atividade-adicionada';
   liTarefa.innerText = list;
-  console.log(liTarefa)
-  listaOrdenada.appendChild(liTarefa); 
+  liTarefa.addEventListener('click', listBackground);
+  console.log(liTarefa);
+  listaOrdenada.appendChild(liTarefa);
 }
 // evento no botão que ao receber um click ele execulta a função addTarefas
 buttonCriar.addEventListener('click', capturarTarefa);
-buttonCriar.addEventListener('keypress', capturarTarefa);
