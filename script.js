5065f30699702a6bbfdbc0f9d694a8bed930946b
Cezar <cezarguimaraes@gmail.com>
2021-07-10 07:55:44 -0300

    testando

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let getButton = document.getElementById("criar-tarefa");
-let getOl = document.getElementById("lista-tarefas")
-let getInput = document.getElementById("texto-tarefa")
+
+
 
 function addTask(){
+    let getOl = document.getElementById("lista-tarefas");
+    let getInput = document.getElementById("texto-tarefa");
     let newLi = document.createElement("li");
     newLi.innerHTML = getInput.value;
     getOl.appendChild(newLi);
     getInput.value = "";
 };
+let getButton = document.getElementById("criar-tarefa");
 getButton.addEventListener("click", addTask);
 
