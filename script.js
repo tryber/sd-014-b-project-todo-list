d3b2d7c06157e6e3dff0dfe4b44b3bc7f20296dd
Matheus <mateeus.soarees@outlook.com>
2021-07-11 15:20:29 -0300

    Até o 11 e 8 a corrigir

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const botaoAdiciona = document.querySelector('#criar-tarefa');
 const userInput = document.querySelector('#texto-tarefa');
 const listaDesordenada = document.querySelector('#lista-tarefas');
+const botaoApaga = document.querySelector('#apaga-tudo');
+const botaoSelecionados = document.querySelector('#remover-finalizados');
+const pegarSelecionados = '#lista-tarefas li';
 
 botaoAdiciona.addEventListener('click', function () {
   if (userInput.value !== '') {
@@ -11,7 +14,7 @@ botaoAdiciona.addEventListener('click', function () {
     userInput.value = '';
   }
 });
-function pintaTarefa() {
+/*function pintaTarefa() {
   let selecaoDeTarefas = document.querySelectorAll('.tarefas');
   for (let i = 0; i < selecaoDeTarefas.length; i++) {
     if (selecaoDeTarefas[i].classList.contains('selecionado')) {
@@ -20,14 +23,38 @@ function pintaTarefa() {
       selecaoDeTarefas[i].style.backgroundColor = 'white';
     }
   }
-}
+}*/
 
-listaDesordenada.addEventListener('click', function (evento) {
-  let itemSelecionado = document.querySelector('.selecionado');
-  if (itemSelecionado !== null) {
-    itemSelecionado.classList.remove('selecionado');
+function selecionaItem() {
+  listaDesordenada.addEventListener('click', function (evento) {
+    let itemSelecionado = document.querySelector('.selecionado');
+    if (itemSelecionado !== null) {
+      itemSelecionado.classList.remove('selecionado');
+      itemSelecionado.removeAttribute('style');
+    } else {
+      evento.target.classList.add('selecionado');
+    }
+  });
+}
+selecionaItem();
+
+listaDesordenada.addEventListener('dblclick', function (evento) {
+  if (!evento.target.className.includes('completed')) {
+    evento.target.classList.add('completed');
   } else {
-    evento.target.classList.add('selecionado');
-    pintaTarefa();
+    evento.target.removeAttribute('class');
+  }
+});
+
+botaoApaga.addEventListener('click', function () {
+  listaDesordenada.innerHTML = '';
+});
+
+botaoSelecionados.addEventListener('click', function () {
+  const pegarTodos = document.querySelectorAll(pegarSelecionados);
+  for (let i = 0; i < pegarTodos.length; i++) {
+    if (pegarTodos[i].className.includes('completed')) {
+      listaDesordenada.removeChild(pegarTodos[i]);
+    }
   }
 });
