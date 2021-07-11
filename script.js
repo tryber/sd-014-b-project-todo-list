3b6309f1567ceb92918a41cb6d29449f8f50532d
Anderson Leite <andersonleite.dev@gmail.com>
2021-07-11 19:52:57 -0300

    Req 12 -Add botao salvar tarefas no localStorage

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonCreate = document.querySelector('#criar-tarefa');
 const buttonRemoveFinished = document.querySelector('#remover-finalizados');
 const buttonDeleteAll = document.querySelector('#apaga-tudo');
+const buttonSave = document.querySelector('#salvar-tarefas');
 const list = document.querySelector('#lista-tarefas');
 
 function addItemList(item) {
@@ -61,3 +62,56 @@ buttonRemoveFinished.addEventListener('click', () => {
   removeItemsFinished();
   removeItemsFinished();
 });
+
+/**
+ * Para cumprir o requesito 12 consultei o Bloco 5
+ * JavaScript: Web Storage e me basiei nos trechos
+ * de códigos lá existentes para desenvolver a logica
+ * que soluciona o que pede o requesito
+ * LinK: https://app.betrybe.com/course/fundamentals/javascript-dom-eventos-e-web-storage/javascript-web-storage/b332393f-7548-4075-83e3-f632735efb95/conteudos/11077350-a28a-47c9-9b1e-2312a4a2c9a1/local-e-session-storage/b78f3c98-e93d-41d9-a9dc-ed8776f776d1?use_case=next_button
+ */
+function task(li) {
+  const nameTask = li.innerHTML;
+  const completed = li.classList.contains('completed');
+  return {
+    name: nameTask,
+    isCompleted: completed,
+  };
+}
+
+function addTasksInLocalStorage() {
+  const oldList = [];
+  const li = list.childNodes;
+  for (let i = 0; i < li.length; i += 1) {
+    oldList[i] = task(li[i]);
+  }
+  localStorage.setItem('tasks', JSON.stringify(oldList));
+  alert('Salvo com sucesso!');
+}
+
+function insertTasksInDOM() {
+  const tasksList = JSON.parse(localStorage.getItem('tasks'));
+  for (let i = 0; i < tasksList.length; i += 1) {
+    const taskItem = document.createElement('li');
+    taskItem.innerText = tasksList[i].name;
+    if (tasksList[i].isCompleted) {
+      taskItem.classList.add('completed');
+    }
+    list.appendChild(taskItem);
+  }
+}
+
+buttonSave.addEventListener('click', () => {
+  addTasksInLocalStorage();
+});
+
+function initialRenderization() {
+  if (localStorage.getItem('tasks') === null) {
+    localStorage.setItem('tasks', JSON.stringify([]));
+  }
+}
+
+window.onload = () => {
+  initialRenderization();
+  insertTasksInDOM();
+};
