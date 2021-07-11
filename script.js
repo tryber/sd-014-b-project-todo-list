7495e9f4e4f05f7c9d71e17823315ddedaee3e88
Elithon Silva <elithonsilva@gmail.com>
2021-07-10 22:06:09 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const textInput = document.querySelector('#texto-tarefa');
 const addButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
-const clearButton = document.querySelector('#apaga-tudo');
 
-// Referencias para resolver o requisito 05:
+// Adiciona texto do input na lista ordenada
+// Referências:
 // https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/
 // Conteúdo do Course [5.2] Parte II Criando Elementos
 // Exercício de fixação - Spotrybefy - link para meu repositório https://github.com/oelithon/other-activities/pull/2
@@ -12,6 +12,7 @@ function addText() {
   const texto = textInput.value;
   const taskListItem = document.createElement('li');
   taskListItem.innerText = texto;
+  taskListItem.classList.add('task');
   textInput.value = null;
   taskList.appendChild(taskListItem);
 }
@@ -20,6 +21,8 @@ addButton.addEventListener('click', addText);
 // Adiciona background-color da tarefa clicada
 
 function addBackground(event) {
+  const taskBgColor = document.querySelector('.task');
+  taskBgColor.classList.remove('bgColor');
   event.target.classList.add('bgColor');
 }
 taskList.addEventListener('click', addBackground);
