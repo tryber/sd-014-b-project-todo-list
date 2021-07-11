f8782c8cec195856d8758c39835c7deacf2e9285
Guilherme Neri <guinsneri@gmail.com>
2021-07-11 04:44:16 -0300

    Desafio 12

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-//insert
+function init() {
+    if(JSON.parse(localStorage.getItem('li'))) {
+    let text = JSON.parse(localStorage.getItem('li'));
+    text.forEach(element => {
+        if(element.style)
+        {
+            let li = document.createElement('li');
+            let tasks = document.getElementById('lista-tarefas').appendChild(li);
+            tasks.innerText = element.text;
+            tasks.classList.add('completed');
+            //update color + function bellow
+            tasks.addEventListener('click', updateColor);
+            //toggle line through + function bellow
+            tasks.addEventListener('dblclick', doubleClick);
+        } else {
+            let li = document.createElement('li');
+            let tasks = document.getElementById('lista-tarefas').appendChild(li);
+            tasks.innerText = element.text;
+            //update color + function bellow
+            tasks.addEventListener('click', updateColor);
+            //toggle line through + function bellow
+            tasks.addEventListener('dblclick', doubleClick);
+        }
+    })}
+};
+init();
+//insert task
 let insert = document.getElementById('criar-tarefa');
 insert.addEventListener('click', function(){
     let li = document.createElement('li');
     li.innerText = document.getElementById('texto-tarefa').value;
     document.getElementById('lista-tarefas').appendChild(li);
-    task = document.getElementById('lista-tarefas').appendChild(li);
+    let task = document.getElementById('lista-tarefas').appendChild(li);
     //update color + function bellow
     task.addEventListener('click', updateColor);
     //toggle line through + function bellow
@@ -41,3 +67,24 @@ delCompleted.addEventListener('click', function() {
         element.remove();
     });
 });
+
+//save tasks
+let saveTasks = document.getElementById('salvar-tarefas');
+saveTasks.addEventListener('click', function () {
+    let tasks = document.querySelectorAll('li');
+    localStorage.clear();
+    let li = [];
+    tasks.forEach(element => {
+        let text = element.innerText;
+        let style = false;
+        if (element.classList.contains("completed"))
+        {
+            style = true;
+            li.push({'text':text, "style": style});
+        } else
+        { 
+            li.push({'text':text, "style": style});
+        }
+    });
+    localStorage.setItem('li', JSON.stringify(li));
+})
