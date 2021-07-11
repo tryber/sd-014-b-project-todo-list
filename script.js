ab842109bd4bb07247d8d80a1f48c51ea70c3151
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-07-11 17:58:21 -0300

    Completa os requisitos restantes, organiza o código e adiciona referências

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const input = document.querySelector('#texto-tarefa'); /* Input requisito 3 */
 const taskList = document.querySelector('#lista-tarefas'); /* Lista ordenada requisito 4 */
 const button = document.querySelector('#criar-tarefa'); /* Botão requisito 5 */
-const allTasks = document.getElementsByClassName('task'); /* Array com todas os list-itens, utilizado em várias funções */
+const allTasks = document.getElementsByClassName('task'); /* Array com todas os list-itens, utilizado na função do requisito 7 e 8 */
 const clearAllButton = document.querySelector('#apaga-tudo'); /* Botão requisito 10 */
 const clearFinished = document.querySelector('#remover-finalizados'); /* Botão requisito 11 */
 const saveTasks = document.querySelector('#salvar-tarefas'); /* Botão requisito 12 */
+const moveUp = document.querySelector('#mover-cima'); /* Botão requisito 13 */
+const moveDown = document.querySelector('#mover-baixo'); /* Botão requisito 13 */
+const removeSelected = document.querySelector('#remover-selecionado'); /* Botão requisito 14 */
+
+function enableSelection() {
+  for (let i = 0; i < allTasks.length; i += 1) {
+    allTasks[i].addEventListener('click', (event) => {
+      for (let i2 = 0; i2 < allTasks.length; i2 += 1) {
+        allTasks[i2].classList.remove('selected');
+        event.target.classList.add('selected');
+      }
+    });
+  }
+}
 
 //  Requisito 5 e 6
 
@@ -14,23 +28,13 @@ function createTask() {
   taskList.appendChild(createLi);
   createLi.innerText = input.value;
   input.value = '';
+  enableSelection();
 }
 
 button.addEventListener('click', createTask);
 
-//  Requisito 7 e 8
-
-function selectTask(event) {
-  for (let i = 0; i < allTasks.length; i += 1) {
-    allTasks[i].classList.remove('selected');
-    event.target.classList.add('selected');
-  }
-}
-
-taskList.addEventListener('click', selectTask);
-
 // Requisito 9
-
+// Referência classList.toggle
 function markCompleted(event) {
   event.target.classList.toggle('completed');
 }
@@ -50,7 +54,7 @@ clearAllButton.addEventListener('click', clearAll);
 
 function clearDone() {
   const completedLis = document.getElementsByClassName('completed');
-  console.log(completedLis);
+
   for (let i = 0; i < completedLis.length; i += 0) {
     completedLis[i].remove();
   }
@@ -58,12 +62,46 @@ function clearDone() {
 
 clearFinished.addEventListener('click', clearDone);
 
+// Requisito 12
+
 saveTasks.addEventListener('click', () => {
   localStorage.setItem('list', taskList.innerHTML);
 });
 
 window.onload = () => {
   taskList.innerHTML = localStorage.getItem('list');
+  enableSelection();
 };
 
-clearFinished.addEventListener('click', clearDone);
+// Requisito 13
+// element.insertbefore() que usei para resolver os requisitos 13
+// Referências https://stackoverflow.com/questions/34913953/move-an-element-one-place-up-or-down-in-the-dom-tree-with-javascript/34914096#34914096
+// Documentação do element.insert.before()
+
+moveUp.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) {
+    if (selected === allTasks[0]) {
+      alert('Não pode mais subir');
+    } else {
+      selected.parentNode.insertBefore(selected, selected.previousElementSibling);
+    }
+  }
+});
+
+moveDown.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) {
+    if (selected.nextElementSibling === null) {
+      alert('Não pode mais descer');
+    } else {
+      selected.parentNode.insertBefore(selected, selected.nextElementSibling.nextElementSibling);
+    }
+  }
+});
+
+// Requisito 14
+
+removeSelected.addEventListener('click', () => {
+  document.querySelector('.selected').remove();
+});
