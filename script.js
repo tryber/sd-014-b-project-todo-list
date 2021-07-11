e04a53e6c8f0825884c48789ceca2be328f5371b
Elithon Silva <elithonsilva@gmail.com>
2021-07-10 21:26:58 -0300

    correcoes em andamento

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 const textInput = document.querySelector('#texto-tarefa');
 const addButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+const clearButton = document.querySelector('#apaga-tudo');
 
 // Referencias para resolver o requisito 05:
 // https://www.horadecodar.com.br/2020/12/15/como-pegar-valor-de-input-com-javascript/
 // Conteúdo do Course [5.2] Parte II Criando Elementos
 // Exercício de fixação - Spotrybefy - link para meu repositório https://github.com/oelithon/other-activities/pull/2
 
-function addText() {
+addButton.addEventListener('click', function() {
   const texto = textInput.value;
   const taskListItem = document.createElement('li');
   taskListItem.innerText = texto;
-  taskListItem.classList = 'task';
   textInput.value = null;
   taskList.appendChild(taskListItem);
-}
+});
 
-addButton.addEventListener('click', addText);
+// Adiciona background-color da tarefa clicada
 
-// Remove background-color da tarefa que estava selecionada
-
-function addColor(event) {
-  const taskClass = document.querySelector('.task');
-  taskClass.classList.remove('bgColor');
+taskList.addEventListener('click', function(event) {
   event.target.classList.add('bgColor');
-}
-
-taskList.addEventListener('click', addColor);
+});
 
 // Risca tarefas concluídas
 
-function textCompleted(event) {
-  event.target.classList.add('completed')
+taskList.addEventListener('dblclick', function(event) {
+  event.target.classList.add('completed');
+});
+
+taskList.addEventListener('dblclick', function(event) {
+  const theTask = document.querySelector('#lista-tarefas');
+  if (theTask.classList.contains('completed')) {
+    event.classList.remove('completed');
+  }
+});
+
+// Limpar Lista
+
+function clearList() {
+  taskList = null;
 }
 
-taskList.addEventListener('dblclick', textCompleted);
+clearButton.addEventListener('click', clearList);
