fff17d9dd9c2e3018fce281a9a331b94d43d7ca5
icaroemanuel <icaro.emanuel@outlook.com>
2021-07-10 22:22:44 -0300

    Comentários

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let btnCriarTarefa = document.querySelector('#criar-tarefa');
 let listaTarefas = document.querySelector('#lista-tarefas');
 let tarefa = '';
 
+// Limpa o input depois que o botão é clicado
 function limparInput() {
   textoTarefa.value = '';
 }
 
+// Adiciona o elemento 'li' e renderiza a tarefa no DOM
 function criarTarefa() {
   tarefa = document.createElement('li');
   tarefa.innerText = textoTarefa.value;
@@ -14,4 +16,5 @@ function criarTarefa() {
   limparInput();
 }
 
+// Cria um evento de clique para quando o botão é pressionado
 btnCriarTarefa.addEventListener('click', criarTarefa);
