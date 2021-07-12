8d38b1af50f1bb58ed1be648c32d6c70c1c8de96
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-07-12 12:28:26 -0300

    Fix

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-
 function addToList() {
-    const inputBox = document.getElementById('texto-tarefa');
-    const text = inputBox.value;
-    const list = document.getElementById("lista-tarefas");
-    const item = document.createElement('li');
-    item.innerHTML = text;
-    list.appendChild(item);
+  const inputBox = document.getElementById('texto-tarefa');
+  const text = inputBox.value;
+  const list = document.getElementById("lista-tarefas");
+  const item = document.createElement("li");
+  item.innerHTML = text;
+  list.appendChild(item);
+  inputBox.value = "";
 }
 
 function Main() {
-    const addButton = document.getElementById('criar-tarefa');
-    addButton.addEventListener('click', addToList);
+  const addButton = document.getElementById('criar-tarefa');
+  addButton.addEventListener('click', addToList);
 }
 
 window.onload = Main;
