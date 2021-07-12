78fcf86654408e68f8e5a7c9119ad4f466635315
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-07-12 15:06:49 -0300

    requisito 9 refatorado

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-let btn = document.querySelector('#criar-tarefa');
-btn.addEventListener('click', addTask);
-
-function addTask() {
-  let input = document.querySelector('#texto-tarefa');
-  let list = document.querySelector('#lista-tarefas');
-  let li = document.createElement('li');
-  li.className = 'tarefa';
-  list.appendChild(li);
-  selecItem();
-  endTask();
-  li.innerText = input.value;
-  input.value = '';
-  console.log(li);
+function mark(element) {
+  let li = document.querySelectorAll('li');
+  element.addEventListener('dblclick', function risca(event) {
+    event.target.classList.toggle('completed');
+  });
 }
 
 function selecItem() {
-  let li = document.querySelectorAll('.tarefa');
+  let li = document.querySelectorAll('li');
   for (let i = 0; i < li.length; i += 1) {
     const element = li[i];
     element.addEventListener('click', function printLi() {
@@ -27,16 +18,17 @@ function selecItem() {
   }
 }
 
-function endTask() {
-  let li = document.querySelectorAll('.tarefa');
-  for (let i = 0; i < li.length; i++) {
-    const element = li[i];
-    element.addEventListener('dblclick', function risca(event) {
-      if (element.classList.length === 2) {
-        event.target.classList.remove('completed');
-      } else {
-        event.target.classList.add('completed');
-      }
-    });
-  }
+let btn = document.querySelector('#criar-tarefa');
+btn.addEventListener('click', addTask);
+
+function addTask() {
+  let input = document.querySelector('#texto-tarefa');
+  let list = document.querySelector('#lista-tarefas');
+  let li = document.createElement('li');
+  list.appendChild(li);
+  selecItem();
+  mark(li);
+  li.innerText = input.value;
+  input.value = '';
+  console.log(li);
 }
