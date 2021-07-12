86a5dd2aad0ab70fe502c93e5e96c4039ef719da
Raphael Oliveira <raphael.caninde@gmail.com>
2021-07-11 23:53:23 -0300

    Requeriment numbers 7, 8, 9 complete

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let inputElement = document.getElementById("texto-tarefa");  //input de texto
 let buttonElement = document.querySelector("#criar-tarefa"); // botão adicionar
 let olElement = document.querySelector("#lista-tarefas"); // recuperando lista
+let btnRemove = document.querySelector("#apaga-tudo"); //recupera o botão de remover
+let btnSelectedClear = document.querySelector("#remover-finalizados");
 
-buttonElement.addEventListener("click", recebeClick);
 
-function recebeClick(event) {
+buttonElement.addEventListener("click", function recebeClick() {
+  let liElement = document.createElement('li');
 
   if (inputElement.value) {
-    let liElement = document.createElement('li');
     liElement.innerText = `${inputElement.value}`;
     olElement.appendChild(liElement);
     inputElement.value = "";
   }
 
-}
+  liElement.addEventListener("click", function selectColorGray(event){
+    if (document.querySelector('.selected')) {
+      let colorGray = document.querySelector('.selected');
+      colorGray.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  })
+
+  liElement.addEventListener("dblclick", function doubleClicknone(event) {
+    event.target.classList.toggle('completed');
+  })
+
+  btnRemove.addEventListener("click", function removeTasks() {
+    olElement.removeChild(liElement);
+  })
+
+
+
+})
