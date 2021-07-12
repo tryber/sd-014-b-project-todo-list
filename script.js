d2296b80bffdddfa2b715ca762d8cc83a3e82dd4
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-07-12 15:26:18 -0300

    refatoracao do codigo

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-function mark(element) {
-  let li = document.querySelectorAll('li');
-  element.addEventListener('dblclick', function risca(event) {
-    event.target.classList.toggle('completed');
+const query = document.querySelector.bind(document);
+const queryAll = document.querySelectorAll.bind(document);
+
+const selecTask = (x) =>
+  x.addEventListener('click', (e) => {
+    if (query('.color')) query('.color').classList.remove('color');
+    e.target.classList.add('color');
   });
-}
 
-function selecItem() {
-  let li = document.querySelectorAll('li');
-  for (let i = 0; i < li.length; i += 1) {
-    const element = li[i];
-    element.addEventListener('click', function printLi() {
-      li.forEach((element) => {
-        element.style.backgroundColor = 'white';
-      });
-      element.style.backgroundColor = 'rgb(128, 128, 128)';
-    });
-  }
-}
+const mark = (element) => {
+  let li = queryAll('li');
+  element.addEventListener('dblclick', function risca(e) {
+    e.target.classList.toggle('completed');
+  });
+};
 
-let btn = document.querySelector('#criar-tarefa');
-btn.addEventListener('click', addTask);
-
-function addTask() {
-  let input = document.querySelector('#texto-tarefa');
-  let list = document.querySelector('#lista-tarefas');
+const addTask = () => {
+  let input = query('#texto-tarefa');
+  let list = query('#lista-tarefas');
   let li = document.createElement('li');
   list.appendChild(li);
-  selecItem();
+  selecTask(li);
   mark(li);
   li.innerText = input.value;
   input.value = '';
-  console.log(li);
-}
+};
+
+query('#criar-tarefa').addEventListener('click', addTask);
