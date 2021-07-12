2a9bf3d93a4941de4a919a0bd196d95d8ac2c472
dsmarcelo <celoygm@gmail.com>
2021-07-11 22:42:18 -0300

    Requisito 7,8 fix

--- script.js
+++ script.js
@@ -5,25 +5,8 @@ let tasks = document.querySelectorAll('.task');
 
 //Referecia: https://www.w3schools.com/howto/howto_html_clear_input.asp
 
-function addTask() {
-  if (taskInput.value === '') {
-    console.log('empty');
-  } else {
-    const liCreate = document.createElement('li');
-    liCreate.innerText = taskInput.value;
-    liCreate.classList.add('lastTaskCreated');
-    liCreate.classList.add('task');
-    taskList.appendChild(liCreate);
-    taskInput.value = '';
-    taskList.lastChild.previousSibling.classList.remove('lastTaskCreated');
-    tasks = document.querySelectorAll('.task');
-    clickTask();
-  }
-}
-
-button.addEventListener('click', addTask);
-
 function selectTask(event) {
+  console.log('ltc.length');
   const selTaskList = document.getElementsByClassName('selectedTask');
   if (selTaskList.length >= 1) {
     selTaskList[0].classList.remove('selectedTask');
@@ -36,8 +19,35 @@ function selectTask(event) {
 function clickTask() {
   for (let i = 0; i < tasks.length; i += 1) {
     tasks[i].addEventListener('click', selectTask);
+    tasks[i].addEventListener('dbclick', markCompleted);
   }
 }
+
+function addTask() {
+  const ltc = document.querySelectorAll('.lastTaskCreated');
+  const liCreate = document.createElement('li');
+  if (ltc.length >= 1) {
+    ltc[0].classList.remove('lastTaskCreated');
+    liCreate.innerText = taskInput.value;
+    liCreate.classList.add('lastTaskCreated', 'task');
+    taskList.appendChild(liCreate);
+    taskInput.value = '';
+  } else if (ltc.length === 0) {
+    liCreate.innerText = taskInput.value;
+    liCreate.classList.add('lastTaskCreated', 'task');
+    taskList.appendChild(liCreate);
+    taskInput.value = '';
+    console.log(ltc.length);
+    console.log();
+  }
+  tasks = document.querySelectorAll('.task');
+  clickTask();
+}
+function markCompleted(event) {
+  event.target.classList.add('completed');
+}
+
+button.addEventListener('click', addTask, clickTask);
 
 // function clickTask() {
 //   const selTaskList = [];
