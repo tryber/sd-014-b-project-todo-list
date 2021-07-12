360c544cc552584d906145cc2efdf3ecc13e13da
Bruno M Landim <brunolandim13@hotmail.com>
2021-07-12 17:41:59 -0300

    7 Requisito

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let addTask = document.querySelector('#criar-tarefa');
-let textInput = document.querySelector('#texto-tarefa');
-let ols = document.querySelector('#lista-tarefas')
+const addTask = document.querySelector('#criar-tarefa');
+const textInput = document.querySelector('#texto-tarefa');
+const ols = document.querySelector('#lista-tarefas')
 
-addTask.addEventListener('click',function () {
+addTask.addEventListener('click',  function () {
     let text = textInput.value;
 
     if (text != '') {
        let createLi = document.createElement('li');
        createLi.innerText = text;
        ols.appendChild(createLi);
+
+        createLi.addEventListener('click',  function() {
+        createLi.className = 'cor-de-fundo'
+    });
     }
+    
     textInput.value = '';
 });
