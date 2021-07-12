585cfeb1c4a5702539133f854c2a08b5049ae6e7
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-07-11 23:03:04 -0300

    seleciona itens individualmente

--- script.js
+++ script.js
@@ -5,7 +5,24 @@ function addTask() {
   let input = document.querySelector('#texto-tarefa');
   let list = document.querySelector('#lista-tarefas');
   let li = document.createElement('li');
+  li.className = 'tarefa';
   list.appendChild(li);
+  selecItem();
   li.innerText = input.value;
   input.value = '';
+  console.log(li);
+}
+
+function selecItem() {
+  let li = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < li.length; i += 1) {
+    console.log(i);
+    let element = li[i];
+    element.addEventListener('click', function printLi() {
+      li.forEach((element) => {
+        element.style.backgroundColor = 'white';
+      });
+      element.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
 }
