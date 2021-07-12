53c4c19e084798d79c805a96d1e53ad609f720db
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-07-12 16:52:34 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let tarefaBotao = document.getElementById('criar-tarefa');
 let caixaTexto = document.getElementById('texto-tarefa');
 let lista = document.getElementById('lista-tarefas');
+let limparTudoBotao = document.getElementById('apaga-tudo');
 
 tarefaBotao.addEventListener('click', function() {
     let item = document.createElement('li')
     item.innerText = caixaTexto.value;
     lista.appendChild(item);
     caixaTexto.value = "";
+});
+
+
+limparTudoBotao.addEventListener('click', function() {
+    lista.innerHTML = '';
+
 })
+
