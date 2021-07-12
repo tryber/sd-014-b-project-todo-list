c67dd7af9ed5c3f9bf3e3eb8f651b68fdbe80cb7
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-07-12 18:16:29 -0300

    Todos os requisitos ok

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const button = document.getElementById('criar-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const input = document.getElementById('texto-tarefa');
 const buttonApagaTudo = document.getElementById('apaga-tudo');
+const removerFinalizado = document.getElementById('remover-finalizados');
+const buttonSalvar = document.getElementById('salvar-tarefas');
+const buttonRemover = document.getElementById('remover-selecionado')
+const buttonCima = document.getElementById('mover-cima');
+const buttonBaixo = document.getElementById('mover-baixo');
 
+window.onload = () => {
+    
+    listaTarefas.innerHTML = localStorage.getItem("Lista");
 
+}
 
 
 button.addEventListener('click',criarTarefas)
 
 function linhaRiscada (event){
-    let completed = document.querySelector('.completed')
-    if (completed){
-        completed.classList.remove('completed');
-    }else {
-        event.target.classList.add('completed');
-    }
-
+        event.target.classList.toggle('completed');
+   
 }
 
 function trocarCor(event){
@@ -33,7 +37,7 @@ function criarTarefas(){
     let li = document.createElement('li');
     li.innerText = input.value;
     listaTarefas.appendChild(li);
-    let clear = document.getElementById('texto-tarefa').value = ""
+    document.getElementById('texto-tarefa').value = ""
     li.addEventListener('click',trocarCor);
     li.addEventListener('dblclick',linhaRiscada);
     }else {
@@ -46,6 +50,62 @@ function clearTarefas(){
    let li = document.querySelector('ol')
    li.innerHTML = "";
 }    
+
+function removerFinalizados(){
+let finalizados = document.querySelectorAll('.completed')
+    for (let cont = 0; cont < finalizados.length; cont+=1){
+        finalizados[cont].remove();
+    }
+}
     
 buttonApagaTudo.addEventListener('click',clearTarefas);
 
+removerFinalizado.addEventListener('click',removerFinalizados);
+
+buttonSalvar.addEventListener('click',salvarTarefas);
+
+buttonRemover.addEventListener('click',removerSelecionado)
+
+buttonCima.addEventListener('click',moverParaCima);
+
+buttonBaixo.addEventListener('click',moverParaBaixo);
+
+// buttonCima.addEventListener('click',moverParaCima);
+
+function salvarTarefas(){
+  let lista = listaTarefas.innerHTML;
+    
+    localStorage.setItem("Lista",lista);
+}
+
+
+
+function moverParaCima(){
+    let no = document.querySelector('.selected');
+    if (no == listaTarefas.firstElementChild){
+        alert('ERRO')
+    }else if(no){
+        listaTarefas.insertBefore(no, no.previousElementSibling)
+    }
+}
+
+
+function moverParaBaixo(){
+    let no = document.querySelector('.selected');
+    if (no == listaTarefas.lastElementChild){
+        alert('ERRO')
+    }else if(no){
+        listaTarefas.insertBefore(no.nextElementSibling, no)
+    }
+}
+
+
+function removerSelecionado(){
+    let selecionado = document.querySelector('.selected')
+    if (selecionado){
+        selecionado.remove();
+    }else{
+    alert('Selecione para remover')
+    }
+
+}
