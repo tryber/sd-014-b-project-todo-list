5bd37cf86a996efaa32a9b457a184630a627bb71
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-07-12 12:42:14 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
+function highlightItem(element) {
+    const item = element;
+    item.addEventListener('click', () => {
+        if (item.style.backgroundColor === 'rgb(255, 255, 255)') {
+          item.style.backgroundColor = 'rgb(128, 128, 128)';
+        } else {
+          item.style.backgroundColor = 'rgb(255, 255, 255)';
+        }
+      });
+}
+
 function addToList() {
   const inputBox = document.getElementById('texto-tarefa');
   const text = inputBox.value;
-  const list = document.getElementById("lista-tarefas");
-  const item = document.createElement("li");
+  const list = document.getElementById('lista-tarefas');
+  const item = document.createElement('li');
   item.innerHTML = text;
   list.appendChild(item);
   inputBox.value = "";
+  highlightItem(item);
 }
 
 function Main() {
