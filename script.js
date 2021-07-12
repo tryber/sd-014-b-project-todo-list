18e9c71e8466077d719a50ec7d7cfb6629c59162
Renata <renatateixeira.est@gmail.com>
2021-07-12 16:53:44 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let listaOrdenada = document.querySelector('#lista-tarefas');
 let addBotao = document.querySelector('#criar-tarefa');
 let inputTarefa = document.querySelector('#texto-tarefa');
 let linha = document.querySelector('li');
+let deletar = document.querySelector('#remover-finalizados');
+let apagaTudo = document.querySelector('#apaga-tudo')
 
 //Requisito 5 e 6:
 addBotao.addEventListener('click',function(){
@@ -21,10 +23,11 @@ function teste(){listaOrdenada.addEventListener('click',function(event){
     let fundo = lista[index].style.backgroundColor;
     //console.log("fundo")
     if(fundo === 'rgb(128, 128, 128)'){
-      lista[index].style.backgroundColor = 'white';
+      lista[index].style.backgroundColor = 'greenyellow';
     }
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  //event.target.classList.add('finalizados');
 })}
 
 teste()
@@ -32,9 +35,8 @@ teste()
 //Requisito 9:
 //erro ao dar o segundo cuplo click a classe não é removida
 listaOrdenada.addEventListener('dblclick',function(event){
-  let lista = document.querySelectorAll('li')
-  console.log(lista)
-  //o for garante que apenas um elemento tenha a classe .completed
+  let lista = document.querySelectorAll('li');
+  //console.log(lista)  
   for(let index=0;index<lista.length; index+=1){
     if(lista[index].className === 'completed'){
       console.log('oi');
@@ -45,4 +47,23 @@ listaOrdenada.addEventListener('dblclick',function(event){
     listaOrdenada.addEventListener('dblclick', function(){
       event.target.classList.remove('completed')
     })
+})
+
+
+//Requisito 10:
+apagaTudo.addEventListener('click',function(event){
+  let lista = document.querySelectorAll('li');
+  for(let index = 0; index < lista.length; index += 1){
+    lista[index].parentNode.removeChild(lista[index]);
+  }
+})
+
+//Requisito 11:
+deletar.addEventListener('click',function(event){
+  let lista = document.querySelectorAll('li');
+  for(let valor of lista){
+    if(valor.className ==='completed'){
+      valor.parentNode.removeChild(valor);
+    }
+  }
 })
