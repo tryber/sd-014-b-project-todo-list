91bf206ecb9fa809d18ec08293ad161ec74fab1e
Paulo França <paulovinicius.franca@gmail.com>
2021-07-12 18:51:56 -0300

    Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("olá!")
+const button = document.querySelector('#criar-tarefa');
+button.addEventListener('click', addListItem);
+
+function addListItem() {
+  const input = document.querySelector('#texto-tarefa');
+  const list = document.querySelector('#lista-tarefas')
+  const listItem = document.createElement('li');
+  console.log(input.value);
+  listItem.innerText = input.value;
+  list.appendChild(listItem);
+  input.value = '';
+}
