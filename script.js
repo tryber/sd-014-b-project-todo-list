4ec9b07587ba4b39916d77956396d853d682e5eb
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-07-11 23:29:46 -0300

    risca tarefa com double click

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function addTask() {
   li.className = 'tarefa';
   list.appendChild(li);
   selecItem();
+  endTask();
   li.innerText = input.value;
   input.value = '';
   console.log(li);
@@ -16,13 +17,22 @@ function addTask() {
 function selecItem() {
   let li = document.querySelectorAll('.tarefa');
   for (let i = 0; i < li.length; i += 1) {
-    console.log(i);
-    let element = li[i];
+    const element = li[i];
     element.addEventListener('click', function printLi() {
       li.forEach((element) => {
         element.style.backgroundColor = 'white';
       });
       element.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+
+function endTask() {
+  let li = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < li.length; i++) {
+    const element = li[i];
+    element.addEventListener('dblclick', function risca(event) {
+      event.target.classList.add('riscado');
     });
   }
 }
