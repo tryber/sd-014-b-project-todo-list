659bb9d5e4d42f9c781974a6c19626f9ce0cd8ec
DevJunior21 <junior-wmw@hotmail.com>
2021-07-11 21:38:05 -0400

    chegando até o requisito 8

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const lista = document.getElementById('lista-tarefas')
 
-
-    
-
-
 function clicar() {
     const botao = document.getElementById('criar-tarefa')
         botao.addEventListener('click', () => {
             let stringui = document.getElementById('texto-tarefa').value;
                 const tucano = document.createElement('li')
+                    tucano.className= ('listinha')
                     tucano.innerText = stringui
                         lista.appendChild(tucano)
                             document.getElementById('texto-tarefa').value=''
-                           
-                            
-
-                
+                                tucano.onclick = function(){
+                                    tucano.style.backgroundColor = '#808080'
+                                }
         
-    }
+        }
+        
+        
     )
 
 }
