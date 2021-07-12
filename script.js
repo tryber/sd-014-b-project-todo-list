65d6115944785f8829070e757862d65fee0eda6e
Ali21ne <bezerraalineg@gmail.com>
2021-07-12 18:40:24 -0300

    resquisitos 7, 8, 9

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button1 = document.querySelector('button');
 const parentLi = document.querySelector('#lista-tarefas');
 const input1 = document.querySelector('input');
+const ols = document.querySelector('ol');
 
 function list() {
   const lis = document.createElement('li');
@@ -10,3 +11,13 @@ function list() {
 }
 
 button1.addEventListener('click', list);
+
+function addCor(event) {
+  const allLi = document.querySelectorAll('li');
+  for (const index of allLi) {
+    index.style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = ' rgb(128, 128, 128)';
+}
+
+ols.addEventListener('click', addCor);
