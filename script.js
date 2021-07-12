a1ed49b2517c4f353aef21fea9da75230305dbbf
Ali21ne <bezerraalineg@gmail.com>
2021-07-12 18:18:03 -0300

    requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-  let h1 = document.createElement('h1');  
-      h1.innerHTML = "Minha Lista de Tarefas";
-      title.appendChild('h1');
+const button1 = document.querySelector('button');
+const parentLi = document.querySelector('#lista-tarefas');
+const input1 = document.querySelector('input');
+
+function list() {
+  const lis = document.createElement('li');
+  lis.innerText = input1.value;
+  parentLi.appendChild(lis);
+  input1.value = '';
+}
+
+button1.addEventListener('click', list);
