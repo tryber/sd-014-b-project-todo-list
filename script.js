26801d3baf91371c3a2aa06b8f053349833d60e8
jarana-2020 <julioaranabr@yahoo.com.br>
2021-07-12 13:16:37 -0300

    Requisito #8

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-constant-condition */
+function alterColor() {
+  const getTasks = document.querySelectorAll('.task');
+  for (let index = 0; index < getTasks.length; index += 1) {
+    getTasks[index].classList.remove('selected');
+  }
+}
+// eslint-disable-next-line max-lines-per-function
 function addTask() {
   const getOl = document.querySelector('#lista-tarefas');
   const getButtonTask = document.querySelector('#criar-tarefa');
@@ -8,8 +16,9 @@ function addTask() {
     createLi.className = 'task';
     createLi.innerHTML = getInput.value;
     createLi.addEventListener('click', (event) => {
+      alterColor();
+      event.target.classList.add('selected');
       // eslint-disable-next-line no-param-reassign
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
     });
     getOl.appendChild(createLi);
     getInput.value = '';
