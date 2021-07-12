383772b3cebcbe9c9654f339d6a62a472404c4af
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-07-12 15:32:44 -0300

    Atende requisito 13 fechando os 200%

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const saveListButton = document.getElementById('salvar-tarefas');
 const removeSelectedTaskButton = document.getElementById('remover-selecionado');
 let savedListItems = document.getElementById('lista-tarefas');
 savedListItems.innerHTML = localStorage.getItem('ol');
+const upButton = document.getElementById('mover-cima');
+const downButton = document.getElementById('mover-baixo');
 
 
 
@@ -20,9 +22,9 @@ function generateTask(){
     const inputSection = document.getElementById('inputSection');
    inputSection.removeChild(deletedInput);
    const newInput = document.createElement('input');
-   newInput.id ="texto-tarefa";
+   newInput.id = "texto-tarefa";
    newInput.placeholder="Digite uma tarefa";
-    inputSection.appendChild(newInput);
+   inputSection.appendChild(newInput);
 
 }
 
@@ -87,3 +89,51 @@ function removeSelectedTask(){
 
 
 removeSelectedTaskButton.addEventListener('click', removeSelectedTask);
+
+
+
+
+function upTask(){
+    let seletedTask = document.getElementsByTagName('li');
+    let tempValue = "";
+    for(let index = 0; index < seletedTask.length; index += 1){
+        if(seletedTask[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+            if(seletedTask[index].previousSibling === null){
+                seletedTask[index].innerText = seletedTask[index].innerText;
+            }else{
+                tempValue = seletedTask[index].innerText;
+                seletedTask[index].innerText = seletedTask[index].previousSibling.innerText;
+                seletedTask[index].style.backgroundColor = 'rgb(255, 255, 255)';
+                seletedTask[index].previousSibling.innerText = tempValue;
+                seletedTask[index].previousSibling.style.backgroundColor = 'rgb(128, 128, 128)';
+                }
+        }
+    }
+
+}
+
+upButton.addEventListener('click', upTask);
+
+
+
+function downTask(){
+    let seletedTask = document.getElementsByTagName('li');
+    let tempValue = "";
+    for(let index = seletedTask.length -1; index >=0; index -= 1){
+        if(seletedTask[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+            if(seletedTask[index].nextSibling === null){
+                seletedTask[index].innerText = seletedTask[index].innerText;
+            }else{
+                tempValue = seletedTask[index].innerText;
+                seletedTask[index].innerText = seletedTask[index].nextSibling.innerText;
+                seletedTask[index].style.backgroundColor = 'rgb(255, 255, 255)';
+                seletedTask[index].nextSibling.innerText = tempValue;
+                seletedTask[index].nextSibling.style.backgroundColor = 'rgb(128, 128, 128)';
+            }
+        }
+    }
+
+}
+
+
+downButton.addEventListener('click', downTask);
