0c876d2a3f16e5a9e6cc742fd8cc032ac5f42a10
samsantosb <samsantosb@outlook.com>
2021-07-12 17:23:15 -0300

    refazendo sintaxe

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ const grey = (x) => x.addEventListener('click', (e) => {
   if (query('.color')) query('.color').classList.remove('color');
   e.target.classList.add('color');
 });
-const mark = (y) => {
-  y.addEventListener('dblclick', (e) => {
-    if (e.target.classList.contains('completed')) e.target.classList.remove('completed');
-    else e.target.classList.add('completed');
-  });
-}
-const newTask = () => {
+const mark = (y) => y.addEventListener('dblclick', (e) => {
+  e.target.classList.contains('completed') ? e.target.classList.remove('completed') :
+    e.target.classList.add('completed');
+});
+query('#criar-tarefa').addEventListener('click', () => {
   const newItem = document.createElement('li');
   grey(newItem), mark(newItem);
   newItem.innerText = query('#texto-tarefa').value;
   query('#lista-tarefas').appendChild(newItem);
   query('#texto-tarefa').value = null;
-}
-query('#criar-tarefa').addEventListener('click', newTask);
+});
 query('#apaga-tudo').addEventListener('click', () => queryAll('#lista-tarefas').forEach((element) => element.remove()));
 query('#remover-finalizados').addEventListener('click', () => {
   queryAll('.completed').forEach((element) => element.remove());
