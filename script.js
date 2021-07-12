d71e90481fef50e3b8366594cc5857897ef3c4be
Paulo França <paulovinicius.franca@gmail.com>
2021-07-12 19:12:21 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ const button = document.querySelector('#criar-tarefa');
 button.addEventListener('click', addListItem);
 
 function addListItem() {
+  const list = document.querySelector('#lista-tarefas');
   const input = document.querySelector('#texto-tarefa');
-  const list = document.querySelector('#lista-tarefas')
   const listItem = document.createElement('li');
-  console.log(input.value);
   listItem.innerText = input.value;
+  listItem.className = 'tarefa'
   list.appendChild(listItem);
   input.value = '';
+  selectItem();
+}
+
+function selectItem() {
+  let getItem = document.querySelectorAll('.tarefa');
+  for (let index = 0; index <= getItem.length - 1; index += 1) {
+    getItem[index].addEventListener('click', changeColor);
+    console.log(getItem[index]);
+  }
+}  
+
+function changeColor(item) {
+  item.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
