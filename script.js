8dadbb67b6397b698e363a5bebf11e7f5c9aa470
Daniel <daniel.acoelho@outlook.com>
2021-07-12 18:33:23 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const addButton = document.querySelector('#criar-tarefa');
-const insertText = document.querySelector('#texto-tarefa');
-const taskList = document.getElementById('lista-tarefas');
+const inputText = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
 
 addButton.addEventListener('click', function() {
-    let createList = document.createElement('li');
-    if (insertText.value !== '') {
-        createList.innerText = insertText.value;
-        createList.className = 'listTask';
-        createList.addEventListener('click', function(){
-            if (createList.style.backgroundColor !== 'rgb(128, 128, 128)') {
-                createList.style.backgroundColor = 'rgb(128, 128, 128)';
-            }
-        });
-        taskList.appendChild(createList);
-    }    
-    insertText.value = ''; 
+  const createList = document.createElement('li');
+  const liBckgdColor = taskList.appendChild(createList);
+  liBckgdColor.innerText = inputText.value;
+  liBckgdColor.style.backgroundColor = 'transparent';
+  inputText.value = '';
 });
-   
+
+function selection(event) {
+  const selec = event;
+  for (let index = 0; index < taskList.children.length; index += 1) {
+    if (selec.target.parentElement.children[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      selec.target.parentElement.children[index].style.backgroundColor = 'transparent';
+    }
+    if (selec.target.style.backgroundColor === 'transparent') {
+      selec.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  }
+}
+
+taskList.addEventListener('click', selection);
+
