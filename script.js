56633e17bfe4d4d384acb31275ea8c4f342fcb26
Vinicius <vinicius@DESKTOP-NBO50A7.localdomain>
2021-07-13 12:11:00 -0300

    Requisito #7 - Adicionando Cor no item selecionado

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function clickButton() {
- const btn = document.querySelector('#criar-tarefa');
- btn.addEventListener('click', addList);
+  const btn = document.querySelector('#criar-tarefa');
+  btn.addEventListener('click', addTarefa);
 }
 clickButton();
 
-function addList() {
+function addTarefa() {
   const addList = document.querySelector('#lista-tarefas');
   const createNew = document.createElement('li');
-  let tarefaAdd = document.querySelector('#texto-tarefa').value;
+  const tarefaAdd = document.querySelector('#texto-tarefa').value;
   createNew.innerHTML = tarefaAdd;
-  createNew.classList.add('tarefa');
   addList.appendChild(createNew);
   document.querySelector('#texto-tarefa').value = '';
+  console.log(tarefaAdd + ' adicionado!');
+  selectTarefa();
+}
+
+function selectTarefa() {
+  const tarefaSelecionada = document.querySelectorAll('li');
+  for (let i = 0; i < tarefaSelecionada.length; i += 1) {
+    tarefaSelecionada[i].addEventListener('click', () => {
+      console.log("hm");
+      tarefaSelecionada[i].classList.add('tarefa');
+    });
+  }
 }
