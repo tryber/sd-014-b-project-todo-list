f9e55c8d6abede8f4fde976408f6eb7c0c778316
Matheus Laurindo <matheus104521@gmail.com>
2021-07-13 11:21:49 -0300

    Atualizaçao dos erros ESLINT

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function changeColor(evento) {
   const listItems = document.querySelectorAll('.tasks');
   const itemClicked = evento.target;
+  for (let index = 0; index < listItems.length; index += 1) {
+    const item = listItems[index];
 
-  for (const key of listItems) {
-    const item = key;
-
-    // Muda a cor de todos para branco 
+    // Muda a cor de todos para branco
     item.style.backgroundColor = 'rgb(0, 0, 0, 0)';
 
-    // Seta a cor do item clickado para cinza  
+    // Seta a cor do item clickado para cinza
     itemClicked.style.backgroundColor = 'rgb(128, 128, 128)';
   }
-} 
+}
 
 function taskCompleted(evento) {
   const itemClicked = evento.target;
   const searchClassCompleted = itemClicked.classList.contains('completed');
 
-  // Verifica se possui a classe 'completed', se sim, remove, se não, adiciona 
+  // Verifica se possui a classe 'completed', se sim, remove, se não, adiciona
   if (searchClassCompleted === true) {
     itemClicked.classList.remove('completed');
   } else {
@@ -26,26 +25,26 @@ function taskCompleted(evento) {
 }
 
 function eventTasks() {
-  const listTasks = document.querySelectorAll('.tasks');
+  const listItems = document.querySelectorAll('.tasks');
 
-  for (const key of listTasks) {
-    const task = key;
+  for (let index = 0; index < listItems.length; index += 1) {
+    const task = listItems[index];
     task.addEventListener('click', changeColor);
     task.addEventListener('dblclick', taskCompleted);
   }
 }
 
-// Cria os elementos li, dentro da lista   
-function createTask() { 
-  // Faz a ligação da OL e do Input Text 
+// Cria os elementos li, dentro da lista
+function createTask() {
+  // Faz a ligação da OL e do Input Text
   const listTask = document.querySelector('#lista-tarefas');
   const txtTask = document.querySelector('#texto-tarefa').value;
 
-  if (txtTask.length === 0) { 
+  if (txtTask === '') {
     alert('[ERRO] Campo vazio!');
   } else {
-    // Cria o elemento, adiciona o texto inserido no input 
-    // Adiciona classe, evento de clique e adiciona no html  
+    // Cria o elemento, adiciona o texto inserido no input
+    // Adiciona classe, evento de clique e adiciona no html
     const itemTask = document.createElement('li');
     itemTask.innerHTML = txtTask;
     itemTask.className = 'tasks';
@@ -81,47 +80,37 @@ window.onload = function initialPage() {
   }
 };
 
-const btnClearTasks = document.querySelector('#apaga-tudo');
-btnClearTasks.addEventListener('click', clearTasks);
-
 function clearTasks() {
   localStorage.clear();
-  const listTask = document.querySelectorAll('.tasks');
+  const listItems = document.querySelectorAll('.tasks');
 
-  for (const key of listTask) {
-    const task = key;
-
+  for (let index = 0; index < listItems.length; index += 1) {
+    const task = listItems[index];
     task.parentNode.removeChild(task);
   }
 }
+const btnClearTasks = document.querySelector('#apaga-tudo');
+btnClearTasks.addEventListener('click', clearTasks);
 
 function saveTasks() {
-  const listTasks = document.querySelectorAll('.tasks');
+  const listItems = document.querySelectorAll('.tasks');
 
-  for (let index = 0; index < listTasks.length; index += 1) {
-    const task = listTasks[index];
-    localStorage.setItem('name' + index, task.outerHTML);
+  for (let index = 0; index < listItems.length; index += 1) {
+    const task = listItems[index];
+    localStorage.setItem(`name${index}`, task.outerHTML);
   }
 }
 const btnSalvarTarefas = document.querySelector('#salvar-tarefas');
 btnSalvarTarefas.addEventListener('click', saveTasks);
 
 function clearCompleted() {
-  saveTasks();
-  const listTask = document.querySelectorAll('.tasks');
-  for (const key of listTask) {
-    const task = key;
-    const searchClassCompleted = task.classList.contains('completed'); // Retorna true se o elemento tiver a classe 'completed'
-    if (searchClassCompleted === true ) { // Se tiver, remove o elemento da lista e do Storage
-      task.parentNode.removeChild(task);
-    }
-  }
+  const listItems = document.querySelectorAll('.tasks');
 
-  for (let index = 0; index < localStorage.length; index += 1) {
-    const itemRemove = localStorage.getItem(`name${index}`);
-    const searchCompletedRemove = itemRemove.includes('completed');
-    if (searchCompletedRemove === true) {
-      localStorage.removeItem(`name${index}`);
+  for (let index = 0; index < listItems.length; index += 1) {
+    const task = listItems[index];
+    const searchClassCompleted = task.classList.contains('completed'); // Retorna true se o elemento tiver a classe 'completed'
+    if (searchClassCompleted === true) { // Se tiver, remove o elemento da lista e do Storage
+      task.parentNode.removeChild(task);
     }
   }
 }
