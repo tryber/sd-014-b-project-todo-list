a3c0e9c6ed6da948366395b308aea96299574e37
Ali21ne <bezerraalineg@gmail.com>
2021-07-13 11:29:30 -0300

    correçao add 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button1 = document.querySelector('button');
 const parentLi = document.querySelector('#lista-tarefas');
 const input1 = document.querySelector('input');
 const ols = document.querySelector('ol');
+const body = document.querySelector('body');
 
 function list() {
   const lis = document.createElement('li');
@@ -21,3 +22,25 @@ function addCor(event) {
 }
 
 ols.addEventListener('click', addCor);
+
+//referencia por Bel Albuquerque
+//
+
+function sublinha(event) {
+  event.target.classList.toggle('completed');
+}
+
+ols.addEventListener('dblclick', sublinha);
+
+let button2 = document.createElement('button');
+button2.setAttribute("id", "apaga-tudo");
+button2.innerText = 'Apaga Tudo';
+body.appendChild(button2)
+
+function apaga(){
+  const ol = document.querySelector('ol');
+   ol.innerHTML = ''
+}
+button2.addEventListener('click', apaga)
+
+
