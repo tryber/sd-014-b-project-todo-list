162994f82db293b3aaf47aa73f13c221dc4bcb97
Luana Eugênia <luaeugenia1309@outlook.com>
2021-07-14 11:16:21 -0300

    Requisito 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
+function adicionarCor(event) {
+  const elemento = event.target;
+  elemento.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function adicionarTarefa() {
   const listaTarefas = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
   const inputTextoTarefa = document.getElementById('texto-tarefa');
   li.innerHTML = inputTextoTarefa.value;
+  li.addEventListener('click', adicionarCor);
   inputTextoTarefa.value = '';
   listaTarefas.appendChild(li);
 }
