a6dc1c72617e2d7cde68af2e0533b3c6cc530df0
Paulo França <paulovinicius.franca@gmail.com>
2021-07-14 20:23:50 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const addButton = document.querySelector('#criar-tarefa');
+const saveButton = document.querySelector('#salvar-tarefas');
 const eraseButton = document.querySelector('#apaga-tudo');
 const eraseCompletedTaskButton = document.querySelector('#remover-finalizados');
 const killButton = document.querySelector('#remover-selecionado');
 const list = document.querySelector('#lista-tarefas');
+
+// Verificar o funcionamento da saveList e eraseList. Havia feito de uma forma muito mais complexa, mas o code review do Esdras mostrou que seria muito mais simples!
+
+window.onload = function load() {
+  list.innerHTML = localStorage.getItem('list');
+  selectItem();
+};
 
 function changeColor(item) {
   const selectedItem = document.querySelectorAll('.tarefa');
@@ -29,32 +37,32 @@ function selectItem() {
 }
 
 function addListItem() {
-  const input = document.querySelector('#texto-tarefa');
-  const listItem = document.createElement('li');
-  listItem.innerText = input.value;
-  listItem.className = 'tarefa';
-  list.appendChild(listItem);
-  input.value = '';
-  selectItem();
+  const input = document.querySelector('#texto-tarefa');  
+  if (input.value === '') {
+    alert('Inserir um valor!');
+  } else {
+    const listItem = document.createElement('li');
+    listItem.innerText = input.value;
+    listItem.className = 'tarefa';
+    list.appendChild(listItem);
+    input.value = '';
+    selectItem();
+  }
 }
 
 function eraseList() {
-  const selectedItem = document.querySelectorAll('.tarefa');
-  for (let index = 0; index <= selectedItem.length - 1; index += 1) {
-    list.removeChild(selectedItem[index]);
-  }
+  list.innerHTML = '';
 }
 
 function removeListItem() {
   const selectedItem = document.querySelectorAll('.tarefa');
-  for (index = 0; index <= selectedItem.length - 1; index += 1) {
+  for (let index = 0; index <= selectedItem.length - 1; index += 1) {
     if (selectedItem[index].style.backgroundColor === 'rgb(128, 128, 128)') {
       const getlist = document.querySelector('#lista-tarefas');
       getlist.removeChild(selectedItem[index]);
     }
   }
 }
-
 
 function removeCompleted() {
   const getCompletedItem = document.querySelectorAll('.completed');
@@ -63,7 +71,12 @@ function removeCompleted() {
   }
 }
 
+function saveList() {
+  localStorage.setItem('list', list.innerHTML);
+}
+
 addButton.addEventListener('click', addListItem);
+saveButton.addEventListener('click', saveList);
 eraseButton.addEventListener('click', eraseList);
 eraseCompletedTaskButton.addEventListener('click', removeCompleted);
-killButton.addEventListener('click', removeListItem)
+killButton.addEventListener('click', removeListItem);
