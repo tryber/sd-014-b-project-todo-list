bcc794086726d7326789336adc5300d4c17c8f6a
Vitor Lima <vitorliman@gmail.com>
2021-07-14 13:09:44 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputText = document.querySelector('#texto-tarefa');
 const listaOrde = document.querySelector('#lista-tarefas');
 const button = document.querySelector('#criar-tarefa');
 const buttonErase = document.querySelector('#apaga-tudo');
+const buttonFinalizados = document.querySelector('#remover-finalizados');
 
 button.addEventListener('click', function() {
   let li = document.createElement('li');
@@ -27,5 +28,15 @@ buttonErase.addEventListener('click', function() {
   let list = document.querySelectorAll('li');
   for (let index = 0; index < list.length; index += 1) {
     list[index].remove('li');
+  }
+})
+
+buttonFinalizados.addEventListener('click', function() {
+  const list = document.querySelectorAll('#lista-tarefas>li');
+  // const completed = document.querySelector('.completed');
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index].classList.contains('completed')) {
+      list[index].parentNode.removeChild(list[index]);
+    }
   }
 })
