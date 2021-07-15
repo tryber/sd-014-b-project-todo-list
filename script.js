5a7447ca886d687b6029ce5b8430adf4e0afe8b7
Bruno M Landim <brunolandim13@hotmail.com>
2021-07-14 22:35:43 -0300

    8 Requisito

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 const addTask = document.querySelector('#criar-tarefa');
 const textInput = document.querySelector('#texto-tarefa');
-const ols = document.querySelector('#lista-tarefas')
+const itemol = document.querySelector('ol')
 
-addTask.addEventListener('click',  function () {
+
+addTask.addEventListener('click',  function createLiItem() {
     let text = textInput.value;
 
     if (text != '') {
-       let createLi = document.createElement('li');
-       createLi.innerText = text;
-       ols.appendChild(createLi);
-
-        createLi.addEventListener('click',  function() {
-        createLi.className = 'cor-de-fundo'
-    });
+        let createLi = document.createElement('li');
+        createLi.innerText = text;
+        itemol.appendChild(createLi);
     }
-    
     textInput.value = '';
+    const itemli = document.querySelectorAll('li')
+    console.log(itemli);
+    for (let index = 0; index < itemli.length; index += 1) {
+        let activeClass = itemli[index];
+        activeClass.addEventListener('click' , function() {
+            let disableClass = document.querySelector('.cor-de-fundo')
+            if (activeClass.className != 'cor-de-fundo') {
+                
+                activeClass.classList.add('cor-de-fundo')
+                disableClass.classList.remove('cor-de-fundo')
+            }
+        });
+
+    }
 });
+
+
