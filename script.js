547f72aeaa3cd65b5f7974a5ff91c1c929be5d89
Bruno M Landim <brunolandim13@hotmail.com>
2021-07-15 16:01:36 -0300

    Requisito 7 e 8 e ajustes na escrita do JS

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
-const addTask = document.querySelector('#criar-tarefa');
-const textInput = document.querySelector('#texto-tarefa');
-const itemol = document.querySelector('ol')
 
-
-addTask.addEventListener('click',  function createLiItem() {
+/* 
+addTask.addEventListener('click',function () {
     let text = textInput.value;
 
     if (text != '') {
         let createLi = document.createElement('li');
         createLi.innerText = text;
-        itemol.appendChild(createLi);
-    }
-    textInput.value = '';
-    const itemli = document.querySelectorAll('li')
-    console.log(itemli);
-    for (let index = 0; index < itemli.length; index += 1) {
-        let activeClass = itemli[index];
-        activeClass.addEventListener('click' , function() {
-            let disableClass = document.querySelector('.cor-de-fundo')
-            if (activeClass.className != 'cor-de-fundo') {
-                
-                activeClass.classList.add('cor-de-fundo')
-                disableClass.classList.remove('cor-de-fundo')
-            }
-        });
+        ols.appendChild(createLi);
 
+        createLi.addEventListener('click', function () {
+            let liItem = document.querySelectorAll('li');
+            for(let index = 0; index < liItem)
+        });
     }
-});
+    
+    textInput.value = '';
+});  */
+let button = document.querySelector('#criar-tarefa');
+let ol = document.querySelector('#lista-tarefas');
+
+function createLi () {
+    let textInput = document.querySelector('#texto-tarefa');
+    let text = textInput.value;
+    let createElementLi = document.createElement('li');
+    
+    createElementLi.innerText = text;
+    ol.appendChild(createElementLi);
+    textInput.value = '';
+// abaixo é verificado se o elemento possui a classe cor-de-fundo
+    createElementLi.addEventListener('click' , function (event) {
+        let liItem = document.querySelectorAll('li');
+
+        for(let index = 0; index < liItem.length; index += 1) {
+            liItem[index].style.backgroundColor = 'white';
+        }
+        event.target.style.backgroundColor = 'rgb(128,128,128)';
+    });
+}
+button.addEventListener('click' , createLi);
+
+
+
+
+
+
+
 
 
