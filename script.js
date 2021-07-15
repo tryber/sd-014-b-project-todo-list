4f5aa4adf5080e96904f99a1ab846aadfc8d6610
Jucélio <juceneon@gmail.com>
2021-07-14 22:13:57 -0300

    Requisito 13

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const apagaTudo = document.querySelector('#apaga-tudo');
 const apagaFinalizados = document.querySelector('#remover-finalizados');
 const apagaSelecionado = document.querySelector('#remover-selecionado');
 const salvarTarefas = document.querySelector('#salvar-tarefas');
+const moverCima = document.querySelector('#mover-cima');
+const moverBaixo = document.querySelector('#mover-baixo');
 
 lista.innerHTML = localStorage.getItem('lista');
 
@@ -62,6 +64,26 @@ function saveTasks() {
   localStorage.setItem('lista', lista.innerHTML);
 }
 
+function moveUp() {
+  const itemSelecionado = document.querySelector('.selected');
+  lista.insertBefore(itemSelecionado, itemSelecionado.previousSibling);
+}
+// Gastei um pouco de tempo, mas descobri a função insertBefore() e previousSibling que funcionam muito bem um com o outro, Source: https://www.w3schools.com/jsref/met_node_insertbefore.asp e https://www.w3schools.com/jsref/prop_node_previoussibling.asp
+
+function moveDown() {
+  const itemSelecionado = document.querySelector('.selected');
+  const newItem = document.createElement('li');
+  newItem.innerText = itemSelecionado.innerText;
+  newItem.classList.add('tarefa');
+  newItem.classList.add('selected')
+  newItem.addEventListener('click', selectItem);
+  newItem.addEventListener('dblclick', completeItem);
+  itemSelecionado.nextSibling.insertAdjacentElement('afterend', newItem);
+  lista.removeChild(itemSelecionado);
+}
+
+// Aqui eu gastei mais tempo ainda, acredito que tem uma forma mais simples de fazer, mas depois de quebrar a cabeça foi a forma que encontrei ;c,  Source: https://www.w3docs.com/snippets/javascript/how-to-insert-an-element-after-another-element-in-javascript.html
+
 createList.addEventListener('click', createItem);
 
 apagaTudo.addEventListener('click', eraseAll);
@@ -71,3 +93,7 @@ apagaFinalizados.addEventListener('click', eraseCompleted);
 apagaSelecionado.addEventListener('click', eraseSelected);
 
 salvarTarefas.addEventListener('click', saveTasks);
+
+moverCima.addEventListener('click', moveUp);
+
+moverBaixo.addEventListener('click', moveDown);
