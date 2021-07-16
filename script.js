fb87a8bbc3e42e31c95f62a49a812b0546a19849
mahnetti <mariah.manetti@gmail.com>
2021-07-16 11:40:48 -0300

    Desafio 8

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-//Adicionando tarefa pelo botão
+// Adicionando tarefa pelo botão
 const botaoAdd = document.querySelector('#criar-tarefa');
 
 function addTarefa() {
   const inputTask = document.querySelector('#texto-tarefa');
   const list = document.querySelector('#lista-tarefas');
   const listTask = document.createElement('li');
-
+  
   list.appendChild(listTask);
   listTask.innerText = inputTask.value;
+  listTask.classList.add('task');
   inputTask.value = '';
 }
 
 botaoAdd.addEventListener('click', addTarefa);
 
-//troca cor do funda da tarefa
+// troca cor do funda da tarefa
 const colorTask = document.querySelector('#lista-tarefas');
 
-function changeColor (event) {
+function changeColor(event) {
+  const classSelector = document.querySelector('.background-task');
   event.target.classList.add('background-task');
+  if (
+    classSelector !== null && classSelector !== event.target
+  )
+  classSelector.classList.remove('background-task');
 }
 colorTask.addEventListener('click', changeColor);
-
-// function changeColor() {
-//   document.querySelector('li').style.backgroundColor = 'rgb(128, 128, 128)';
-// }
-// document.addEventListener('click', changeColor);
