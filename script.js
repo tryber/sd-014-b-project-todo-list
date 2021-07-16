553aae71b3aa98b36ae5e878f49b2cebb5741d44
Arturo Vaine <arturo.vaine@gmail.com>
2021-07-15 21:45:08 -0300

    Todos os requisitos OK.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// OK - 1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
-// OK - 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
-// OK - 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
+// 1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
+// 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+// 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
 
 // 4 - Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
 
 // 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
 
 const taskList = document.querySelector('#lista-tarefas'); // Atribuir elemento de id lista-tarefas à variável taskList
-
+// 6 - Ordene os itens da lista de tarefas por ordem de criação
 function newTask() {
   const task = document.createElement('li'); // Criar elemento de item listado (li) no document
   taskList.appendChild(task); //  Adiciona elemento task (tarefa do input) como elemento filho do elemento pai taskList
@@ -18,21 +18,13 @@ function newTask() {
   document.getElementById('texto-tarefa').value = ''; // Limpar texto do input
 }
 
-// 9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item
-function checkTask(event) {
-  if (event.target.classList.value.includes('completed')) {
-    event.target.classList.remove('completed');
-  } else {
-    event.target.classList.add('completed')};
-  console.log(event.target);
-}
+const buttonCreateTask = document.querySelector('#criar-tarefa');
+buttonCreateTask.addEventListener('click', newTask);
 
-const dbClickTask = document.querySelector('ol');
-dbClickTask.addEventListener('dblclick', checkTask);
-
+// 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)--> CSS
 function selectTask(selectEvent) {
   if (document.querySelector('.selected') != null) {
-    document.querySelector('.selected').classList.remove('selected');
+    document.querySelector('.selected').classList.remove('selected'); // 8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
   }
   selectEvent.target.classList.add('selected');
   console.log(selectEvent.target);
@@ -41,10 +33,23 @@ function selectTask(selectEvent) {
 const singleClickTask = document.querySelector('ol');
 singleClickTask.addEventListener('click', selectTask);
 
+// 9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item
+function checkTask(event) {
+  if (event.target.classList.value.includes('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+  console.log(event.target);
+}
+
+const dbClickTask = document.querySelector('ol');
+dbClickTask.addEventListener('dblclick', checkTask);
+
 // 10 - Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista
 
 function removeAll() {
-  while(taskList.lastElementChild != null) {
+  while (taskList.lastElementChild != null) {
     taskList.removeChild(taskList.lastElementChild);
   }
 }
@@ -52,8 +57,20 @@ function removeAll() {
 const apagarTudo = document.getElementById('apaga-tudo');
 apagarTudo.addEventListener('click', removeAll);
 
+// 11 - Adicione um botão com id="remover-finalizados" que quando clicado remove somente os elementos finalizados da sua lista
+function removeCompletedTasks() {
+  const tasksCompleted = document.querySelectorAll('.completed'); // NodeList: seleção de todos os elementos 'completed'
+  tasksCompleted.forEach((li) => { // Arrow function com forEach
+    li.parentElement.removeChild(li); // Remover child nodes ---> remover nós da lista de 'completed'
+  });
+}
+
+const buttonRemoveCompletedTasks = document.getElementById('remover-finalizados');
+buttonRemoveCompletedTasks.addEventListener('click', removeCompletedTasks);
+
 // function removeSelected()
 
+// 12 - Adicione um botão com id="salvar-tarefas" que salve o conteúdo da lista. Se você fechar e reabrir a página, a lista deve continuar no estado em que estava
 function saveTasks() {
   localStorage.setItem('taskList', taskList.innerHTML);
 }
@@ -69,20 +86,14 @@ function recoverAllTasks() {
 
 recoverAllTasks();
 
-function removeCompletedTasks() {
-  const tasksCompleted = document.querySelectorAll('completed');
-  for (let i = 0; i < tasksCompleted.length; i += 1) {
-    if (tasksCompleted.lastElementChild != null) {
-      tasksCompleted.removeChild(tasksCompleted.lastElementChild);
-    }
-  }
-}
-
-const buttonRemoveCompletedTasks = document.getElementById('remover-finalizados');
-buttonRemoveCompletedTasks.addEventListener('click', removeCompletedTasks);
-
 // Lógica das funções de movimentação das tarefas (up-down) foi baseada no trabalho do colega Dheniarley Cruz
+// Referências adicionais:
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_node_insertbefore
+// https://www.w3schools.com/jsref/met_node_insertbefore.asp
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_foreach
+// https://www.w3schools.com/jsref/jsref_foreach.asp
 
+// 13 - Adicione dois botões, um com id="mover-cima" e outro com id="mover-baixo", que permitam mover o item selecionado para cima ou para baixo na lista de tarefas
 function moveUp() {
   const taskSelected = document.querySelector('li.selected'); // Objeto 'taskSelected': seleção do elemento de tarefa selecionado com single click
   const tasksListItems = document.querySelectorAll('li'); // NodeList: seleção de todos os elementos de tarefas, list-items
@@ -118,3 +129,14 @@ buttonMoveDown.addEventListener('click', moveDown);
 
 const buttonMoveUp = document.getElementById('mover-cima');
 buttonMoveUp.addEventListener('click', moveUp);
+
+// 14 - Adicione um botão com id="remover-selecionado" que, quando clicado, remove o item selecionado
+function removeSelectedTask() {
+  const tasksCompleted = document.querySelectorAll('.selected'); // NodeList: seleção de todos os elementos 'completed'
+  tasksCompleted.forEach((li) => { // Arrow function com forEach
+    li.parentElement.removeChild(li); // Remover child nodes ---> remover nós da lista de 'completed'
+  });
+}
+
+const buttonRemoveSelectedTask = document.getElementById('remover-selecionado');
+buttonRemoveSelectedTask.addEventListener('click', removeSelectedTask);
