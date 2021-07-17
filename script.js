fd2c2095efae527e30a74cd3178a25e7f143de03
Paola Gay <paolagaydossantos@gmail.com>
2021-07-17 17:38:20 -0300

    Item 7 e 8

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function moreTask() {
   const enterTask = document.querySelector('#texto-tarefa');
   const list = document.querySelector('#lista-tarefas');
   const listTask = document.createElement('li');
-
+  listTask.className = 'itensList';
   list.appendChild(listTask);
   listTask.innerText = enterTask.value;
   enterTask.value = '';
@@ -12,17 +12,37 @@ function moreTask() {
 
 buttonOne.addEventListener('click', moreTask);
 
-/*
-const botaoAdd = document.querySelector('#criar-tarefa');
+function testeAdd(event) {
+  console.log('deu');
+  itensList = document.querySelector('.itensList.background');
+  if (itensList === null) {
+    event.target.classList.add('background');
+  } else {
+    const itemSelect = document.querySelector('.background');
+    itemSelect.classList.remove('background');
+    event.target.classList.add('background');
+  }
+}
+document.querySelector('#lista-tarefas').addEventListener('click', testeAdd);
 
-function addTarefa() {
-  const inputTask = document.querySelector('#texto-tarefa');
-  const list = document.querySelector('#lista-tarefas');
-  const listTask = document.createElement('li');
-
-  list.appendChild(listTask);
-  listTask.innerText = inputTask.value;
-  inputTask.value = '';
+function deleteItens() {}
+document.querySelector('#apaga-tudo').addEventListener('click', deleteItens);
+/*const itens = document.querySelectorAll('itensList');
+listTask.className = 'itensList';
+for (let i = 0; i < itens.length; i++) {
+  itens[i].onclick = () => {
+    itens[i].className = 'background';
+  };
 }
 
-botaoAdd.addEventListener('click', addTarefa); */
+const itens = document.querySelectorAll('itensList');
+
+for (let i = 0; i < itens.length; i++) {
+  const el = itens[i];
+  el.onclick = () => {
+    for (let j = 0; j < itens.length; j++) {
+      const color = itens[j] === el ? 'red' : 'inherit';
+      itens[j].style.color = color;
+    }
+  };
+} */
