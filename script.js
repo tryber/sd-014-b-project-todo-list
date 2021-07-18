418917e1d119562d852ab0722ab853a11211a35f
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-07-18 12:58:04 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onload=function(){
     const buttonAddTask= document.querySelector("#criar-tarefa");
-    const listTask =document.getElementById('lista-tarefas');
+    let listTask =document.getElementById('lista-tarefas');
+
     function addItem(){
         let addTask=document.getElementById('texto-tarefa').value;
         let additem=document.createElement('li');
@@ -8,6 +9,24 @@ window.onload=function(){
         listTask.appendChild(additem);
         document.querySelector('#texto-tarefa').value = ''; 
     }
-    buttonAddTask.addEventListener('click',addItem)
+
+    buttonAddTask.addEventListener('click',addItem);
+
+    function select(event){
+        if (document.querySelector('.selected') != null) {
+            document.querySelector('.selected').classList.remove('selected');
+            }
+            event.target.classList.add('selected');
+    }
+
+    function complet(){
+
+    }
+    if(listTask.childNodes!=null){
+        var lista =listTask.childNodes;
+        listTask.addEventListener('click',select);
+        listTask.addEventListener('dblclick',complet);
+       
+      }
 
 }
