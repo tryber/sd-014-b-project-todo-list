b02f66832431393bdea47d956cbadf1c16762153
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-18 14:04:01 -0400

    update code to lint standards

--- script.js
+++ script.js
@@ -1,68 +1,53 @@
 const addButton = document.getElementById('criar-tarefa');
 const allClearButton = document.getElementById('apaga-tudo');
 const doneClearButton = document.getElementById('remover-finalizados');
-let list = document.getElementById('lista-tarefas');
-let firstTime = true;
+const list = document.getElementById('lista-tarefas');
 let previousSelected;
 
-function UpdateBackgroundColor(params) 
+function UpdateBackgroundColor(params)
 {
-  let target = params.target;
-  if (previousSelected == target) return;
+  const target = params.target;
+  if (previousSelected === target) return;
+  if (previousSelected !== undefined) previousSelected.classList.toggle('cinza');
   target.classList.toggle('cinza');
-
-  if (firstTime == true)
-  {
-    firstTime = false;
-  }
-  else
-  {
-    previousSelected.classList.toggle('cinza');
-  }
   previousSelected = target;
 }
 
-function UpdateCompleteStatus(params) 
+function UpdateCompleteStatus(params)
 {
   params.target.classList.toggle('completed');
 }
 
-function CreateTask() 
+function CreateTask()
 {
-  const newTask = document.createElement('li');  
+  const newTask = document.createElement('li');
   const taskValue = document.getElementById('texto-tarefa').value;
   newTask.innerText = taskValue;
   list.appendChild(newTask);
   newTask.addEventListener('click', UpdateBackgroundColor);
-  newTask.addEventListener('dblclick', UpdateCompleteStatus)
+  newTask.addEventListener('dblclick', UpdateCompleteStatus);
   document.getElementById('texto-tarefa').value = '';
-
 }
 
-function UpdateList(clearAll) 
+function UpdateList(params) 
 {
   const size = list.children.length;
-  let count = 0;
-  for (i = 0; i < size; i += 1)
+  const target = params.target.id;
+  for (let i = 0; i < size; i += 1)
   {
-    if (clearAll) 
+    if (target === 'apaga-tudo')
     {
       list.removeChild(list.firstChild);
+      continue;
     }
-    else
+    if (list.children[i].classList.contains('completed'))
     {
-      if (list.children[count].classList.contains('completed')) 
-      {
-        list.removeChild(list.children[count]);
-      }
-      else
-      {
-        count += 1;
-      }
+      list.removeChild(list.children[i]);
+      continue;
     }
   }
 }
 
 addButton.addEventListener('click', CreateTask);
-allClearButton.addEventListener('click', function(){UpdateList(true)});
-doneClearButton.addEventListener('click', function(){UpdateList(false)});
+allClearButton.addEventListener('click', UpdateList);
+doneClearButton.addEventListener('click', UpdateList);
