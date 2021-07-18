dddb4800fe40c17f33e508c0a1211f9e62698094
bruholanda <brunaholandacunha7@gmail.com>
2021-07-18 14:45:49 -0300

    Requirements till #11; Except for final part of requirement #9

--- script.js
+++ script.js
@@ -12,7 +12,53 @@ function sendTask() {
   let line = document.createElement('li');
   //atributing textTask's value to list item's html
   line.innerHTML = textTask.value;
+  //atributing class name
+  line.classList.add('item');
   //appending list item to ordered list
   taskList.appendChild(line);
+  //"blanking" text input
   textTask.value = '';
-}
+};
+
+taskList.addEventListener('click', alterBackground);
+function alterBackground(evento) {
+  let items = document.querySelectorAll('.item');
+  //removing class "selected" from all items
+  for (let index = 0; index < items.length; index++) {
+    items[index].classList.remove('selected');
+  }
+  //adding class selected to clicked item
+  evento.target.classList.add('selected');
+};
+
+taskList.addEventListener('dblclick', lineThrough);
+function lineThrough(evento) {
+  //adding class completed to clicked item
+  evento.target.classList.add('completed');
+};
+
+//atributes the button "apaga-tudo" to "clearItAll"
+const clearItAll = document.querySelector('#apaga-tudo');
+//calls the function when clicks the button
+clearItAll.addEventListener('click', clearAll);
+
+function clearAll() {
+  //verifying if it still has a last element
+  while (taskList.lastElementChild) {
+    //removes the last element of "taskList"
+    taskList.removeChild(taskList.lastElementChild);
+  };
+};
+
+//atributes the button "apaga-tudo" to "clearItAll"
+const removeComplete = document.querySelector('#remover-finalizados');
+//calls the function when clicks the button
+removeComplete.addEventListener('click', removeCompleted);
+
+function removeCompleted() {
+  let items = document.querySelectorAll('.completed');
+
+  for (let i = 0; i < items.length; i++) {
+    items[i].parentNode.removeChild(items[i]);
+  };
+};
