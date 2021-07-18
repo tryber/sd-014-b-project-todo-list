135dc8ef20150482fe14ab6dcaf6bad5840dd996
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-07-18 01:59:41 -0300

    Requesito #13 e refatorando o codigo.

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
 const getButton = document.querySelector('#criar-tarefa');
 const getInput = document.querySelector('#texto-tarefa');
 const getOrdenedList = document.querySelector('#lista-tarefas');
+const buttonApagaTudo = document.querySelector('#apaga-tudo');
+const buttonRemoveFinalizados = document.querySelector('#remover-finalizados');
+const buttonUp = document.querySelector('#mover-cima');
+const buttonDown = document.querySelector('#mover-baixo');
+const buttonSave = document.querySelector('#salvar-tarefas');
 
-getButton.addEventListener('click', function() {
+function adicionarTarefa() {
   const criaLi = document.createElement('li');
   if (getInput.value !== '') {
     getOrdenedList.appendChild(criaLi).innerText = getInput.value;
+  } else {
+    alert('Adicione uma tarefa!');
   }
   getInput.value = '';
-
-  criaLi.addEventListener('click', gray);
-  
-  criaLi.addEventListener('dblclick', completed);
-});
-
-function gray(event) {
-  const getGray = document.querySelector('.gray');
-  event.target.classList.add('gray');
-  if (getGray) {
-    getGray.classList.remove('gray');
-  }
 }
 
-function completed(event) {
+getButton.addEventListener('click', adicionarTarefa);
+
+function alteraCor(event) {
+  const getGray = document.querySelector('.gray');
+  if (getGray !== null) {
+    getGray.classList.remove('gray');
+  }
+  event.target.classList.add('gray');
+}
+
+getOrdenedList.addEventListener('click', alteraCor);
+
+function riscaCompleto(event) {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
@@ -30,29 +37,53 @@ function completed(event) {
   }
 }
 
-const buttonApagaTudo = document.querySelector('#apaga-tudo');
+getOrdenedList.addEventListener('dblclick', riscaCompleto);
 
-buttonApagaTudo.addEventListener('click', function () {
+function apagaTudo() {
   getOrdenedList.innerHTML = '';
-});
+}
 
-const buttonRemoveFinalizados = document.querySelector('#remover-finalizados');
+buttonApagaTudo.addEventListener('click', apagaTudo);
 
-buttonRemoveFinalizados.addEventListener('click', function () {
+function removeFinalizados() {
   const ols = document.querySelectorAll('.completed');
-
   for (let index = 0; index < ols.length; index +=1) {
     ols[index].remove();
   }
-});
+}
 
+buttonRemoveFinalizados.addEventListener('click', removeFinalizados);
 
-const buttonSave = document.querySelector('#salvar-tarefas');
-
-buttonSave.addEventListener('click', function() {
+function salvarLocal() {
   localStorage.setItem('listaDeTarefas', getOrdenedList.innerHTML);
-});
+}
+
+
+buttonSave.addEventListener('click', salvarLocal);
 
 window.onload = function() {
   getOrdenedList.innerHTML = localStorage.getItem('listaDeTarefas');
-};
+}
+
+function moveCima() {
+  const getSelected = document.querySelector('.gray');
+
+  if (getOrdenedList.firstChild !== getSelected && getSelected !== null) {
+    getOrdenedList.insertBefore(getSelected, getSelected.previousSibling);
+  } else {
+    alert('Opção Invalida!');
+  }
+}
+
+buttonUp.addEventListener('click', moveCima);
+
+function moveBaixo() {
+  const getSelected = document.querySelector('.gray');
+  if (getOrdenedList.lastChild !== getSelected && getSelected !== null) {
+    getOrdenedList.insertBefore(getSelected.nextSibling, getSelected);
+  } else {
+    alert('Opção Invalida!');
+  }
+}
+
+buttonDown.addEventListener('click', moveBaixo);
