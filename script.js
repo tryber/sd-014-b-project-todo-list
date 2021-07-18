87214d96aae3ce1c3f6785637665e448686a9c5e
bruholanda <brunaholandacunha7@gmail.com>
2021-07-18 11:53:28 -0300

    Starting over;

--- script.js
+++
@@ -1,8 +0,0 @@
-const
-function enviarTarefa() {
-  const tarefa = document.querySelector('#texto-tarefa').innerText;
-  console.log(tarefa);
-}
-
-const creatTask = document.querySelector('#criar-tarefa');
-creatTask.addEventListener('click', enviarTarefa);
