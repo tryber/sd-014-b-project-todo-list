ce92df84bbe32dc2c9963d2827f6fe2b88437e80
Eliana Nunes <elianadnunes@gmail.com>
2021-07-18 19:06:00 -0300

    Adiciona Requisito 10

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const clicarBotao = document.getElementById('criar-tarefa');
+const adicionaItemNaLista = document.getElementById('criar-tarefa');
 const textoDigitado = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
 
-function recebeClick() {
+function adicionaItem() {
   const elementoDaLista = document.createElement('li');
   elementoDaLista.innerHTML = textoDigitado.value;
   lista.appendChild(elementoDaLista);
   textoDigitado.value = '';
 }
-clicarBotao.addEventListener('click', recebeClick);
+adicionaItemNaLista.addEventListener('click', adicionaItem);
+
+function apaga() {
+  lista.innerHTML = '';
+}
+const botaoDeApagar = document.getElementById('apaga-tudo');
+botaoDeApagar.addEventListener('click', apaga);
