20d470a2c4e03ddce927b735a62e8c3b3ab1017d
Vinicius TnK <viniciusbtanaka@hotmail.com>
2021-07-18 02:14:19 -0400

    requisito #7 e #8

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const button = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
+let firstTime = true;
+let previousSelected;
+
+function UpdateBackgroundColor(params) 
+{
+  let target = params.target;
+  if (previousSelected == target) return;
+  target.classList.toggle('cinza');
+
+  if (firstTime == true)
+  {
+    firstTime = false;
+  }
+  else
+  {
+    previousSelected.classList.toggle('cinza');
+  }
+  previousSelected = target;
+}
 
 function CreateTask() 
 {
@@ -7,7 +26,9 @@ function CreateTask()
   const taskValue = document.getElementById('texto-tarefa').value;
   newTask.innerText = taskValue;
   list.appendChild(newTask);
+  newTask.addEventListener('click', UpdateBackgroundColor);
   document.getElementById('texto-tarefa').value = '';
+
 }
 
 button.addEventListener('click', CreateTask)
