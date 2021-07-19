c4d6e28f6f1a06dcfeb2abe6654d95198eab3567
rphsantos1995 <rph.santos@Outlook.com>
2021-07-19 13:27:26 -0300

    Requisitos obrigatorios

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ const addBtn = document.getElementById("criar-tarefa");
 const taskList = document.getElementById("lista-tarefas");
 const listItem = document.getElementById("li");
 const removeBtn = document.getElementById("apaga-tudo");
+const removeOnlyBtn = document.getElementById("remover-finalizados");
 
-function addNewTask() {   
+
+
+function addNewTask(elem) {   
 
     let inputField = document.getElementById("texto-tarefa");
+    if(inputField.value == ""){
+        alert("Insira um valor pro campo");
+        
+    }
+    else {
     let newTask = document.createElement("li");
     newTask.innerHTML = inputField.value;
     newTask.className = "li-style";
     newTask.id = "li";
     taskList.appendChild(newTask);
     inputField.value = "";
+}
 };
 
 console.log("addNewTask()");
@@ -19,24 +28,29 @@ console.log("addNewTask()");
 function changeLiColor (eve) {
 
     let rmv = document.querySelector(".selected");
+    let divList = document.getElementById("divList");
+    let itemList = document.querySelectorAll("#li");
+
+    if (rmv == null && itemList.length == 0) {
+      divList.className = "selected";
+    }
+    if (rmv == null && itemList.length > 0) { 
+      eve.target.classList.add("selected");
+    }
     rmv.classList.remove("selected");
     eve.target.classList.add("selected");
     taskList.style.backgroundColor = "cornflowerblue";
 };
 
-console.log("changeLiColor");
+console.log("changeLiColor()");
 
 
 function completedTask (event) {
 
     let item = event.target;
- 
- item.classList.add("completed");
- console.log("Marking as completed");
-
+    item.classList.add("completed");
  if (item.classList == "li-style completed selected") {
      item.classList.remove("completed");
-     console.log("marking as uncompleted");
  }
 }
 
@@ -45,21 +59,24 @@ function deleteAll () {
     let liList = document.querySelectorAll("#li");
     
     for ( let i = 0; i < liList.length; i++){
-
        let li = document.getElementById("li");
        taskList.removeChild(li);
-       console.log("removendo itens");
-
-    }
-
-
-    
-
-    console.log(liList);
-    
-
+    }  
 }
 
+console.log("deleteAll()");
+
+function deleteCompleted () {
+    let liList = document.querySelectorAll(".completed");
+    console.log(liList);
+    for ( let i = 0; i < liList.length; i++){
+       let completedLi = document.querySelector(".completed");
+       taskList.removeChild(completedLi);
+       console.log("removendo completos")
+    }  
+  
+}
+console.log("deleteCOmpleted()");
 
 
 console.log("completedTask()");
@@ -68,3 +85,4 @@ addBtn.addEventListener("click", addNewTask);
 taskList.addEventListener("click", changeLiColor);
 taskList.addEventListener("dblclick", completedTask);
 removeBtn.addEventListener('click', deleteAll);
+removeOnlyBtn.addEventListener('click', deleteCompleted);
