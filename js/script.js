832c31fa4e69d258014313734a235e965af1b39c
Luana Eugênia <luaeugenia1309@outlook.com>
2021-07-19 10:39:37 -0300

    Requisito 12 - Adicione um botão com id="salvar-tarefas" que salve o conteúdo da lista. Se você fechar e reabrir a página, a lista deve continuar no estado em que estava.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
 const botaoApagaTudo = document.getElementById('apaga-tudo');
 const botaoRemoverFinalizados = document.getElementById('remover-finalizados');
+const botaoSalvarTarefas = document.getElementById('salvar-tarefas');
+const classeListaDeTarefas = 'lista-tarefas';
 let elementoSelecionado = null;
 
 function selecionarTarefa(event) {
@@ -24,24 +26,32 @@ function completarTarefa(event) {
   }
 }
 
-function adicionarTarefa() {
-  const listaTarefas = document.getElementById('lista-tarefas');
+function criarTarefa(texto, classe = '') {
   const li = document.createElement('li');
-  const inputTextoTarefa = document.getElementById('texto-tarefa');
-  li.innerHTML = inputTextoTarefa.value;
+
+  li.setAttribute('class', classe);
   li.addEventListener('click', selecionarTarefa);
   li.addEventListener('dblclick', completarTarefa);
+  li.innerHTML = texto;
+
+  return li;
+}
+
+function adicionarTarefa() {
+  const listaTarefas = document.getElementById(classeListaDeTarefas);
+  const inputTextoTarefa = document.getElementById('texto-tarefa');
+  const li = criarTarefa(inputTextoTarefa.value);
   inputTextoTarefa.value = '';
   listaTarefas.appendChild(li);
 }
 
 function apagarListaDeTarefas() {
-  const listaTarefas = document.getElementById('lista-tarefas');
+  const listaTarefas = document.getElementById(classeListaDeTarefas);
   listaTarefas.innerHTML = '';
 }
 
 function removerFinalizados() {
-  const listaTarefas = document.getElementById('lista-tarefas');
+  const listaTarefas = document.getElementById(classeListaDeTarefas);
   const tarefas = listaTarefas.childNodes;
 
   for (let i = 0; i < tarefas.length; i += 1) {
@@ -54,6 +64,42 @@ function removerFinalizados() {
   }
 }
 
+function salvarTarefas() {
+  const listaTarefas = document.getElementById(classeListaDeTarefas);
+  const tarefas = listaTarefas.childNodes;
+  const arrayTarefas = [];
+
+  for (let i = 0; i < tarefas.length; i += 1) {
+    const tarefa = tarefas[i];
+    arrayTarefas
+      .push(`${tarefa.innerHTML}:`
+        + `${tarefa.classList.contains('completed') ? 'completed' : ''}`);
+  }
+
+  if (arrayTarefas.length) {
+    localStorage.setItem('tarefas', arrayTarefas);
+  } else {
+    localStorage.removeItem('tarefas');
+  }
+}
+
+window.onload = () => {
+  if (!localStorage.getItem('tarefas')) {
+    return;
+  }
+
+  const tarefas = localStorage.getItem('tarefas').split(',');
+  const listaTarefas = document.getElementById(classeListaDeTarefas);
+
+  for (let i = 0; i < tarefas.length; i += 1) {
+    const tarefa = tarefas[i].split(':');
+
+    const li = criarTarefa(tarefa[0], tarefa[1]);
+    listaTarefas.appendChild(li);
+  }
+};
+
 botaoCriarTarefa.addEventListener('click', adicionarTarefa);
 botaoApagaTudo.addEventListener('click', apagarListaDeTarefas);
 botaoRemoverFinalizados.addEventListener('click', removerFinalizados);
+botaoSalvarTarefas.addEventListener('click', salvarTarefas);
