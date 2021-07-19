323db09b39d92c09aef17c456638ddf52dea82a9
rphsantos1995 <rph.santos@Outlook.com>
2021-07-19 12:02:34 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const btn = document.getElementById("criar-tarefa")
-const taskList = document.getElementById("lista-tarefas")
+const addBtn = document.getElementById("criar-tarefa");
+const taskList = document.getElementById("lista-tarefas");
 const listItem = document.getElementById("li");
+const removeBtn = document.getElementById("apaga-tudo");
 
 function addNewTask() {   
 
-    let inputField = document.getElementById("texto-tarefa")
+    let inputField = document.getElementById("texto-tarefa");
     let newTask = document.createElement("li");
     newTask.innerHTML = inputField.value;
     newTask.className = "li-style";
@@ -26,5 +27,44 @@ function changeLiColor (eve) {
 console.log("changeLiColor");
 
 
-btn.addEventListener("click", addNewTask);
+function completedTask (event) {
+
+    let item = event.target;
+ 
+ item.classList.add("completed");
+ console.log("Marking as completed");
+
+ if (item.classList == "li-style completed selected") {
+     item.classList.remove("completed");
+     console.log("marking as uncompleted");
+ }
+}
+
+function deleteAll () {  
+    
+    let liList = document.querySelectorAll("#li");
+    
+    for ( let i = 0; i < liList.length; i++){
+
+       let li = document.getElementById("li");
+       taskList.removeChild(li);
+       console.log("removendo itens");
+
+    }
+
+
+    
+
+    console.log(liList);
+    
+
+}
+
+
+
+console.log("completedTask()");
+
+addBtn.addEventListener("click", addNewTask);
 taskList.addEventListener("click", changeLiColor);
+taskList.addEventListener("dblclick", completedTask);
+removeBtn.addEventListener('click', deleteAll);
