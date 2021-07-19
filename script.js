f9761f176583e712281878efaf903f01375aad2c
Patrick Paredes <patrickparedes44@gmail.com>
2021-07-19 05:48:00 -0400

    função letra riscada

--- script.js
+++ script.js
@@ -15,23 +15,17 @@ button.addEventListener('click', function () {
   buttonClear.addEventListener('click', function () {
     toDoList.innerHTML = '';
   });
+});
 
-  toDoList.addEventListener('click', (event) => {
-    const listTasks = document.querySelectorAll('li');
+toDoList.addEventListener('click', (event) => {
+  const listTasks = document.querySelectorAll('li');
 
-    for (let index = 0; index < listTasks.length; index += 1) {
-      listTasks[index].style.backgroundColor = 'white';
-    }
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
-  });
+  for (let index = 0; index < listTasks.length; index += 1) {
+    listTasks[index].style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+});
 
-  const saveButton = document.getElementById('salvar-tarefas');
-
-  saveButton.addEventListener('click', function () {
-    localStorage.setItem('listOfTasks', toDoList.innerHTML);
-
-    window.onload = function () {
-      toDoList.innerHTML = localStorage.getItem('listOfTasks');
-    }
-  });
+toDoList.addEventListener('dblclick', (event) => {
+  event.target.classList.toggle('completed');
 });
