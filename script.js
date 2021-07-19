2148646ebec3ce4b102e4af8db94efee2cff810c
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-07-19 13:25:28 -0300

    Arrumando alguns erros de indentaçao

--- script.js
+++ script.js
@@ -2,24 +2,20 @@ const tarefaBotao = document.getElementById('criar-tarefa');
 const caixaTexto = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const limparTudoBotao = document.getElementById('apaga-tudo');
-let itensDaLista = document.getElementsByTagName('li');
 
 tarefaBotao.addEventListener('click', function() {
-    let item = document.createElement('li')
-    item.setAttribute('class', 'tarefa');
-    item.innerText = caixaTexto.value;
-    lista.appendChild(item);
-    caixaTexto.value = "";
-})
-
+  let item = document.createElement('li')
+  item.setAttribute('class', 'tarefa');
+  item.innerText = caixaTexto.value;
+  lista.appendChild(item);
+  caixaTexto.value = "";
+});
 
 limparTudoBotao.addEventListener('click', function() {
     lista.innerHTML = '';
-
 })
 
 const corTarefa = document.querySelector('#lista-tarefas');
-
 function trocaCor(event) {
   const classSelector = document.querySelector('.marcado');
   event.target.classList.add('marcado');
@@ -32,8 +28,8 @@ function trocaCor(event) {
  corTarefa.addEventListener('click', trocaCor);
 
  function risca(event) {
-   const classSelectorDois = document.querySelector('.completed');
-   if (classSelectorDois === null) {
+  const classSelectorDois = document.querySelector('.completed');
+  if (classSelectorDois === null) {
     event.target.classList.add('completed');
    } else {
      event.target.classList.remove('completed')
@@ -41,9 +37,7 @@ function trocaCor(event) {
  }
   corTarefa.addEventListener('dblclick', risca);
 
-
 const removeTarefa = document.getElementById('remover-finalizados');
-
 function apagaRiscado() {
   const apagaFinalizado = document.querySelectorAll('.completed');
   for (let i of apagaFinalizado) {
@@ -53,4 +47,3 @@ function apagaRiscado() {
   }
 }
 removeTarefa.addEventListener('click', apagaRiscado);
-
