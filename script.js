a9893ae4e13362e1db06be966a317cd23a62ba9a
Alexandre Fortes <fortes04alexandre@gmail.com>
2021-07-19 04:05:25 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.onload = function () {
   const AddButton = document.querySelector('#criar-tarefa');
+  const AddTask = document.querySelector('#lista-tarefas');
+  // Adicionar tarefa ao clicar no botão
+  AddButton.addEventListener('click', () => {
+    const input = document.querySelector('#texto-tarefa');
+    const AddList = document.createElement('li');
+    if (input.value === '') alert('Adicione uma tarefa');
+    AddList.innerText = input.value;
+    AddTask.appendChild(AddList);
+    input.value = '';
+  });
 };
