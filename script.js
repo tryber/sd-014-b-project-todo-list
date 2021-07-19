ed0bf193d306129515ddc243e73141cb9098ebf8
Jean Marcel <jeanvanzella91@gmail.com>
2021-07-18 23:06:39 -0300

    finalizando

--- script.js
+++ script.js
@@ -17,17 +17,38 @@ getList.addEventListener('click', function(){
   for(let i = 0; i < getLi.length; i += 1) {
     getLi[i].classList.remove('selected')
   }
-  event.target.className = 'selected';
+  event.target.classList.add('selected');
 })
 
 getList.addEventListener('dblclick', function(){
-  event.target.classList.toggle('completed');
+  if (event.target.classList[0] === 'completed' || event.target.classList[1] === 'completed'){
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 })
-
+//criando botao remover tudo
 GetButtomApaga.addEventListener('click', function(){
   let getLi = document.querySelectorAll('li');
   for(let i = 0; i < getLi.length; i += 1) {
     getList.removeChild(getLi[i]);
   }
 })
-
+//criando botao remover finalizados
+getButtomRemove.addEventListener('click', function(){
+  let getLi = document.querySelectorAll('li');
+  for(let i = 0; i < getLi.length; i += 1) {
+    if (getLi[i].classList[0] === 'completed' || getLi[i].classList[1] === 'completed'){
+      getList.removeChild(getLi[i]);
+    }
+  }
+})
+//criando botao remover selecionados
+getButtomSelec.addEventListener('click', function(){
+  let getLi = document.querySelectorAll('li');
+  for(let i = 0; i < getLi.length; i += 1) {
+    if (getLi[i].classList[0] === 'selected'){
+      getList.removeChild(getLi[i]);
+    }
+  }
+})
