08645a4ca7371e1bf880d4744d6686da9b0e2832
Patrick Paredes <patrickparedes44@gmail.com>
2021-07-19 02:16:41 -0400

    correção npm run

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let tasks = document.getElementById('texto-tarefa');
-let button = document.getElementById('criar-tarefa');
-let toDoList = document.getElementById('lista-tarefas');
+const tasks = document.getElementById('texto-tarefa');
+const button = document.getElementById('criar-tarefa');
+const toDoList = document.getElementById('lista-tarefas');
 
 button.addEventListener('click', function () {
-  let newTasks = document.createElement('li');
+  const newTasks = document.createElement('li');
 
   newTasks.innerHTML = tasks.value;
   toDoList.appendChild(newTasks);
