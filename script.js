a4e5dbdebac70cee787111b65b57c3e5cd2c1022
Patrick Paredes <patrickparedes44@gmail.com>
2021-07-19 05:16:23 -0400

    função backcolor e save

--- script.js
+++ script.js
@@ -7,12 +7,31 @@ button.addEventListener('click', function () {
 
   newTasks.innerHTML = tasks.value;
   toDoList.appendChild(newTasks);
-
+  
   tasks.value = '';
 
   const buttonClear = document.getElementById('apaga-tudo');
 
   buttonClear.addEventListener('click', function () {
     toDoList.innerHTML = '';
+  });
+
+  toDoList.addEventListener('click', (event) => {
+    const listTasks = document.querySelectorAll('li');
+
+    for (let index = 0; index < listTasks.length; index += 1) {
+      listTasks[index].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  });
+
+  const saveButton = document.getElementById('salvar-tarefas');
+
+  saveButton.addEventListener('click', function () {
+    localStorage.setItem('listOfTasks', toDoList.innerHTML);
+
+    window.onload = function () {
+      toDoList.innerHTML = localStorage.getItem('listOfTasks');
+    }
   });
 });
