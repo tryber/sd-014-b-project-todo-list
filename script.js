24f98f77cde640ff9210e2aebc11b5aa18f582a2
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-07-19 13:40:29 -0300

    Deixando o codigo indentado2

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ const caixaTexto = document.getElementById('texto-tarefa');
 const lista = document.getElementById('lista-tarefas');
 const limparTudoBotao = document.getElementById('apaga-tudo');
 
-tarefaBotao.addEventListener('click', function() {
-  let item = document.createElement('li')
+tarefaBotao.addEventListener('click', function()  {
+  const item = document.createElement('li');
   item.setAttribute('class', 'tarefa');
   item.innerText = caixaTexto.value;
   lista.appendChild(item);
-  caixaTexto.value = "";
-});
+  caixaTexto.value = '';
+}
+);
 
-limparTudoBotao.addEventListener('click', function() {
-    lista.innerHTML = '';
-})
+limparTudoBotao.addEventListener('click', function()  {
+  lista.innerHTML = '';
+}
+);
 
 const corTarefa = document.querySelector('#lista-tarefas');
 function trocaCor(event) {
@@ -23,19 +25,19 @@ function trocaCor(event) {
     classSelector !== null && classSelector !== event.target
   ) {
     classSelector.classList.remove('marcado');
-  }
+ }
 } 
- corTarefa.addEventListener('click', trocaCor);
+corTarefa.addEventListener('click', trocaCor);
 
  function risca(event) {
   const classSelectorDois = document.querySelector('.completed');
   if (classSelectorDois === null) {
     event.target.classList.add('completed');
-   } else {
-     event.target.classList.remove('completed')
+  } else {
+    event.target.classList.remove('completed')
    }
  }
-  corTarefa.addEventListener('dblclick', risca);
+corTarefa.addEventListener('dblclick', risca);
 
 const removeTarefa = document.getElementById('remover-finalizados');
 function apagaRiscado() {
