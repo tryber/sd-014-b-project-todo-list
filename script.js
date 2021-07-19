0ddf789d2819410f2d451f2279a0c2a732261b68
Alexandre Fortes <fortes04alexandre@gmail.com>
2021-07-19 05:03:26 -0300

    req7

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 window.onload = function () {
   const AddButton = document.querySelector('#criar-tarefa');
-  const AddTask = document.querySelector('#lista-tarefas');
+  const TaskSelect = document.querySelector('#lista-tarefas');
   // Adicionar tarefa ao clicar no botão
   AddButton.addEventListener('click', () => {
     const input = document.querySelector('#texto-tarefa');
     const AddList = document.createElement('li');
     if (input.value === '') alert('Adicione uma tarefa');
     AddList.innerText = input.value;
-    AddTask.appendChild(AddList);
+    TaskSelect.appendChild(AddList);
     input.value = '';
+  });
+  // Colorir tarefa selecionada
+  TaskSelect.addEventListener('click', (selected) => {
+    const TaskSelected = document.querySelectorAll('li');
+    for (const i of TaskSelected) { i.style.backgroundColor = 'white'; }
+    selected.target.style.backgroundColor = 'rgb(128,128,128)';
   });
 };
