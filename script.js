a0068e0d1bded7f1c19f196ff5a94e1cca167825
FelipeMartins <mfelipeluiz99@gmail.com>
2021-07-21 11:59:09 -0300

    requisito 9

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+////https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
 const query = document.querySelector.bind(document);
 const queryAll = document.querySelectorAll.bind(document);
 
@@ -7,12 +8,22 @@ const turnGrey = (x) =>
     event.target.classList.add('selected');
   });
 
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+const lineTrough = (y) => {
+  y.addEventListener('dblclick', (e) => {
+    e.target.classList.contains('completed')
+      ? e.target.classList.remove('completed')
+      : e.target.classList.add('completed');
+  });
+};
+
 const insertLi = () => {
   let taskList = query('#lista-tarefas');
   let newLi = document.createElement('li');
   let tagTextoTarefa = query('#texto-tarefa');
   taskList.appendChild(newLi);
   turnGrey(newLi);
+  lineTrough(newLi);
   newLi.innerText = tagTextoTarefa.value;
   tagTextoTarefa.value = '';
 };
