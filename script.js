44ca91b310e1c334c340ef2dabc89b7ce24aba22
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-07-31 18:38:37 -0300

    requisitos

--- script.js
+++ script.js
@@ -1,10 +1,109 @@
-let listaTarefas = document.querySelector('#lista-tarefas');
-let botao = document.querySelector('#criar-tarefa');
-let tarefa = document.querySelector('#texto-tarefa');
+const createTask = document.querySelector('#criar-tarefa');
+const clearAll = document.querySelector('#apaga-tudo');
+// const removeAllFinal = document.querySelector('#remover-finalizados');
+const saveTasks = document.querySelector('#salvar-tarefas');
+const moveUp = document.querySelector('#mover-cima');
+const moveDown = document.querySelector('#mover-baixo');
+const removeSelected = document.querySelector('#remover-selecionado');
+const newTask = document.querySelector('#lista-tarefas');
+const inputTask = document.querySelector('#texto-tarefa');
 
-function adicionaTarefa () {
-  let aux = document.createElement('li');
-  aux.innerText = tarefa.value;
-  listaTarefas.appendChild(aux);
-  tarefa.value = '';
+//  questão 5 - cria-se novo item na lista
+function createNewTask (){
+  if (inputTask.value === '') {
+    return null;
+  }
+
+  const createLisTask = document.createElement('li');
+  createLisTask.innerHTML = inputTask.value;
+  createLisTask.classList.add('tarefa');
+  newTask.appendChild(createLisTask);
+  inputTask.value = '';
 }
+
+createTask.addEventListener('click', createNewTask);
+
+// -----------------------------------------------------------------------------------------------
+// questão 7
+newTask.addEventListener('click', (event) => {
+  const white = event.target;
+  if (white.className === 'item-list') {
+    const selectedColor = document.querySelector('.selected');
+    if (selectedColor !== null) {
+      selectedColor.classList.remove('.selected');
+      selectedColor.style.backgroundColor = 'white';
+    }
+    white.classList.add('.selected');
+    white.style.backgroundColor = 'rgb(128,128,128)';
+  }
+});
+
+// ---------------------------------------------------------------------------------------------
+// questão 9
+function completed(event){
+  const riscaTask = event.target;
+  if (riscaTask.classList.contains('completed')) {
+    riscaTask.classList.remove('completed');
+  } else {
+    riscaTask.classList.add('completed');
+  }
+}
+newTask.addEventListener('dblclick', completed);
+// ---------------------------------------------------------------------------------------------
+// questão 10
+function clearTask() {
+  newTask.innerHTML = '';
+}
+clearAll.addEventListener('click', clearTask);
+
+// --------------------------------------------------------------------------------------------
+//  questão 12
+function saveAllTask() {
+  const taskSave = newTask.innerHTML;
+  localStorage.setItem('save',taskSave);
+}
+saveTasks.addEventListener('click', saveAllTask);
+
+function saveLocal() {
+  const saveNew = localStorage.getItem('save');
+  if (saveNew !== null);
+  newTask.innerHTML = saveNew;
+}
+saveLocal();
+
+
+// ------------------------------------------------------------------------------------------
+// questão 13 
+function buttonUp() {
+  const selectButtonUp = document.querySelector('.selected');
+    if (selectButtonUp !== null) {
+    const taskUp = selectButtonUp.previousElementSibling;
+    if (selectButtonUp !== newTask.firstChild) {
+      newTask.insertBefore(selectButtonUp, taskUp);
+    }
+  }
+}
+
+function buttonDown (){
+  const selectButtonDown = document.querySelector('.selected');
+  if (selectButtonDown !== null) {
+    const taskDown = selectButtonDown.nextElementSibling;
+    if (selectButtonDown !== newTask.lastChild) {
+    newTask.insertBefore(selectButtonDown, taskDown.nextElementSibling);
+    }
+  }
+}
+
+moveUp.addEventListener('click',buttonUp);
+moveDown.addEventListener('click',buttonDown);
+// --------------------------------------------------------------------------------
+  document.querySelector('#criar-tarefa').onclick = function () {
+  if (document.querySelector('.task input').value.length === 0){
+  alert ("Adicione uma tarefa!")
+   }
+}  
+
+// referências;
+// https://www.youtube.com/watch?v=cOUNOi297Mw&list=PLX4L0zwlay26RzUq6leLM0mdAEFQY1Rky&index=65&ab_channel=CodingArtist
+// /* https://www.w3schools.com/css/css_overflow.asp
+// https://desenvolvimentoparaweb.com/css/unidades-css-rem-vh-vw-vmin-vmax-ex-ch/ 
