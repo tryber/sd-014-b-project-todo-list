16cc17e65bdc4645d214f132a23a34762c0a9b0a
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-08-02 15:31:46 -0300

    requisito 14

--- script.js
+++ script.js
@@ -4,20 +4,19 @@ const removeAllFinal = document.querySelector('#remover-finalizados');
 const saveTasks = document.querySelector('#salvar-tarefas');
 // const moveUp = document.querySelector('#mover-cima');
 // const moveDown = document.querySelector('#mover-baixo');
-const removeSelected = document.querySelector('#remover-selecionado');
 const newTask = document.querySelector('#lista-tarefas');
 const inputTask = document.querySelector('#texto-tarefa');
-const list = document.getElementsByTagName('li');
 const listaOrdenada = document.querySelector('ol'); 
+const list = document.getElementsByTagName('li');
 
-function alert() {
+function alertButton() {
   document.querySelector('#criar-tarefa').onclick = function () {
     if (document.querySelector('.task input').value.length === 0) {
       alert('Adicione uma tarefa!');
     }
-  }  
+  };  
 }
-alert();
+alertButton();
 //  questão 5 - cria-se novo item na lista
 function createNewTask() {
   if (inputTask.value === '') {
@@ -38,8 +37,7 @@ createTask.addEventListener('click', createNewTask);
 function backgroundGray() {
   listaOrdenada.addEventListener('click', (event) => {
     for (let i = 0; i < list.length; i += 1) {
-      let listGray = list[i];
-      // listGray.style.backgroundColor = 'rgb(128,128,128)';
+      const listGray = list[i];
       listGray.style.backgroundColor = '';
     }
     event.target.style.backgroundColor = 'rgb(128,128,128)';
@@ -48,7 +46,7 @@ function backgroundGray() {
 backgroundGray();
 // -------------------------------------------------------------------------------------
 // questão 9
-function completed(event){
+function completed(event) {
   const riscaTask = event.target;
   if (riscaTask.classList.contains('completed')) {
     riscaTask.classList.remove('completed');
@@ -65,7 +63,7 @@ function clearTask() {
 clearAll.addEventListener('click', clearTask);
 
 // --------------------------------------------------------------------------------------------
-// questão 11 pois é Brasil aqui estava chamando queryselector e não pela className!
+// questão 11 
 function removeFinalizados(){
   const fim = document.getElementsByClassName('completed');
   for (let index = 0; index < fim.length; index += 1) {
@@ -77,7 +75,7 @@ removeAllFinal.addEventListener('click', removeFinalizados);
 //  questão 12
 function saveAllTask() {
   const taskSave = newTask.innerHTML;
-  localStorage.setItem('save',taskSave); 
+  localStorage.setItem('save' , taskSave); 
 }
 saveTasks.addEventListener('click', saveAllTask);
 
@@ -88,37 +86,20 @@ function saveLocal() {
 }
 saveLocal();
 
-// ------------------------------------------------------------------------------------------
-// questão 13 
-// function buttonUp() {
-//   const selectButtonUp = document.querySelector('selected');
-//     if (selectButtonUp.previousSibling) {
-//   selectButtonUp.parentNode.insertBefore(selectButtonUp.nextSibling);
-//     }
-// }
-// moveUp.addEventListener('click',buttonUp);
-
-// function buttonDown (){
-//   const selectButtonDown = document.querySelector('selected');
-//   if (selectButtonDown.previousSibling) {
-//     selectButtonDown.parentNode.insertBefore(selectButtonDown.nextSibling);
-//   }
-// }
-// moveDown.addEventListener('click',buttonDown);
-
 // --------------------------------------------------------------------------------
 // questão 14
-// function removerSelecionado(){
-//   removeSelected.addEventListener('click', () => {
-//    const listRemove = document.querySelectorAll('selected');
-//    for (let i = 0; i < listRemove.length; i += 1) {
-//      if(listRemove[i].classList.contains('backgroundGray')) {
-//        newTask.removeChild(listRemove[i]);
-//   }
-//    }
-//   });
-// }
-// removerSelecionado();
+let removeSelected = document.querySelector('#remover-selecionado');
+removeSelected.addEventListener('click', function (event) {
+  for (let index = 0; index < list.length; index += 1) {
+    if(list[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+      list[index].remove()
+    }
+
+  }
+})
+
+// referências:
 // https://www.youtube.com/watch?v=cOUNOi297Mw&list=PLX4L0zwlay26RzUq6leLM0mdAEFQY1Rky&index=65&ab_channel=CodingArtist
 // /* https://www.w3schools.com/css/css_overflow.asp
 // https://desenvolvimentoparaweb.com/css/unidades-css-rem-vh-vw-vmin-vmax-ex-ch/ 
+// https://www.w3schools.com/jsref/met_node_insertbefore.asp
