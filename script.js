8441a8788b9e630fa027267c4a57bd1ec0fce71c
vinicius23-dev <nicius.cadedes@gmail.com>
2021-08-02 23:20:10 -0300

    Push

--- script.js
+++ script.js
@@ -1,96 +1,118 @@
 const listadetarefas = document.getElementById('lista-tarefas'); //lista ol <li>
 const textoinput = document.getElementById('texto-tarefa').value; //caixa de input
 
+//***** LIMPAR O SAVE
+const lmpsave = document.getElementById('limpar-save');
+lmpsave.addEventListener('click', LimpSave);
+
+function LimpSave(){
+  const lmp = document.querySelector('ol').innerHTML;
+  localStorage.clear();
+  location.reload();
+}
+//***** FIM DO LIMPAR O SAVE
 
 
+//***** SALVAR LISTA
+const svTarefas = document.getElementById('salvar-tarefas'); //o botao salvar lista
+svTarefas.addEventListener('click', SalvarTarefas);
+
+window.load = CarregarSave(); //carregamento da pagina chamando uma funcao
+
+
+function SalvarTarefas(){
+  const lis = document.querySelector('ol').innerHTML;
+  localStorage.setItem('save', lis);
+}
+
+function CarregarSave(){
+  if (localStorage.save !== undefined) {
+    document.querySelector('ol').innerHTML = localStorage.save;
+  }
+}
+//***** FIM DO SALVAR LISTA
+
+const itens = document.getElementById('lista-tarefas');
+const rmvfinalizados = document.getElementById('remover-finalizados');
+rmvfinalizados.addEventListener('click', rmvEnd);
+
+function rmvEnd(){
+  const list = itens.childNodes;
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i].style === 'text-decoration: line-through') {
+      list[i].remove();
+    }
+  }
+
+}
+
+//r.target.style = "text-decoration: line-through";
+
+
+
+
+
+
+//FUNÇÃO DE TESTE
 function alertaaa(){
     alert("Photografh!");
 }
+//FIM DA FUNÇÃO DE TESTE
 
+
+//***** ADICIONANDO UM ITEM
 function adicionarItem(){
-    
-
     const info = document.getElementById('texto-tarefa').value
     const list = document.getElementById('lista-tarefas');
-    const toDo = document.createElement('li');
-    toDo.innerHTML = info;
-    if (toDo !== '') {
-      list.appendChild(toDo);
+    const liElement = document.createElement('li');
+    liElement.innerHTML = info;
+    if (liElement !== '') {
+      list.appendChild(liElement);
     }
     document.querySelector('#texto-tarefa').value = '';
 
-   
-    
     /*
     let aderenceli = document.createElement('li');
     aderenceli.innerText = textoinput;
 
     listadetarefas.appendChild(aderenceli);
    document.querySelector('texto-tarefa').valeu = '';*/
-   
-   
 }
+//***** FIM DE ADICIONANDO UM ITEM
 
-    //Limpar Lista
-    function apagatudo(){
+
+
+
+function apagatudo(){
     const list = document.getElementById('lista-tarefas');
     let qq = document.querySelectorAll("li");
     list.remove('li'); 
-    location.reload();
-    //método encontrado na internet para recarregar a página
-    //após o método. Senao nao haveria como add um item novamente
-  
-}
-
-function load(){
-  if (localStorage.save !== undefined) {
-    document.querySelector('ol').innerHTML = localStorage.save;
-  }
-}
-
-/*function storeList() {
-  const lis = document.querySelector('ol').innerHTML;
-  localStorage.setItem('save', lis);
-}
-
-const saveList = document.getElementById('salvar-tarefas');
-saveList.addEventListener('click', storeList)*/
-
-
-
-    //REMOVENDO UMA TAREFA
-    function rmv(){  
-    /*let qq = document.querySelector("li");
-    qq.remove('li');  */ 
-    let qq = document.querySelectorAll("li");
-    for (let index = 0; index < qq.length; index++) {
-      const element = qq[index];
-      
+    location.reload(); //método encontrado na internet para recarregar a página
+                        //após o método. Senao nao haveria como add um item novamente
     }
-    qq.remove(element);  
-    }
+
     
+    //REMOVENDO UMA TAREFA
+    function rmv(e){  
+    let qq = document.querySelector("li"); 
+    qq.remove('li');
+  } 
+    const selecionado = document.querySelector('#remover-finalizados');
+    selecionado.addEventListener('click', rmv);
+
 
     //TROCANDO O BACKGROUND DE UMA TAREFA
     function trocarCor(e) {    
-      //e.target.style.backgroundColor = 'rgb(128,128,128)';
-      let qq = document.querySelectorAll("li");
-      for (let index = 0; index < qq.length; index++) {
-        const element = qq[index];  
-        qq[index].style.backgroundColor = 'white';
-      }        
-
-      e.target.style.backgroundColor = 'rgb(128,128,128)';
+      e.target.style.backgroundColor = 'rgb(128,128,128)';            
     }
-
     //const nomeCinza = document.querySelector('ol');
     const nomeCinza = document.getElementById('lista-tarefas');
     nomeCinza.addEventListener('click', trocarCor);
-           
+        
     
     //RISCANDO UMA TAREFA
     function riscar(r){
-    r.target.style = "text-decoration: line-through solid rgb(0, 0, 0)";
+    r.target.style = "text-decoration: line-through";
     }
     const nomeRiscado = document.querySelector('ol');
     nomeRiscado.addEventListener('dblclick', riscar);
