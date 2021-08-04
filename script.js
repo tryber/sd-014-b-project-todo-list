8f784f6793030ccfb0e5f204e512a599cb2aa945
Jamez Mayke <jamezmayke@gmail.com>
2021-08-04 15:00:52 -0300

    Requisitos do lint corrigidos

--- script.js
+++ script.js
@@ -3,32 +3,30 @@ const ordenaListaTarefas = document.querySelector('#lista-tarefas');
 const inputTextoTarefa = document.querySelector('#texto-tarefa');
 
 btnCriarTarefa.addEventListener('click', () => {
-    let criaTarefaLista = document.createElement('li');
-    ordenaListaTarefas.appendChild(criaTarefaLista);
+	const criaTarefaLista = document.createElement('li');
+	ordenaListaTarefas.appendChild(criaTarefaLista);
 
-    criaTarefaLista.innerText = inputTextoTarefa.value;
-    criaTarefaLista.className = 'tarefa-criada';
+	criaTarefaLista.innerText = inputTextoTarefa.value;
+	criaTarefaLista.className = 'tarefa-criada';
 
-    inputTextoTarefa.value = null; // obs: undefined quando aplicado fica como conteúdo do input!!
+	inputTextoTarefa.value = null; // obs: undefined quando aplicado fica como conteúdo do input!!
 });
 
 ordenaListaTarefas.addEventListener('click', (event) => {
+	const selecionarTarefa = document.querySelector('.select');
 
-    const selecionarTarefa = document.querySelector('.select');
-
-    if(event.target.className === 'tarefa-criada') {
-        event.target.classList.add('select');
-    }
-    if (selecionarTarefa !== null) {
-        selecionarTarefa.target.classList.remove('select');
-    }
+	if (event.target.className === 'tarefa-criada') {
+		event.target.classList.add('select');
+	};
+	if (selecionarTarefa !== null) {
+		selecionarTarefa.target.classList.remove('select');
+	};
 });
 
 ordenaListaTarefas.addEventListener('dblclick', (event) => {
-    if (event.classList.contains('complete')) { // includes não deu certo, depois verificar pq.
-        event.classList.remove('complete')
-    } else {
-        event.classList.add('complete');
-    }
-})
-
+	if (event.classList.contains('complete')) { // includes não deu certo, depois verificar pq.
+		event.classList.remove('complete');
+	} else {
+		event.classList.add('complete');
+	};
+});
